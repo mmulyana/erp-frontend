9e85266e044696157ee36934b0473efcf19480b9
mmulyana <mulyan.t20@gmail.com>
2024-06-25 23:22:47 +0700

    feat: add suspense for dashboard

---
+++ src/routes/dashboard/index.tsx
@@ -0,0 +1,39 @@
+import { PATH } from '@/utils/constant/_paths.ts'
+import { Route } from 'react-router-dom'
+import React, { Suspense, lazy } from 'react'
+import LoadingScreen from '@/components/loading-screen/index.tsx'
+import ProtectedRoute from '@/utils/protected-route.tsx'
+
+const Main = lazy(() => import('./main/index.tsx'))
+
+const useRoutes = () => {
+  return [
+    {
+      path: PATH.DASHBOARD,
+      element: <Main />,
+      show: true,
+    },
+  ]
+}
+
+export default function DashboardRoutes() {
+  const routes = useRoutes()
+  return (
+    <React.Fragment>
+      {routes.map(
+        (route, index) =>
+          route.show && (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Suspense fallback={<LoadingScreen />}>
+                  <ProtectedRoute>{route.element}</ProtectedRoute>
+                </Suspense>
+              }
+            />
+          )
+      )}
+    </React.Fragment>
+  )
+}
