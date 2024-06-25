23bdfa7797687a3cf9bf0955e01c29d6c7a43ee0
mmulyana <mulyan.t20@gmail.com>
2024-06-25 23:20:29 +0700

    feat: add suspense at routes auth

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -1,6 +1,7 @@
 import { PATH } from '@/utils/constant/_paths.ts'
 import { Route } from 'react-router-dom'
-import { lazy } from 'react'
+import { Suspense, lazy } from 'react'
+import LoadingScreen from '@/components/loading-screen/index.tsx'
 
 const Login = lazy(() => import('./login/index.tsx'))
 const Register = lazy(() => import('./register/index.tsx'))
@@ -32,7 +33,13 @@ export default function AuthRoutes() {
   return (
     <Route>
       {routes.map((route, index) => (
-        <Route key={index} {...route} />
+        <Route
+          key={index}
+          path={route.path}
+          element={
+            <Suspense fallback={<LoadingScreen />}>{route.element}</Suspense>
+          }
+        />
       ))}
     </Route>
   )
