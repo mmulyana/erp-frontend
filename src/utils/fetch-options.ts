761e554941b0b7730698009321a01cfcc5166a4c
mmulyana <mulyan.t20@gmail.com>
2024-06-25 23:28:38 +0700

    feat: update fetch options

--- src/utils/fetch-options.ts
+++ src/utils/fetch-options.ts
@@ -1,7 +1,7 @@
 export default class fetchOptions {
   public data: any
 
-  constructor(method: string, body: any, type: string = 'application/json') {
+  constructor(method: string, body?: any, type: string = 'application/json') {
     this.data = {
       method: method || 'GET',
       headers: {
