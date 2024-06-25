e37a674e3e2766fd88473407ed038ae0ed3ae7f9
mmulyana <mulyan.t20@gmail.com>
2024-06-25 23:19:34 +0700

    feat: add loading screen

---
+++ src/components/loading-screen/index.tsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+  return (
+    <div className='h-screen w-full bg-gray-100 flex items-center justify-center'>
+      <p className='text-4xl text-gray-800'>Loading..</p>
+    </div>
+  )
+}
