7783ecd2ec354ae35078527d713e7ccf0c6b4f11
mmulyana <mulyan.t20@gmail.com>
2024-06-28 02:23:20 +0700

    chore: update navbar and active nav

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/utils/cn'
 import { LucideIcon } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { Link, matchPath, useLocation } from 'react-router-dom'
 import { buttonVariants } from '../ui/button'
 
 interface navVertical {
@@ -11,7 +11,7 @@ type Links = {
   title: string
   label?: string
   icon: LucideIcon
-  variant: 'default' | 'ghost' | 'secondary'
+  path: string
 }
 
 type Props = {
@@ -24,33 +24,39 @@ export default function Navbar(props: Props) {
 }
 
 function NavbarVertical(props: navVertical) {
+  const location = useLocation()
+
   return (
     <div className='flex flex-col gap-4 py-2'>
       <nav className='grid gap-1'>
-        {props.links.map((link, index) => (
-          <Link
-            key={index}
-            to='#'
-            className={cn(
-              buttonVariants({ variant: link.variant, size: 'sm' }),
-              link.variant === 'default' && 'justify-start'
-            )}
-          >
-            <link.icon className='mr-2 h-4 w-4' />
-            {link.title}
-            {link.label && (
-              <span
-                className={cn(
-                  'ml-auto',
-                  link.variant === 'default' && 'text-background',
-                  link.variant === 'ghost' && 'text-neutral-400'
-                )}
-              >
-                {link.label}
-              </span>
-            )}
-          </Link>
-        ))}
+        {props.links.map((link, index) => {
+          const isActive = matchPath({ path: link.path }, location.pathname)
+          return (
+            <Link
+              key={index}
+              to={link.path}
+              className={cn(
+                buttonVariants({ variant: isActive ? 'secondary' : null }),
+                'rounded-lg flex justify-between items-center'
+              )}
+            >
+              <div className='flex gap-2 items-center'>
+                <link.icon className='h-4 w-4' />
+                {link.title}
+              </div>
+              {link.label && (
+                <span
+                  className={cn(
+                    'font-normal',
+                    isActive ? 'text-neutral-800' : 'text-neutral-300'
+                  )}
+                >
+                  {link.label}
+                </span>
+              )}
+            </Link>
+          )
+        })}
       </nav>
     </div>
   )
