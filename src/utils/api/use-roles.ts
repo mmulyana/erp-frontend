6afc0e50066419ea30d4091f79756fb15600629d
mmulyana <mulyan.t20@gmail.com>
2024-06-29 22:27:28 +0700

    chore: remove token fetcher

--- src/utils/api/use-roles.ts
+++ src/utils/api/use-roles.ts
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 import { KEYS } from '../constant/_keys'
 import fetchOptions from '../fetch-options'
-import { CookieKeys, CookieStorage } from '../cookie'
 import { URLS } from '../constant/_urls'
 
 export const useRoles = () => {
@@ -10,8 +9,7 @@ export const useRoles = () => {
 
 const fetcherRoles = async () => {
   const options = new fetchOptions('GET')
-  const token = CookieStorage.get(CookieKeys.AuthToken)
-  options.addToken(token)
+  options.addToken()
   const response = await fetch(URLS.ROLES, options.data)
   const { data } = await response.json()
 
