2598783ab7aab94b3cea1611c53ca8f405f416b3
mmulyana <mulyan.t20@gmail.com>
2024-07-07 03:11:18 +0700

    feat(hooks): add new fetcher permission group

--- src/utils/api/fetcher/fetcher-permission.ts
+++ src/utils/api/fetcher/fetcher-permission.ts
@@ -1,6 +1,10 @@
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
 
-export const fetcherPermissionGroup = async () => {
+export const fetcherPermissionsGroup = async () => {
   return await http(URLS.PERMISSION_GROUP)
+}
+
+export const fetcherPermissionGroup = async (id: number) => {
+  return await http(`${URLS.PERMISSION_GROUP}/${id}`)
 }
