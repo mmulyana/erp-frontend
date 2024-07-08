38dcbd7e6d67ee94e82e97aeee99b619e02e80e5
mmulyana <mulyan.t20@gmail.com>
2024-07-08 22:04:02 +0700

    feat(hooks): fetcher create permission group

--- src/utils/api/fetcher/fetcher-permission.ts
+++ src/utils/api/fetcher/fetcher-permission.ts
@@ -1,5 +1,10 @@
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
+import { PermissionGroup } from '@/utils/types/permision-group'
+
+export type createPGPayload = Pick<PermissionGroup, 'name' | 'description'> & {
+  permissionNames: string[]
+}
 
 export const fetcherPermissionsGroup = async () => {
   return await http(URLS.PERMISSION_GROUP)
@@ -7,4 +12,10 @@ export const fetcherPermissionsGroup = async () => {
 
 export const fetcherPermissionGroup = async (id: number) => {
   return await http(`${URLS.PERMISSION_GROUP}/${id}`)
+}
+
+export const fetcherCreatePermissionGroup = async (
+  payload: createPGPayload
+) => {
+  return await http.post(URLS.PERMISSION_GROUP, payload)
 }
