58ad32a504fd9e0b46bcfce05aca8b05a3365b4e
mmulyana <mulyan.t20@gmail.com>
2024-07-08 23:20:57 +0700

    feat(permission): new schema update

--- src/routes/dashboard/account-and-roles/permission/_components/schema.ts
+++ src/routes/dashboard/account-and-roles/permission/_components/schema.ts
@@ -11,3 +11,24 @@ export const permissionCreateSchema = z.object({
     )
     .optional(),
 })
+
+export const permissionUpdateSchema = z.object({
+  name: z.string().min(1, { message: 'nama grup wajib terisi' }),
+  description: z.string().optional(),
+  permissionNames: z
+    .array(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+      })
+    )
+    .optional(),
+  newPermissionNames: z
+    .array(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+      })
+    )
+    .optional(),
+})
