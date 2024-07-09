69203792482e8ff1137ef03ef0cd72ab58f30f93
mmulyana <mulyan.t20@gmail.com>
2024-07-09 23:05:05 +0700

    feat(hooks): new fetcher permission

--- src/utils/api/fetcher/fetcher-permission.ts
+++ src/utils/api/fetcher/fetcher-permission.ts
@@ -19,6 +19,10 @@ export const fetcherPermissionGroup = async (id?: number) => {
   return await http(`${URLS.PERMISSION_GROUP}/${id}`)
 }
 
+export const fetcherCheckPermission = async (id?: number) => {
+  return await http(`${URLS.PERMISSION_CHECK}/${id}`)
+}
+
 export const fetcherCreatePermissionGroup = async (
   payload: createPGPayload
 ) => {
@@ -28,9 +32,11 @@ export const fetcherCreatePermissionGroup = async (
 export const fetcherUpdatePermissionGroup = async (
   payload: updatePGPayload
 ) => {
-  return await http.post(`${URLS.PERMISSION_GROUP}/${payload.id}`, payload)
+  return await http.patch(`${URLS.PERMISSION_GROUP}/${payload.id}`, payload)
 }
 
-export const fetcherDeletePermission = async (id: number) => {
-  return await http.delete(`${URLS.PERMISSION_GROUP}/${id}`)
+export const fetcherDeletePermission = async (id?: number) => {
+  if (!id) throw new Error('id group is required')
+
+  return await http.delete(`${URLS.PERMISSION}/${id}`)
 }
