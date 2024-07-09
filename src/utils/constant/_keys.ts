6f7f7269039e8c6794799f49d8336f4c3b125c2c
mmulyana <mulyan.t20@gmail.com>
2024-07-09 23:06:31 +0700

    feat(keys): permission check

--- src/utils/constant/_keys.ts
+++ src/utils/constant/_keys.ts
@@ -2,5 +2,6 @@ export const KEYS = {
   ACCOUNT: 'account',
   ACCOUNTS: 'accounts',
   ROLES: 'roles',
-  PERMISSION_GROUP: 'permission'
+  PERMISSION_GROUP: 'permission',
+  PERMISSION_CHECK: 'permission_check',
 }
