388ebd2e7ebdc7b9a70c59ccf620ceff17f8e13b
mmulyana <mulyan.t20@gmail.com>
2024-07-11 01:56:10 +0700

    fix(hooks): add update payload

--- src/utils/api/fetcher/fetcher-roles.ts
+++ src/utils/api/fetcher/fetcher-roles.ts
@@ -2,6 +2,15 @@ import http from '@/utils/http'
 import { URLS } from '@/utils/constant/_urls'
 import { Roles } from '@/utils/types/roles'
 
+type createPayload = {
+  name: string,
+  permissionIds: number[]
+}
+
+type updatePayload = createPayload & {
+  id: number
+}
+
 export const fetcherRole = async (id?: number) => {
   return await http(`${URLS.ROLES}/${id}`)
 }
@@ -10,14 +19,14 @@ export const fetcherRoles = async () => {
   return await http(URLS.ROLES)
 }
 
-export const fetcherUpdateRoles = async ({ name, id }: Roles) => {
-  return await http.patch(`${URLS.ROLES}/${id}`, { name })
+export const fetcherUpdateRoles = async (payload: updatePayload) => {
+  return await http.patch(`${URLS.ROLES}/${payload.id}`, payload)
 }
 
 export const fetcherDeleteRoles = async ({ id }: Pick<Roles, 'id'>) => {
   return await http.delete(`${URLS.ROLES}/${id}`)
 }
 
-export const fetcherCreateRoles = async ({ name }: Pick<Roles, 'name'>) => {
-  return await http.post(`${URLS.ROLES}/`, { name })
+export const fetcherCreateRoles = async (payload: createPayload) => {
+  return await http.post(`${URLS.ROLES}/`, payload)
 }
