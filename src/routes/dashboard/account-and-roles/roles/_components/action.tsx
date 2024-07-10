5e0ad350ad24ef89b833d626dd5d038aa33112a0
mmulyana <mulyan.t20@gmail.com>
2024-07-11 01:55:02 +0700

    feat(roles) add edit in action

--- src/routes/dashboard/account-and-roles/roles/_components/action.tsx
+++ src/routes/dashboard/account-and-roles/roles/_components/action.tsx
@@ -1,10 +1,12 @@
+import Edit from './edit'
+
 type Props = {
   id?: number
 }
 export default function Action(props: Props) {
   return (
     <div className='flex gap-2 items-center justify-end'>
-        
+      <Edit id={props.id} />
     </div>
   )
 }
