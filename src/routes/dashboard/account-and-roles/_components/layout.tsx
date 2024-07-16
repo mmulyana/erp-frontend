31e9c3860c6f49f5796772e7f354a9aa8725c29d
mmulyana <mulyan.t20@gmail.com>
2024-07-17 03:51:13 +0700

    feat: add dashboard layout

--- src/routes/dashboard/account-and-roles/_components/layout.tsx
+++ src/routes/dashboard/account-and-roles/_components/layout.tsx
@@ -1,11 +1,25 @@
-import Navbar from '@/components/navbar'
+import React, { useMemo } from 'react'
 import { useAccounts } from '@/utils/api/use-account'
 import { usePermissionsGroup } from '@/utils/api/use-permission'
 import { useRoles } from '@/utils/api/use-roles'
 import { PATH } from '@/utils/constant/_paths'
 import { Separator } from '@radix-ui/react-separator'
 import { KeyRound, Shield, Users2 } from 'lucide-react'
-import React, { useMemo } from 'react'
+import { cn } from '@/utils/cn'
+import { LucideIcon } from 'lucide-react'
+import { Link, matchPath, useLocation } from 'react-router-dom'
+import { buttonVariants } from '@/components/ui/button'
+import { DashboardLayout } from '../../component'
+
+type Links = {
+  title: string
+  label?: string
+  icon: LucideIcon
+  path: string
+}
+interface navVertical {
+  links: Links[]
+}
 
 export default function Layout({ children }: React.PropsWithChildren) {
   const { data: dataAccounts, isLoading: isLoadingAccount } = useAccounts()
@@ -28,45 +42,13 @@ export default function Layout({ children }: React.PropsWithChildren) {
   }, [dataRoles])
 
   return (
-    <>
-      <div className='max-w-full mx-auto px-4 pt-4'>
+    <DashboardLayout>
+      <div className='max-w-full mx-auto px-4 pt-2.5'>
         <div className='mb-4'>
-          <p className='text-xl text-gray-800'>Account & Role Management</p>
+          <p className='text-gray-800'>Account & Role Management</p>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-[200px_1fr] gap-4'>
-          <div className='rounded-lg border border-gray-200 bg-white h-fit p-2'>
-            <div className='pt-0.5 px-2'>
-              <span className='text-sm text-gray-600 font-medium'>Menus</span>
-            </div>
-            <Separator className='my-1.5' />
-            <Navbar
-              variant='vertical'
-              links={[
-                {
-                  title: 'Users',
-                  label: accounts?.toString(),
-                  icon: Users2,
-                  path: PATH.ACCOUNT,
-                },
-                {
-                  title: 'Roles',
-                  label: roles?.toString(),
-                  icon: KeyRound,
-                  path: PATH.ROLES,
-                },
-                {
-                  title: 'Permission',
-                  label: groups?.toString(),
-                  icon: Shield,
-                  path: PATH.ROLES_PERMISSION,
-                },
-              ]}
-            />
-          </div>
-          <div className='pb-10'>{children}</div>
-        </div>
+        <div className='pb-10'>{children}</div>
       </div>
-      <div className='fixed top-0 left-0 w-full h-screen bg-common-background-1 -z-10' />
-    </>
+    </DashboardLayout>
   )
 }
