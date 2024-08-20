167257a1f65c8421ccebf4ab34de8cf146dd72c4
mmulyana <mulyan.t20@gmail.com>
2024-08-20 18:56:42 +0700

    feat: new hook useCreatePosition

--- src/utils/api/user-position.ts
+++ src/utils/api/user-position.ts
@@ -1,7 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
+import {
+  fetcherCreatePosition,
+  fetcherPosition,
+} from './fetcher/fetcher-position'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { KEYS } from '../constant/_keys'
-import { fetcherPosition } from './fetcher/fetcher-employee'
 
 export const usePosition = () => {
   return useQuery({ queryKey: [KEYS.HRIS_POSITION], queryFn: fetcherPosition })
+}
+
+export const useCreatePosition = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: fetcherCreatePosition,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [KEYS.HRIS_POSITION],
+      })
+    },
+  })
 }
