26cd72f269d99c10b1341be30c0f13e8e620838a
mmulyana <mulyan.t20@gmail.com>
2024-08-20 07:38:39 +0700

    chore: change routes auth

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -1,49 +1,25 @@
 import { PATH } from '@/utils/constant/_paths.ts'
-import { Route } from 'react-router-dom'
-import { Suspense, lazy } from 'react'
-import LoadingScreen from '@/components/loading-screen/index.tsx'
-import Protected from './_components/protected.tsx'
+import { lazy } from 'react'
 
 const Login = lazy(() => import('./login/index.tsx'))
 const Register = lazy(() => import('./register/index.tsx'))
 const Forgot = lazy(() => import('./forgot/index.tsx'))
 
-const useRoutes = () => {
-  return [
-    {
-      path: PATH.BASE,
-      element: <Login />,
-    },
-    {
-      path: PATH.LOGIN,
-      element: <Login />,
-    },
-    {
-      path: PATH.REGISTER,
-      element: <Register />,
-    },
-    {
-      path: PATH.FORGOT,
-      element: <Forgot />,
-    },
-  ]
-}
-
-export default function AuthRoutes() {
-  const routes = useRoutes()
-  return (
-    <Route>
-      {routes.map((route, index) => (
-        <Route
-          key={index}
-          path={route.path}
-          element={
-            <Suspense fallback={<LoadingScreen />}>
-              <Protected>{route.element}</Protected>
-            </Suspense>
-          }
-        />
-      ))}
-    </Route>
-  )
-}
+export const authRoutes = [
+  {
+    path: PATH.BASE,
+    element: <Login />,
+  },
+  {
+    path: PATH.LOGIN,
+    element: <Login />,
+  },
+  {
+    path: PATH.REGISTER,
+    element: <Register />,
+  },
+  {
+    path: PATH.FORGOT,
+    element: <Forgot />,
+  },
+]
