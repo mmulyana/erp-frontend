575d27c1ab4606762f4499fa5605cd3927fb4040
mmulyana <mulyan.t20@gmail.com>
2024-08-20 07:38:21 +0700

    chore: change routes common

--- src/routes/common/index.tsx
+++ src/routes/common/index.tsx
@@ -1,38 +1,11 @@
 import { PATH } from '@/utils/constant/_paths.ts'
-import { Route } from 'react-router-dom'
-import { Suspense, lazy } from 'react'
-import LoadingScreen from '@/components/loading-screen/index.tsx'
+import { lazy } from 'react'
 
 const NotFound = lazy(() => import('./not-found/index.tsx'))
 
-const useRoutes = () => {
-  return [
-    {
-      path: PATH.NOT_FOUND,
-      element: <NotFound />,
-      show: true,
-    },
-  ]
-}
-
-export default function CommonRoutes() {
-  const routes = useRoutes()
-  return (
-    <>
-      {routes.map(
-        (route, index) =>
-          route.show && (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Suspense fallback={<LoadingScreen />}>
-                  {route.element}
-                </Suspense>
-              }
-            />
-          )
-      )}
-    </>
-  )
-}
+export const commonRoutes = [
+  {
+    path: PATH.NOT_FOUND,
+    element: <NotFound />,
+  },
+]
