430c95ce2f5d0d8c4d4f4ea5d1dd7416c3c6463a
mmulyana <mulyan.t20@gmail.com>
2024-08-20 07:38:07 +0700

    chore: change routes dashboard

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,65 +1,37 @@
 import { PATH } from '@/utils/constant/_paths.ts'
-import { Route } from 'react-router-dom'
-import React, { Suspense, lazy } from 'react'
-import LoadingScreen from '@/components/loading-screen/index.tsx'
-import ProtectedRoute from '@/utils/protected-route.tsx'
+import { lazy } from 'react'
 
-const Overview = lazy(() => import('./overview/index.tsx'))
-const Account = lazy(() => import('./account-and-roles/account/index.tsx'))
-const Roles = lazy(() => import('./account-and-roles/roles/index.tsx'))
 const Permission = lazy(
   () => import('./account-and-roles/permission/index.tsx')
 )
+const Account = lazy(() => import('./account-and-roles/account/index.tsx'))
+const Roles = lazy(() => import('./account-and-roles/roles/index.tsx'))
+const Overview = lazy(() => import('./overview/index.tsx'))
 const Employee = lazy(() => import('./employee/index.tsx'))
 
-const useRoutes = () => {
-  return [
-    {
-      path: PATH.DASHBOARD,
-      element: <Overview />,
-      show: true,
-    },
-    {
-      path: PATH.ACCOUNT,
-      element: <Account />,
-      show: true,
-    },
-    {
-      path: PATH.ROLES,
-      element: <Roles />,
-      show: true,
-    },
-    {
-      path: PATH.ROLES_PERMISSION,
-      element: <Permission />,
-      show: true,
-    },
-    {
-      path: PATH.EMPLOYEE,
-      element: <Employee />,
-      show: true,
-    },
-  ]
-}
-
-export default function DashboardRoutes() {
-  const routes = useRoutes()
-  return (
-    <React.Fragment>
-      {routes.map(
-        (route, index) =>
-          route.show && (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Suspense fallback={<LoadingScreen />}>
-                  <ProtectedRoute>{route.element}</ProtectedRoute>
-                </Suspense>
-              }
-            />
-          )
-      )}
-    </React.Fragment>
-  )
-}
+export const dashboardRoutes = [
+  {
+    path: PATH.DASHBOARD_OVERVIEW,
+    element: <Overview />,
+  },
+  {
+    path: PATH.ACCOUNT,
+    element: <Account />,
+    permission: 'manage_account',
+  },
+  {
+    path: PATH.ROLES,
+    element: <Roles />,
+    permission: 'manage_role',
+  },
+  {
+    path: PATH.ROLES_PERMISSION,
+    element: <Permission />,
+    permission: 'manage_role_permission',
+  },
+  {
+    path: PATH.EMPLOYEE,
+    element: <Employee />,
+    permission: 'manage_employee',
+  },
+]
