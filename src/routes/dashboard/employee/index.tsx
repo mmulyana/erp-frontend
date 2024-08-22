02d76d230648fc0732370f523e3890d08ac9ffe5
mmulyana <mulyan.t20@gmail.com>
2024-08-23 00:41:07 +0700

    chore: update import

--- src/routes/dashboard/employee/index.tsx
+++ src/routes/dashboard/employee/index.tsx
@@ -20,7 +20,7 @@ import {
   useDetailPosition,
   usePosition,
   useUpdatePosition,
-} from '@/utils/api/user-position'
+} from '@/utils/api/use-position'
 import { PATH } from '@/utils/constant/_paths'
 import { useEffect, useState } from 'react'
 import ResponsiveModal from '@/components/responsive-modal.tsx'
