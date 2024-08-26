e9d50dfd6da31345b6830c82d5058f4aecf52195
mmulyana <mulyan.t20@gmail.com>
2024-08-27 00:42:03 +0700

    feat(components): add search

---
+++ src/components/common/search/index.tsx
@@ -0,0 +1,19 @@
+import { Search as SearchIcon } from 'lucide-react'
+import useUrlState from '@ahooksjs/use-url-state'
+import { Input } from '@/components/ui/input'
+
+export default function Search() {
+  const [url, setUrl] = useUrlState({ name: '' })
+
+  return (
+    <div className='relative'>
+      <SearchIcon className='absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground' />
+      <Input
+        value={url.name}
+        onChange={(e) => setUrl({ name: e.target.value })}
+        placeholder='Search'
+        className='pl-8 text-sm h-fit py-2 shadow'
+      />
+    </div>
+  )
+}
