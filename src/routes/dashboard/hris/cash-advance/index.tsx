8c3b091bd3ed2f2e91bc4bd87c6060d7ac7c5e20
mmulyana <mulyan.t20@gmail.com>
2024-08-27 20:14:43 +0700

    feat(cash-advance): update

--- src/routes/dashboard/hris/cash-advance/index.tsx
+++ src/routes/dashboard/hris/cash-advance/index.tsx
@@ -1,6 +1,11 @@
 import { Breadcrumb, Container, DashboardLayout } from '../../component'
 import CardInfo from '@/components/common/card-info'
+import Filter from '@/components/common/filter'
+import Search from '@/components/common/search'
+import { DataTable } from '@/components/data-table'
+import { Button } from '@/components/ui/button'
 import { PATH } from '@/utils/constant/_paths'
+import { columns, data } from './component'
 
 const links = [
   {
@@ -18,7 +23,7 @@ export default function Page() {
     <DashboardLayout>
       <Container>
         <Breadcrumb links={links} />
-        <div className='max-w-[224px] mt-4'>
+        <div className='max-w-[224px] mt-4 mb-6'>
           <CardInfo
             title='Jumlah kasbon bulan ini'
             value='Rp 8,000,000'
@@ -26,6 +31,16 @@ export default function Page() {
             status='down'
           />
         </div>
+        <div className='flex justify-between items-center mb-4'>
+          <div className='flex gap-4'>
+            <div className='max-w-[180px]'>
+              <Search />
+            </div>
+            <Filter />
+          </div>
+          <Button className='h-8'>Tambah data</Button>
+        </div>
+        <DataTable columns={columns} data={data} />
       </Container>
     </DashboardLayout>
   )
