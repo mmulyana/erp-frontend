a69734d1b323a2523dc1334237edf0b4e8bed22f
mmulyana <mulyan.t20@gmail.com>
2024-08-27 21:31:55 +0700

    feat: add pagination

---
+++ src/components/data-table/component.tsx
@@ -0,0 +1,56 @@
+interface PaginationProps {
+  currentPage: number
+  totalPages: number
+  onPageChange: (page: number) => void
+}
+
+export function Pagination({
+  currentPage,
+  totalPages,
+  onPageChange,
+}: PaginationProps) {
+  const pageNumbers = []
+
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i)
+  }
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <button
+            className='page-link'
+            onClick={() => onPageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+        </li>
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={`page-item ${currentPage === number ? 'active' : ''}`}
+          >
+            <button className='page-link' onClick={() => onPageChange(number)}>
+              {number}
+            </button>
+          </li>
+        ))}
+        <li
+          className={`page-item ${
+            currentPage === totalPages ? 'disabled' : ''
+          }`}
+        >
+          <button
+            className='page-link'
+            onClick={() => onPageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
