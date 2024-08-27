c5cb07f6e9c0ef484ce55762aec6018a1d43e2f2
mmulyana <mulyan.t20@gmail.com>
2024-08-27 19:32:32 +0700

    feat: add overtime type

---
+++ src/utils/types/overtime.ts
@@ -0,0 +1,10 @@
+import { Employee } from './employee'
+
+export type Overtime = {
+  id: number
+  employeeId: number
+  date: string
+  total_hour: number
+  description?: string
+  employee: Employee
+}
