161d82fa5af92f8513ce78d8d68ebb7b5330a136
mmulyana <mulyan.t20@gmail.com>
2024-09-02 19:21:23 +0700

    integrate: read data and remove unused local component

--- src/routes/dashboard/hris/employee/detail/component.tsx
+++ src/routes/dashboard/hris/employee/detail/component.tsx
@@ -1,21 +1,15 @@
 import Filter from '@/components/common/filter'
 import Search from '@/components/common/search'
-import Modal from '@/components/modal-v2'
 import { Button } from '@/components/ui/button'
 import { useEmployees } from '@/hooks/use-employee'
 import { EmployeeStatus } from '@/utils/enum/common'
 import { Employee } from '@/utils/types/employee'
 import useUrlState from '@ahooksjs/use-url-state'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { differenceInYears, parse } from 'date-fns'
 import { ColumnDef } from '@tanstack/react-table'
-import { Ellipsis, UserRoundPlus } from 'lucide-react'
-import { useMemo, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { Ellipsis } from 'lucide-react'
+import { useMemo } from 'react'
 import { isString } from 'lodash'
-import { z } from 'zod'
-import { Form, FormField } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -32,7 +26,7 @@ type TableEmployeeProps = {
 }
 export function TableEmployee({ status, positionId }: TableEmployeeProps) {
   const [url] = useUrlState({ name: '', page: '' })
-  const { isLoading } = useEmployees({
+  const { isLoading, data } = useEmployees({
     ...(!!status ? { status } : undefined),
     ...(isString(url.name) ? { name: url.name } : undefined),
     ...(!!positionId ? { positionId } : undefined),
@@ -53,65 +47,22 @@ export function TableEmployee({ status, positionId }: TableEmployeeProps) {
         </div>
         <Button
           className='h-8'
-          onClick={() => navigate(`${PATH.EMPLOYEE_ADD}?path=${pathname}`)}
+          onClick={() =>
+            navigate(
+              `${PATH.EMPLOYEE_ADD}?path=${pathname}&positionId=${positionId}`
+            )
+          }
         >
           Tambah pegawai
         </Button>
       </div>
       <DataTable
         columns={columns}
-        data={dummyEmployees}
+        data={data?.data?.data || []}
         withPagination
         withLoading
         isLoading={isLoading}
       />
-    </>
-  )
-}
-
-function ModalAdd() {
-  const [open, setOpen] = useState(false)
-
-  const form = useForm<EmployeeSchema>({
-    resolver: zodResolver(employeeSchema),
-    defaultValues: {
-      fullname: '',
-      birthdate: '',
-    },
-  })
-
-  const submit = async (data: EmployeeSchema) => {
-    console.log(data)
-  }
-
-  return (
-    <>
-      <Button onClick={() => setOpen(!open)} className='h-8'>
-        Tambah Pegawai
-      </Button>
-      <Modal
-        open={open}
-        setOpen={setOpen}
-        title='Tambah Pegawai Baru'
-        subtitle='Silahkan masukkan data yang dibutuhkan'
-        icon={
-          <div className='h-8 w-8 rounded-[8px] bg-[#5463E8] border border-[#4752BE] flex items-center justify-center shadow-md shadow-[#5463E8]/50'>
-            <UserRoundPlus className='w-5 h-5 text-white' />
-          </div>
-        }
-      >
-        <div className='pb-8 h-[400px]'>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(submit)}>
-              <FormField
-                name='fullname'
-                label='Nama lengkap'
-                render={({ field }) => <Input {...field} />}
-              />
-            </form>
-          </Form>
-        </div>
-      </Modal>
     </>
   )
 }
@@ -194,86 +145,5 @@ export const columns: ColumnDef<Employee>[] = [
         </div>
       )
     },
-  },
-]
-
-const employeeSchema = z.object({
-  fullname: z.string(),
-  nickname: z.string().optional(),
-  hireDate: z.string().optional(),
-  salary_per_month: z.number().optional(),
-  salary_per_day: z.number().optional(),
-  salary_per_hour: z.number().optional(),
-  status: z.enum(['active', 'nonactive']),
-  place_of_birth: z.string().optional(),
-  birthdate: z.string().optional(),
-  gender: z.enum(['male', 'female']).optional(),
-  marital_status: z
-    .enum(['single', 'married', 'divorced', 'widowed'])
-    .optional(),
-  nik: z.string().optional(),
-  kk: z.string().optional(),
-  religion: z.string().optional(),
-  positionId: z.number().optional(),
-})
-
-type EmployeeSchema = z.infer<typeof employeeSchema>
-
-const dummyEmployees: Employee[] = [
-  {
-    id: 1,
-    fullname: 'Alice Johnson',
-    nickname: 'Ali',
-    hireDate: new Date('2022-03-01'),
-    salary_per_month: 5000,
-    salary_per_day: 250,
-    salary_per_hour: 31.25,
-    status: EmployeeStatus.active,
-    place_of_birth: 'New York',
-    birthdate: '1990-05-15',
-    nik: '1234567890',
-    kk: '0987654321',
-    religion: 'Christian',
-    positionId: 1,
-    position: {
-      id: 1,
-      name: 'Software Developer',
-      description: 'Develops software applications',
-    },
-    attendances: [],
-    cashAdvances: [],
-    leaves: [],
-    contact: [],
-    address: [],
-    statusTracks: [],
-    employeeCompetency: [],
-  },
-  {
-    id: 2,
-    fullname: 'Bob Williams',
-    nickname: 'Bobby',
-    hireDate: new Date('2021-11-15'),
-    salary_per_month: 6000,
-    salary_per_day: 300,
-    salary_per_hour: 37.5,
-    status: EmployeeStatus.active,
-    place_of_birth: 'Los Angeles',
-    birthdate: '1988-09-22',
-    nik: '9876543210',
-    kk: '0123456789',
-    religion: 'Muslim',
-    positionId: 2,
-    position: {
-      id: 2,
-      name: 'Project Manager',
-      description: 'Manages software projects',
-    },
-    attendances: [],
-    cashAdvances: [],
-    leaves: [],
-    contact: [],
-    address: [],
-    statusTracks: [],
-    employeeCompetency: [],
   },
 ]
