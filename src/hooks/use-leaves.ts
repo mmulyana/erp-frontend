1d295a1d20d054003dc7f6246652ebb9133643e0
mmulyana <mulyan.t20@gmail.com>
2024-09-03 15:37:47 +0700

    feat: useLeaves

---
+++ src/hooks/use-leaves.ts
@@ -0,0 +1,37 @@
+import { KEYS } from '@/utils/constant/_keys'
+import { URLS } from '@/utils/constant/_urls'
+import http from '@/utils/http'
+import { Pagination } from '@/utils/types/common'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+type ParamsLeaves = Pagination & {
+  name?: string
+  date?: string
+}
+export const useLeaves = (params: ParamsLeaves) => {
+  return useQuery({
+    queryKey: [KEYS.LEAVES, params],
+    queryFn: async () => {
+      return await http.request({
+        method: 'GET',
+        url: URLS.LEAVES,
+        params,
+      })
+    },
+  })
+}
+
+export const useCreateLeaves = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (data: any) => {
+      return await http.post(URLS.LEAVES, data)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.LEAVES] })
+      toast.success(data.data.message)
+    },
+  })
+}
