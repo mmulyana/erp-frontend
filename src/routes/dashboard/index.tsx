51c555d74fa435aca8277ab1cd1375087d1a7eb0
mmulyana <mulyan.t20@gmail.com>
2024-09-16 18:49:04 +0700

    feat: new path

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -12,6 +12,7 @@ const Attendance = lazy(() => import('./hris/attendance/index.tsx'))
 const AttendanceSummary = lazy(() => import('./hris/attendance/summary/index.tsx'))
 const CashAdvance = lazy(() => import('./hris/cash-advance/index.tsx'))
 const Leave = lazy(() => import('./hris/leave/index.tsx'))
+const Project = lazy(() => import('./project/index.tsx'))
 
 export const dashboardRoutes = [
   {
@@ -57,5 +58,9 @@ export const dashboardRoutes = [
   {
     path: PATH.EMPLOYEE_PAID_LEAVE,
     element: <Leave />
+  },
+  {
+    path: PATH.PROJECT_INDEX,
+    element: <Project />
   }
 ]
