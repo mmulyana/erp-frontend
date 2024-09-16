43e8fbddee3f7979f52d8fd08b9df1415aa3b5cc
mmulyana <mulyan.t20@gmail.com>
2024-09-16 18:48:53 +0700

    feat(hooks): add user-project

---
+++ src/hooks/use-project.ts
@@ -0,0 +1,23 @@
+import { KEYS } from '@/utils/constant/_keys'
+import { URLS } from '@/utils/constant/_urls'
+import http from '@/utils/http'
+import { useQuery } from '@tanstack/react-query'
+
+type Params = {
+  id?: string
+  search?: string
+  labelId?: number
+  clientId?: number
+}
+export const useProject = (params: Params) => {
+  return useQuery({
+    queryKey: [KEYS.PROJECT, params],
+    queryFn: async () => {
+      return await http.request({
+        method: 'GET',
+        url: URLS.PROJECT,
+        params,
+      })
+    },
+  })
+}
