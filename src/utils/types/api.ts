3b2cffea42a3f281b11873910ca0e793b4f54ce5
mmulyana <mulyan.t20@gmail.com>
2024-09-18 00:19:28 +0700

    feat: new type api

--- src/utils/types/api.ts
+++ src/utils/types/api.ts
@@ -1,40 +1,67 @@
-export type Projects = {
+export interface Board {
+  id: string
+  name: string
+  color: string
+  position: number
+  items: Item[]
+}
+
+export interface Project {
   id: number
   name: string
-  startDate?: Date
-  budget?: number
-  priority: 'low' | 'medium' | 'high' | 'urgent'
+  startDate: null
+  budget: string
+  priority: string
   boardItemsId: string
   clientId: number
-  boardItems: {
-    id: string
-    position: number
-    containerId: string
-  }
-  labels: Array<{
-    label: {
-      id: number
-      name: string
-      color: string
-    }
-  }>
-  employees: {
-    employee: {
-      fullname: string
-      photo?: string
-    }
-  }[]
-  comments: {
-    comment: string
-  }[]
-  client: {
-    name: string
-    company: {
-      logo: string
-    }
-  }
-  _count: {
-    employees: number
-    comments: number
-  }
+  boardItems: Item
+  labels: LabelElement[]
+  employees: EmployeeElement[]
+  comments: Comment[]
+  client: Client
+  _count: Count
+}
+
+export interface Item {
+  id: string
+  position: number
+  containerId: string
+  project?: Project
+}
+
+export interface Count {
+  employees: number
+  comments: number
+}
+
+export interface Client {
+  name: string
+  company: Company
+}
+
+export interface Company {
+  logo: string
+}
+
+export interface Comment {
+  comment: string
+}
+
+export interface EmployeeElement {
+  employee: EmployeeEmployee
+}
+
+export interface EmployeeEmployee {
+  fullname: string
+  photo?: string
+}
+
+export interface LabelElement {
+  label: LabelLabel
+}
+
+export interface LabelLabel {
+  id: number
+  name: string
+  color: string
 }
