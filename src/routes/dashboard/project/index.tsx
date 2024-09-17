f858fe9a7648c0f35daa32e0b19b06c9cb04dae1
mmulyana <mulyan.t20@gmail.com>
2024-09-18 00:13:46 +0700

    fix: change type import

--- src/routes/dashboard/project/index.tsx
+++ src/routes/dashboard/project/index.tsx
@@ -1,7 +1,7 @@
 import { useProject } from '@/hooks/use-project'
 import { Container, DashboardLayout, useTitle } from '../component'
 import CardProject from '@/components/card-project'
-import { Projects } from '@/utils/types/api'
+import { Project } from '@/utils/types/api'
 
 export default function Dashboard() {
   useTitle('Project')
@@ -12,7 +12,7 @@ export default function Dashboard() {
     <DashboardLayout>
       <Container>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-          {data?.data.data.map((project: Projects) => (
+          {data?.data.data.map((project: Project) => (
             <CardProject key={project.id} {...project} />
           ))}
         </div>
