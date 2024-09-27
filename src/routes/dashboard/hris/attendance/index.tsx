b7b0de31a2dbfb18a9abf2e310e1cae402098606
mmulyana <mulyan.t20@gmail.com>
2024-09-27 22:33:49 +0700

    fix(attendance): page

--- src/routes/dashboard/hris/attendance/index.tsx
+++ src/routes/dashboard/hris/attendance/index.tsx
@@ -1,26 +1,27 @@
 import { PATH } from '@/utils/constant/_paths'
-import { Breadcrumb, Container, DashboardLayout, useTitle } from '../../component'
 import { Tab, Tabs } from '@/components/tab'
 import { Header, Overtime, Regular } from './component'
+import { DashboardLayout } from '../../component'
+import { useTitle } from '../../_component/header'
+import Container from '../../_component/container'
 
 const links = [
   {
     name: 'Dashboard',
-    href: PATH.DASHBOARD_OVERVIEW,
+    path: PATH.DASHBOARD_OVERVIEW,
   },
   {
     name: 'Absensi',
-    href: PATH.EMPLOYEE_ATTENDANCE,
+    path: PATH.EMPLOYEE_ATTENDANCE,
   },
 ]
 
 export default function Page() {
-  useTitle('Absensi')
+  useTitle(links)
 
   return (
     <DashboardLayout>
       <Container>
-        <Breadcrumb links={links} />
         <Header />
       </Container>
       <Tabs>
