cf16fefecad4c2ac77777479f5b7f54a90945347
mmulyana <mulyan.t20@gmail.com>
2024-09-29 09:55:06 +0700

    feat(inventory-setting): page

---
+++ src/routes/dashboard/inventory/setting.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from '../_component/layout'
+
+export default function Settings() {
+  return (
+    <DashboardLayout>
+      <p>Setting</p>
+    </DashboardLayout>
+  )
+}
