a382bced8579f71f8a91dba5e0db68deb288c760
mmulyana <mulyan.t20@gmail.com>
2024-09-29 09:54:46 +0700

    feat(inventory-index): card activity

---
+++ src/routes/dashboard/inventory/_component/card-activity.tsx
@@ -0,0 +1,44 @@
+type Props = {
+  type: string
+  isLast?: boolean
+}
+export default function CardActivity({ type, isLast }: Props) {
+  return (
+    <div className='px-4 grid grid-cols-[40px_1fr] gap-4'>
+      <div className='relative'>
+        <div className='w-full h-10 rounded-full bg-white border border-[#EFF0F2] flex items-center justify-center'></div>
+        {!isLast && (
+          <div className='h-[calc(100%-40px)] w-[1px] bg-[#EFF0F2] absolute left-1/2 -translate-x-1/2 -z-10' />
+        )}
+      </div>
+      <div className='relative flex flex-col gap-2 mb-4'>
+        <TransactionType type={type} />
+        <p className='text-sm text-[#313951]'>
+          Produk 1 kini tersedia, masuk stok sebanyak 10 pcs!
+        </p>
+        <p className='text-sm text-[#313951]/50'>10 Februari 2024</p>
+      </div>
+    </div>
+  )
+}
+
+function TransactionType({ type }: Pick<Props, 'type'>) {
+  if (type == 'in') {
+    return (
+      <div className='flex items-center gap-1.5'>
+        <div className='w-1 h-1 rounded-full bg-[#5FC985]'></div>
+        <p className='text-xs text-[#5FC985]'>Barang masuk</p>
+      </div>
+    )
+  }
+  if (type == 'out') {
+    return (
+      <div className='flex items-center gap-1.5'>
+        <div className='w-1 h-1 rounded-full bg-[#C95F61]'></div>
+        <p className='text-xs text-[#C95F61]'>Barang keluar</p>
+      </div>
+    )
+  }
+
+  return null
+}
