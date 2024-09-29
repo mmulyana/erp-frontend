59d2b295e380a2c90cc1c4382eea71b6e775c053
mmulyana <mulyan.t20@gmail.com>
2024-09-29 09:55:37 +0700

    feat(inventory-stockOut): page

---
+++ src/routes/dashboard/inventory/manage/stock-out.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from '../../_component/layout'
+
+export default function StockOut() {
+  return (
+    <DashboardLayout>
+      <p>Stock Out</p>
+    </DashboardLayout>
+  )
+}
