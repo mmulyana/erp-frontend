44d87103391d56c32aec81850d20ff11371bb540
mmulyana <mulyan.t20@gmail.com>
2024-09-29 14:45:06 +0700

    feat(common): overlay

---
+++ src/components/common/overlay.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/utils/cn'
+import { useState } from 'react'
+
+type Props = {
+  children: React.ReactNode
+  overlay?: React.ReactNode
+  className?: string
+}
+export default function Overlay({ children, overlay, className }: Props) {
+  const [isShow, setIsSHow] = useState(false)
+
+  return (
+    <div
+      className={cn('relative', className)}
+      onMouseEnter={() => setIsSHow(true)}
+      onMouseLeave={() => setIsSHow(false)}
+    >
+      {children}
+      {isShow && overlay}
+    </div>
+  )
+}
