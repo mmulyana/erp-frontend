bd270b59cb0933f864e860d3e0f06f36b6f03e52
mmulyana <mulyan.t20@gmail.com>
2024-09-29 09:55:16 +0700

    feat(inventory-supplier): page

---
+++ src/routes/dashboard/inventory/supplier.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from '../_component/layout'
+
+export default function Supplier() {
+  return (
+    <DashboardLayout>
+      <p>Supplier</p>
+    </DashboardLayout>
+  )
+}
