3f884b59841196b3f0814030705e1f501632f417
mmulyana <mulyan.t20@gmail.com>
2024-09-29 15:16:38 +0700

    feat(supplier): add data

---
+++ src/routes/dashboard/inventory/supplier/data.ts
@@ -0,0 +1,12 @@
+import { PATH } from '@/utils/constant/_paths'
+
+export const links = [
+  {
+    name: 'Inventory',
+    path: PATH.INVENTORY_INDEX,
+  },
+  {
+    name: 'Supplier',
+    path: PATH.INVENTORY_SUPPLIER,
+  },
+]
