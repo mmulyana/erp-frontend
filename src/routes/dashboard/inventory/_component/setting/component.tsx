5bd81c83f0089cb84f1550b35028062df9d18f47
mmulyana <mulyan.t20@gmail.com>
2024-09-30 12:58:25 +0700

    feat(inventory): add DataLocation

--- src/routes/dashboard/inventory/_component/setting/component.tsx
+++ src/routes/dashboard/inventory/_component/setting/component.tsx
@@ -1,8 +1,9 @@
 import { DataTable } from '@/components/data-table'
 import { useBrand } from '@/hooks/api/use-brand'
 import { useMemo } from 'react'
-import { columnsBrand, columnsCategory } from './columns'
+import { columnsBrand, columnsCategory, columnsLocation } from './columns'
 import { useCategory } from '@/hooks/api/use-category'
+import { useLocation } from '@/hooks/api/use-location'
 
 export function DataBrand() {
   const { data: brands, isLoading } = useBrand({})
@@ -29,6 +30,23 @@ export function DataCategory() {
     <>
       <DataTable
         columns={columnsCategory}
+        data={data || []}
+        isLoading={isLoading}
+        withLoading
+        withPagination
+      />
+    </>
+  )
+}
+
+export function DataLocation() {
+  const { data: location, isLoading } = useLocation({})
+  const data = useMemo(() => location?.data?.data, [isLoading])
+
+  return (
+    <>
+      <DataTable
+        columns={columnsLocation}
         data={data || []}
         isLoading={isLoading}
         withLoading
