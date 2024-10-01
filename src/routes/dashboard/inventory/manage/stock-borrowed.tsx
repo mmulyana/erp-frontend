00e2725fd900bbb14bf303bb05e51b062fd5e0c4
mmulyana <mulyan.t20@gmail.com>
2024-10-01 14:57:42 +0700

    feat(inventory-manage): integrate borrowed

--- src/routes/dashboard/inventory/manage/stock-borrowed.tsx
+++ src/routes/dashboard/inventory/manage/stock-borrowed.tsx
@@ -1,9 +1,24 @@
+import { useTransaction } from '@/hooks/api/use-transaction'
 import { DashboardLayout } from '../../_component/layout'
+import { useMemo } from 'react'
+import { column } from './_component/column'
+import { DataTable } from '@/components/data-table'
+import Container from '../../_component/container'
 
 export default function StockBorrowed() {
+  const { data: transaction, isLoading } = useTransaction({ type: 'borrowed' })
+  const data = useMemo(() => transaction?.data?.data, [isLoading])
   return (
     <DashboardLayout>
-      <p>Stock Out</p>
+      <Container>
+        <DataTable
+          columns={column}
+          data={data || []}
+          isLoading={isLoading}
+          withLoading
+          withPagination
+        />
+      </Container>
     </DashboardLayout>
   )
 }
