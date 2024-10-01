422afb132f5c29c2b4be6f3a7ce54740e42bf7b0
mmulyana <mulyan.t20@gmail.com>
2024-10-01 14:32:53 +0700

    feat(inventory-manage): page borrowed

---
+++ src/routes/dashboard/inventory/manage/stock-borrowed.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from '../../_component/layout'
+
+export default function StockBorrowed() {
+  return (
+    <DashboardLayout>
+      <p>Stock Out</p>
+    </DashboardLayout>
+  )
+}
