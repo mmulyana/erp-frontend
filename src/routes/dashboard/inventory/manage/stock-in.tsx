b3e60251859fbac91e3160b527924e1160848241
mmulyana <mulyan.t20@gmail.com>
2024-10-01 14:32:37 +0700

    feat(inventory-manage): integrate page

--- src/routes/dashboard/inventory/manage/stock-in.tsx
+++ src/routes/dashboard/inventory/manage/stock-in.tsx
@@ -1,9 +1,25 @@
+import { useTransaction } from '@/hooks/api/use-transaction'
 import { DashboardLayout } from '../../_component/layout'
+import { useMemo } from 'react'
+import Container from '../../_component/container'
+import { DataTable } from '@/components/data-table'
+import { column } from './_component/column'
 
 export default function StockIn() {
+  const { data: transaction, isLoading } = useTransaction({ type: 'in' })
+  const data = useMemo(() => transaction?.data?.data, [isLoading])
+
   return (
     <DashboardLayout>
-      <p>Stock In</p>
+      <Container>
+        <DataTable
+          columns={column}
+          data={data || []}
+          isLoading={isLoading}
+          withLoading
+          withPagination
+        />
+      </Container>
     </DashboardLayout>
   )
 }
