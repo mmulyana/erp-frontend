62f095beeaf9666ae851e712f35d64cb4be0f1e4
mmulyana <mulyan.t20@gmail.com>
2024-10-01 14:57:54 +0700

    feat(inventory-manage): integrate stock out

--- src/routes/dashboard/inventory/manage/stock-out.tsx
+++ src/routes/dashboard/inventory/manage/stock-out.tsx
@@ -1,9 +1,25 @@
+import { useTransaction } from '@/hooks/api/use-transaction'
 import { DashboardLayout } from '../../_component/layout'
+import { useMemo } from 'react'
+import Container from '../../_component/container'
+import { DataTable } from '@/components/data-table'
+import { column } from './_component/column'
 
 export default function StockOut() {
+  const { data: transaction, isLoading } = useTransaction({ type: 'out' })
+  const data = useMemo(() => transaction?.data?.data, [isLoading])
+
   return (
     <DashboardLayout>
-      <p>Stock Out</p>
+      <Container>
+        <DataTable
+          columns={column}
+          data={data || []}
+          isLoading={isLoading}
+          withLoading
+          withPagination
+        />
+      </Container>
     </DashboardLayout>
   )
 }
