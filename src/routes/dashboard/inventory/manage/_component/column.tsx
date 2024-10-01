65947321ecd56202f0d6d8a915c06bc76fa62eee
mmulyana <mulyan.t20@gmail.com>
2024-10-01 14:32:09 +0700

    feat(inventory-manage): add columns

---
+++ src/routes/dashboard/inventory/manage/_component/column.tsx
@@ -0,0 +1,80 @@
+import { generatePath, Link } from 'react-router-dom'
+import Overlay from '@/components/common/overlay'
+import { ColumnDef } from '@tanstack/react-table'
+import { Transaction } from '@/utils/types/api'
+import { PATH } from '@/utils/constant/_paths'
+import { Button } from '@/components/ui/button'
+import { Ellipsis } from 'lucide-react'
+
+export const column: ColumnDef<Transaction>[] = [
+  {
+    id: 'name',
+    header: 'Nama',
+    cell: ({ row }) => {
+      const detail = row.original.good.name
+      return (
+        <Overlay
+          className='w-fit pr-2.5'
+          overlay={
+            <Link
+              to={generatePath(PATH.INVENTORY_SUPPLIER_EMPLOYEE, {
+                detail: `${detail}-${row.original.id}`,
+              })}
+              className='absolute -right-0 translate-x-full text-sm text-[#313951] py-1 px-2 rounded-[6px] border border-[#EFF0F2] top-1/2 -translate-y-1/2 bg-white hover:shadow-sm hover:shadow-gray-200'
+            >
+              Lihat
+            </Link>
+          }
+        >
+          <Link
+            to={generatePath(PATH.INVENTORY_SUPPLIER_EMPLOYEE, {
+              detail: `${detail}-${row.original.id}`,
+            })}
+          >
+            {detail}
+          </Link>
+        </Overlay>
+      )
+    },
+  },
+  {
+    id: 'measurement',
+    header: 'Satuan',
+    cell: ({ row }) => <p>{row.original.good.measurement?.name}</p>,
+  },
+  {
+    accessorKey: 'qty',
+    header: 'Qty',
+  },
+  {
+    accessorKey: 'price',
+    header: 'Harga',
+  },
+  {
+    id: 'brand',
+    header: 'Merek',
+    cell: ({ row }) => <p>{row.original.good.brand?.name}</p>,
+  },
+  {
+    id: 'supplier',
+    header: 'Toko',
+    cell: ({ row }) => <p>{row.original.supplier.name}</p>,
+  },
+  {
+    accessorKey: 'date',
+    header: 'Tanggal',
+  },
+  {
+    id: 'action',
+    cell: () => (
+      <div className='flex justify-end'>
+        <Button
+          variant='outline'
+          className='p-0.5 rounded-[6px] h-5 w-5 border-[#EFF0F2]'
+        >
+          <Ellipsis className='w-4 h-4 text-[#313951]' />
+        </Button>
+      </div>
+    ),
+  },
+]
