d134da20955ceee81d4d1fb5217c4abfcb406afb
mmulyana <mulyan.t20@gmail.com>
2024-10-01 14:33:04 +0700

    feat(inventory-manage): page opname

---
+++ src/routes/dashboard/inventory/manage/stock-opname.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from '../../_component/layout'
+
+export default function StockOpname() {
+  return (
+    <DashboardLayout>
+      <p>Stock Out</p>
+    </DashboardLayout>
+  )
+}
