156baf5e4ae614bd1b446b7ec62c8479f5db22af
mmulyana <mulyan.t20@gmail.com>
2024-10-02 19:17:26 +0700

    feat(hooks): update useTransaction

--- src/hooks/api/use-transaction.ts
+++ src/hooks/api/use-transaction.ts
@@ -3,14 +3,24 @@ import { URLS } from '@/utils/constant/_urls'
 import { Pagination } from '@/utils/types/common'
 import { useQuery } from '@tanstack/react-query'
 import http from '@/utils/http'
+import { Transaction } from '@/utils/types/api'
+import { AxiosResponse } from 'axios'
 
 type Params = Pagination & {
-  type: 'in' | 'out' | 'borrowed' | 'returned' | 'opname'
+  type?: 'in' | 'out' | 'borrowed' | 'returned' | 'opname'
 }
 export const useTransaction = (params: Params) => {
   return useQuery({
-    queryFn: async () => {
-      return await http(URLS.INVENTORY_TRANSACTION + '?type=' + params.type)
+    queryFn: async (): Promise<
+      AxiosResponse<{
+        data: Transaction[]
+      }>
+    > => {
+      return await http.request({
+        method: 'GET',
+        url: URLS.INVENTORY_TRANSACTION,
+        params,
+      })
     },
     queryKey: [KEYS.TRANSACTION, params.type, params],
   })
