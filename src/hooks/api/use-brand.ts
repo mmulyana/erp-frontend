c67806bea87417acc5eaa5428b63429f6dc4cd51
mmulyana <mulyan.t20@gmail.com>
2024-10-02 08:59:23 +0700

    feat(hooks): add useCreateBrand

--- src/hooks/api/use-brand.ts
+++ src/hooks/api/use-brand.ts
@@ -2,7 +2,7 @@ import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
 import { Pagination } from '@/utils/types/common'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 type brandParams = Pagination & {}
 export const useBrand = (params: brandParams) => {
@@ -11,5 +11,34 @@ export const useBrand = (params: brandParams) => {
       return await http(URLS.INVENTORY_BRAND)
     },
     queryKey: [KEYS.BRAND, params],
+  })
+}
+export const useCreateBrand = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      payload,
+    }: {
+      payload: {
+        name: string
+        photo: File | null
+      }
+    }) => {
+      const formData = new FormData()
+      formData.append('name', payload.name)
+      if (payload.photo) {
+        formData.append('photo', payload.photo)
+      }
+
+      return await http.post(URLS.INVENTORY_BRAND, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.BRAND] })
+    },
   })
 }
