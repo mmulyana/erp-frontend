8b91b9844e0f41517e91587ae49fbbc83e53c3a9
mmulyana <mulyan.t20@gmail.com>
2024-10-02 20:08:46 +0700

    feat: formatCurrency

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,8 @@
+export function formatToRupiah(number: number) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(number)
+}
