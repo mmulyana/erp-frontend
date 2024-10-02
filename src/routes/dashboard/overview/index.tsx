df66dbdf4bfcdd8404ce70467694d36ae25b26ec
mmulyana <mulyan.t20@gmail.com>
2024-10-02 19:18:57 +0700

    fix: import

--- src/routes/dashboard/overview/index.tsx
+++ src/routes/dashboard/overview/index.tsx
@@ -1,6 +1,6 @@
 import { PATH } from '@/utils/constant/_paths'
 import { useTitle } from '../_component/header'
-import { DashboardLayout } from '../component'
+import { DashboardLayout } from '../_component/layout'
 
 export default function Dashboard() {
   useTitle([{ name: 'Dashboard', path: PATH.DASHBOARD_OVERVIEW }])
