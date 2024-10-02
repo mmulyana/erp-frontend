5d2b11060b4f6658a0e0d0d968feb79e70c110d0
mmulyana <mulyan.t20@gmail.com>
2024-10-02 08:59:30 +0700

    feat(hooks): add useCreateCategory

--- src/hooks/api/use-category.ts
+++ src/hooks/api/use-category.ts
@@ -2,7 +2,7 @@ import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
 import { Pagination } from '@/utils/types/common'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 type Params = Pagination & {}
 export const useCategory = (params: Params) => {
@@ -11,5 +11,24 @@ export const useCategory = (params: Params) => {
       return await http(URLS.INVENTORY_CATEGORY)
     },
     queryKey: [KEYS.CATEGORY, params],
+  })
+}
+
+export const useCreateCategory = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      payload,
+    }: {
+      payload: {
+        name: string
+      }
+    }) => {
+      return await http.post(URLS.INVENTORY_CATEGORY, payload)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.CATEGORY] })
+    },
   })
 }
