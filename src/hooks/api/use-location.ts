f0d4e1be85fd979faeb90b0605d4a6a612d6a95f
mmulyana <mulyan.t20@gmail.com>
2024-10-02 12:03:44 +0700

    feat(hooks): add useCreateLocation

--- src/hooks/api/use-location.ts
+++ src/hooks/api/use-location.ts
@@ -2,7 +2,8 @@ import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
 import { Pagination } from '@/utils/types/common'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
 type Params = Pagination & {}
 export const useLocation = (params: Params) => {
@@ -11,5 +12,25 @@ export const useLocation = (params: Params) => {
       return await http(URLS.INVENTORY_LOCATION)
     },
     queryKey: [KEYS.LOCATION, params],
+  })
+}
+
+export const useCreateLocation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      payload,
+    }: {
+      payload: {
+        name: string
+      }
+    }) => {
+      return await http.post(URLS.INVENTORY_LOCATION, payload)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.LOCATION] })
+      toast.success(data.data.message)
+    },
   })
 }
