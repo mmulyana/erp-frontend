f05a4548c1e2ce43085e07598317f3f050f2ef40
mmulyana <mulyan.t20@gmail.com>
2024-10-02 12:13:10 +0700

    feat(hooks): add useCreateTag

--- src/hooks/api/use-tag.ts
+++ src/hooks/api/use-tag.ts
@@ -1,8 +1,9 @@
 import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import { Pagination } from '@/utils/types/common'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import http from '@/utils/http'
+import { toast } from 'sonner'
 
 type Params = Pagination & {}
 export const useTag = (params: Params) => {
@@ -11,5 +12,26 @@ export const useTag = (params: Params) => {
       return await http(URLS.INVENTORY_SUPPLIER_TAG)
     },
     queryKey: [KEYS.TAG, params],
+  })
+}
+
+export const useCreateTag = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      payload,
+    }: {
+      payload: {
+        name: string
+        color: string
+      }
+    }) => {
+      return await http.post(URLS.INVENTORY_SUPPLIER_TAG, payload)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.TAG] })
+      toast.success(data.data.message)
+    },
   })
 }
