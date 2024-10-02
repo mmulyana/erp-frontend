34cefaaf68d49c607e31c8e411330a7030cda091
mmulyana <mulyan.t20@gmail.com>
2024-10-02 09:00:48 +0700

    feat(inventory-setting): add dialog form

--- src/routes/dashboard/inventory/setting.tsx
+++ src/routes/dashboard/inventory/setting.tsx
@@ -10,16 +10,40 @@ import {
   DataMeasurement,
   DataTag,
 } from './_component/setting/component'
+import { useEffect, useState } from 'react'
+import { FormBrand, FormCategory } from './_component/setting/form'
+import Modal from '@/components/modal-v2'
 
 export default function Settings() {
+  const [active, setActive] = useState(0)
+  const [open, setOpen] = useState(false)
+
+  const form: any = {
+    0: <FormBrand onClose={setOpen} />,
+    1: <FormCategory onClose={setOpen} />,
+  }
+
+  useEffect(() => {
+    setOpen(false)
+
+    return () => setOpen(false)
+  }, [active])
+
   return (
     <DashboardLayout>
       <TabsV3
         className='pt-4'
+        onActiveChange={setActive}
         renderAction={() => (
           <div className='flex gap-2 items-center justify-end'>
             <SearchV2 />
-            <Button>Tambah</Button>
+            <Button
+              onClick={() => {
+                setOpen(true)
+              }}
+            >
+              Tambah
+            </Button>
           </div>
         )}
       >
@@ -49,6 +73,9 @@ export default function Settings() {
           </Container>
         </TabV3>
       </TabsV3>
+      <Modal open={open} setOpen={setOpen} title='Buat baru'>
+        {form[active]}
+      </Modal>
     </DashboardLayout>
   )
 }
