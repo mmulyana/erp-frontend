1df12360153b627057726628bdb3832cdbc7bb2a
mmulyana <mulyan.t20@gmail.com>
2024-10-02 12:08:22 +0700

    feat(hooks): add useCreateMeasurement

--- src/hooks/api/use-measurement.ts
+++ src/hooks/api/use-measurement.ts
@@ -2,7 +2,8 @@ import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
 import { Pagination } from '@/utils/types/common'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
 type Params = Pagination & {}
 export const useMeasurement = (params: Params) => {
@@ -11,5 +12,25 @@ export const useMeasurement = (params: Params) => {
       return await http(URLS.INVENTORY_MEASUREMENT)
     },
     queryKey: [KEYS.MEASUREMENT, params],
+  })
+}
+
+export const useCreateMeasurement = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      payload,
+    }: {
+      payload: {
+        name: string
+      }
+    }) => {
+      return await http.post(URLS.INVENTORY_MEASUREMENT, payload)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.MEASUREMENT] })
+      toast.success(data.data.message)
+    },
   })
 }
