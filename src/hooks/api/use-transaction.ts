ac0ebe076c5fe686393602992bc26f1af3d215c5
mmulyana <mulyan.t20@gmail.com>
2024-10-03 09:19:33 +0700

    feat(hooks): add useCreateTransaction

--- src/hooks/api/use-transaction.ts
+++ src/hooks/api/use-transaction.ts
@@ -1,10 +1,13 @@
 import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import { Pagination } from '@/utils/types/common'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import http from '@/utils/http'
 import { Transaction } from '@/utils/types/api'
 import { AxiosResponse } from 'axios'
+import { objectToFormData } from '@/utils/ObjectToFormData'
+import { CreateTransaction } from '@/utils/types/form'
+import { toast } from 'sonner'
 
 type Params = Pagination & {
   type?: 'in' | 'out' | 'borrowed' | 'returned' | 'opname'
@@ -23,5 +26,24 @@ export const useTransaction = (params: Params) => {
       })
     },
     queryKey: [KEYS.TRANSACTION, params.type, params],
+  })
+}
+
+export const useCreateTransaction = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({ payload }: { payload: CreateTransaction }) => {
+      const formData = objectToFormData(payload)
+      return await http.post(URLS.INVENTORY_TRANSACTION, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.SUPPLIER] })
+      toast.success(data.data.message)
+    },
   })
 }
