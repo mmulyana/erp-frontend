1fd25c250c63d22b394a3642e48348033421033a
mmulyana <mulyan.t20@gmail.com>
2024-10-11 16:45:32 +0700

    refactor: links

---
+++ src/routes/dashboard/hris/employee/_component/links.ts
@@ -0,0 +1,12 @@
+import { PATH } from '@/utils/constant/_paths'
+
+export const links = [
+  {
+    name: 'Dashboard',
+    path: PATH.DASHBOARD_OVERVIEW,
+  },
+  {
+    name: 'Pegawai',
+    path: PATH.EMPLOYEE,
+  },
+]
