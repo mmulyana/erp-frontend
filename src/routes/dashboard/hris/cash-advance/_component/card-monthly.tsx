528e37b41039a90e343ff85fcb2b379bcfe9d2ae
mmulyana <mulyan.t20@gmail.com>
2024-10-12 01:26:27 +0700

    feat: card-monthly

---
+++ src/routes/dashboard/hris/cash-advance/_component/card-monthly.tsx
@@ -0,0 +1,54 @@
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
+import { Card, Cardbody, CardHead } from '@/components/common/card-v1'
+
+const chartData = [
+  { month: 'Januari', total: 186 },
+  { month: 'Februari', total: 305 },
+  { month: 'Maret', total: 237 },
+  { month: 'April', total: 73 },
+  { month: 'Mei', total: 209 },
+  { month: 'Juni', total: 214 },
+]
+const chartConfig = {
+  total: {
+    label: 'Total',
+    color: '#2A9D90',
+  },
+} satisfies ChartConfig
+
+export default function CardMonthly() {
+  return (
+    <Card className='rounded-xl'>
+      <CardHead>
+        <p className='text-dark text-sm font-semibold'>
+          Laporan kasbon bulanan
+        </p>
+      </CardHead>
+      <Cardbody>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey='month'
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey='total' fill='var(--color-total)' radius={8} />
+          </BarChart>
+        </ChartContainer>
+      </Cardbody>
+    </Card>
+  )
+}
