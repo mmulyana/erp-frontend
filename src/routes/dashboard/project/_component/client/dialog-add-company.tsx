992963a7325f1e369ed03d443c0501fcb69d0b9d
mmulyana <mulyan.t20@gmail.com>
2024-10-12 18:20:22 +0700

    feat: update

---
+++ src/routes/dashboard/project/_component/client/dialog-add-company.tsx
@@ -0,0 +1,102 @@
+import UploadProfile from '@/components/common/upload-profile'
+import Modal, { ModalContainer } from '@/components/modal-v2'
+import { useCreateClientCompany } from '@/hooks/api/use-client'
+import { Form, FormField } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { delay } from '@/utils/delay'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Textarea } from '@/components/ui/textarea'
+import { CreateClientCompany } from '@/utils/types/form'
+
+const companySchema = z.object({
+  name: z.string(),
+  address: z.string().optional(),
+  contact: z.string().optional(),
+  email: z.string().optional(),
+  photo: z.instanceof(File).optional().nullable(),
+})
+
+type ModalProps = {
+  open: boolean
+  setOpen: (val: boolean) => void
+}
+export default function DialogAddCompany({ open, setOpen }: ModalProps) {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  // HANDLE FORM
+  const { mutate } = useCreateClientCompany()
+
+  const form = useForm<CreateClientCompany>({
+    resolver: zodResolver(companySchema),
+    defaultValues: {
+      name: '',
+      contact: '',
+      email: '',
+      address: '',
+      photo: null,
+    },
+  })
+
+  const onSubmit = async (data: CreateClientCompany) => {
+    mutate(data, {
+      onSuccess: () => {
+        delay(400).then(() => setOpen(false))
+      },
+    })
+  }
+
+  useEffect(() => {
+    if (!open) form.reset()
+  }, [open])
+  // HANDLE FORM
+
+  return (
+    <>
+      <Modal open={open} setOpen={setOpen} title='Buat perusahaan baru'>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='w-full flex flex-col gap-4 px-1 pb-8 md:pb-0'
+          >
+            <ModalContainer setOpen={setOpen}>
+              <UploadProfile
+                name='photo'
+                preview={preview}
+                setPreview={setPreview}
+              />
+              <FormField
+                control={form.control}
+                name='name'
+                label='Nama perusahaan'
+                render={({ field }) => <Input {...field} />}
+              />
+              <div className='grid grid-cols-2 gap-4'>
+                <FormField
+                  control={form.control}
+                  name='contact'
+                  label='Kontak'
+                  render={({ field }) => <Input {...field} />}
+                />
+                <FormField
+                  control={form.control}
+                  name='email'
+                  label='Email'
+                  render={({ field }) => <Input {...field} />}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name='address'
+                label='Alamat'
+                render={({ field }) => <Textarea {...field} />}
+              />
+            </ModalContainer>
+          </form>
+        </Form>
+      </Modal>
+    </>
+  )
+}
