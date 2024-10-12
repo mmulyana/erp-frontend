dd735eba45ee1570e5c841c07f65e0c32ec19755
mmulyana <mulyan.t20@gmail.com>
2024-10-13 01:13:20 +0700

    feat: bar multiple

---
+++ src/components/chart/bar-multiple.tsx
@@ -0,0 +1,60 @@
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
+import { Card, Cardbody, CardHead } from '@/components/common/card-v1'
+
+type Props = {
+  title: string
+  dataKeyX: string
+  dataKeyBar1: string
+  dataKeyBar2: string
+  config: ChartConfig
+  data: any
+}
+export default function CardBarStacked({
+  title,
+  dataKeyX,
+  dataKeyBar1,
+  dataKeyBar2,
+  config,
+  data,
+}: Props) {
+  return (
+    <Card className='rounded-xl'>
+      <CardHead>
+        <p className='text-dark text-sm font-semibold'>{title}</p>
+      </CardHead>
+      <Cardbody>
+        <ChartContainer config={config}>
+          <BarChart accessibilityLayer data={data}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey={dataKeyX}
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator='dashed' />}
+            />
+            <Bar
+              dataKey={dataKeyBar1}
+              fill={`var(--color-${dataKeyBar1})`}
+              radius={4}
+            />
+            <Bar
+              dataKey={dataKeyBar2}
+              fill={`var(--color-${dataKeyBar2})`}
+              radius={4}
+            />
+          </BarChart>
+        </ChartContainer>
+      </Cardbody>
+    </Card>
+  )
+}
