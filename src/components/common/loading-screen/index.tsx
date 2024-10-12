5642c3f35e342cafdf7e774c65284131d1c77cca
mmulyana <mulyan.t20@gmail.com>
2024-10-12 15:20:41 +0700

    feat: update

---
+++ src/components/common/loading-screen/index.tsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+  return (
+    <div className='h-screen w-full bg-gray-100 flex items-center justify-center'>
+      <div className="loader"></div>
+    </div>
+  )
+}
