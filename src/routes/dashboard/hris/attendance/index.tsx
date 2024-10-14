e2b5f2109e426ede64f64c73c7a25613aa27184a
mmulyana <mulyan.t20@gmail.com>
2024-10-14 09:28:50 +0700

    fix: update

--- src/routes/dashboard/hris/attendance/index.tsx
+++ src/routes/dashboard/hris/attendance/index.tsx
@@ -1,9 +1,11 @@
 import { PATH } from '@/utils/constant/_paths'
 import { Tab, Tabs } from '@/components/tab'
-import { Header, Overtime, Regular } from './component'
+import { Overtime, Regular } from './component'
 import { useTitle } from '../../_component/header'
 import Container from '../../_component/container'
 import { DashboardLayout } from '../../_component/layout'
+import TitlePage from '../../_component/title-page'
+import { FilePenIcon } from 'lucide-react'
 
 const links = [
   {
@@ -21,14 +23,15 @@ export default function Page() {
 
   return (
     <DashboardLayout>
-      <Container>
-        <Header />
-      </Container>
+      <TitlePage className='mb-2'>
+        <div className='flex gap-4 items-center'>
+          <FilePenIcon className='text-[#989CA8]' />
+          <p className='text-dark font-medium'>Absensi</p>
+        </div>
+      </TitlePage>
       <Tabs>
         <Tab label='Reguler'>
-          <Container>
-            <Regular />
-          </Container>
+          <Regular />
         </Tab>
         <Tab label='Lembur'>
           <Container>
