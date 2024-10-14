57b5a7f6ec3cd1fe7a2300bba6158daa46f89e03
mmulyana <mulyan.t20@gmail.com>
2024-10-14 10:19:47 +0700

    chore: update

--- src/utils/schema/overtime.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export type OvertimeSchema = z.infer<typeof overtimeSchema>
-export const overtimeSchema = z.object({
-  employeeId: z.string(),
-  date: z.string(),
-  total_hour: z.number(),
-  description: z.string().optional(),
-})
