12898fb658551c51d941f29f699b02006d4d59fc
mmulyana <mulyan.t20@gmail.com>
2024-10-14 21:29:57 +0700

    chore: remove unused file

--- src/utils/schema/cash-advances.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export type CashAdvancesSchema = z.infer<typeof cashAdvanceSchema>
-export const cashAdvanceSchema = z.object({
-  employeeId: z.string(),
-  amount: z.number(),
-  requestDate: z.string().date(),
-  description: z.string().optional(),
-})
