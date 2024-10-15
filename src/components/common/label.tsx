9645fa6fa5ce59d72a6475f5b9429c101d853f2b
mmulyana <mulyan.t20@gmail.com>
2024-10-15 16:30:50 +0700

    feat: new comp label

---
+++ src/components/common/label.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  name: string
+  color: string
+}
+export default function Label({ name, color }: Props) {
+  return (
+    <div className='py-0.5 px-1.5 text-sm w-fit relative'>
+      <p className='z-[1] text-nowrap' style={{ color, opacity: '100' }}>
+        {name}
+      </p>
+      <div
+        className='absolute w-full h-full rounded-full top-0 left-0'
+        style={{ background: color, opacity: '0.2' }}
+      ></div>
+    </div>
+  )
+}
