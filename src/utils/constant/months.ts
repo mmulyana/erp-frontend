cc068a3e1cc8373a76812089ab82a623841542e9
mmulyana <mulyan.t20@gmail.com>
2024-10-15 16:32:39 +0700

    feat: month

---
+++ src/utils/constant/months.ts
@@ -0,0 +1,50 @@
+export const months = [
+  {
+    value: 1,
+    name: 'Januari',
+  },
+  {
+    value: 2,
+    name: 'Februari',
+  },
+  {
+    value: 3,
+    name: 'Maret',
+  },
+  {
+    value: 4,
+    name: 'April',
+  },
+  {
+    value: 5,
+    name: 'Mei',
+  },
+  {
+    value: 6,
+    name: 'Juni',
+  },
+  {
+    value: 7,
+    name: 'Juli',
+  },
+  {
+    value: 8,
+    name: 'Agustus',
+  },
+  {
+    value: 9,
+    name: 'September',
+  },
+  {
+    value: 10,
+    name: 'Oktober',
+  },
+  {
+    value: 11,
+    name: 'November',
+  },
+  {
+    value: 12,
+    name: 'Desember',
+  },
+]
