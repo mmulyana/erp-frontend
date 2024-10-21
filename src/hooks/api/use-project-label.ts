308296539019c2783d5bfd29bbb70c95332d77a0
mmulyana <mulyan.t20@gmail.com>
2024-10-21 21:39:59 +0700

    feat: update use-project-label

--- src/hooks/api/use-project-label.ts
+++ src/hooks/api/use-project-label.ts
@@ -3,16 +3,18 @@ import { URLS } from '@/utils/constant/_urls'
 import { Pagination } from '@/utils/types/common'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import http from '@/utils/http'
-import { CreateSupplier } from '@/utils/types/form'
+import { createLabel } from '@/utils/types/form'
 import { toast } from 'sonner'
+import { AxiosResponse } from 'axios'
+import { IApi, ProjectLabel } from '@/utils/types/api'
 
 type Params = Pagination & {
   name?: string
   tag?: string
 }
-export const useProjectLabel = (params?: Params) => {
+export const useProjectLabels = (params?: Params) => {
   return useQuery({
-    queryFn: async () => {
+    queryFn: async (): Promise<AxiosResponse<IApi<ProjectLabel[]>>> => {
       return await http.request({
         method: 'GET',
         url: URLS.PROJECT_LABEL,
@@ -22,13 +24,48 @@ export const useProjectLabel = (params?: Params) => {
     queryKey: [KEYS.PROJECT_LABEL, params],
   })
 }
+export const useDetailProjectLabel = ({
+  id,
+  enabled,
+}: {
+  id?: number | null
+  enabled?: boolean
+}) => {
+  return useQuery({
+    queryFn: async (): Promise<AxiosResponse<IApi<ProjectLabel>>> => {
+      return await http(`${URLS.PROJECT_LABEL}/${id}`)
+    },
+    queryKey: [KEYS.PROJECT_LABEL, id],
+    enabled,
+  })
+}
 
 export const useCreateProjectLabel = () => {
   const queryClient = useQueryClient()
 
   return useMutation({
-    mutationFn: async ({ payload }: { payload: CreateSupplier }) => {
+    mutationFn: async ({ payload }: { payload: createLabel }) => {
       return await http.post(URLS.PROJECT_LABEL, payload)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.PROJECT_LABEL] })
+      toast.success(data.data.message)
+    },
+  })
+}
+export const useUpdateProjectLabel = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      payload,
+    }: {
+      payload: createLabel & { id: number }
+    }) => {
+      return await http.patch(`${URLS.PROJECT_LABEL}/${payload.id}`, {
+        name: payload.name,
+        color: payload.color,
+      })
     },
     onSuccess: (data) => {
       queryClient.invalidateQueries({ queryKey: [KEYS.PROJECT_LABEL] })
