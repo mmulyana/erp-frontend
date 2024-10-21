210653339a82abc0fbd5c5dcea8cd173d8e870d9
mmulyana <mulyan.t20@gmail.com>
2024-10-21 21:38:50 +0700

    feat: update search and debounce

--- src/components/common/search/index.tsx
+++ src/components/common/search/index.tsx
@@ -1,12 +1,52 @@
+import React, { useState, useCallback, useEffect } from 'react'
 import { Search as SearchIcon } from 'lucide-react'
 import useUrlState from '@ahooksjs/use-url-state'
 import { Input } from '@/components/ui/input'
+import { useDebounce } from '@uidotdev/usehooks'
 
 type Props = {
   placeholder?: string
+  withoutUrl?: boolean
+  onSearch?: (val: string) => void
+  debounceTime?: number
 }
-export default function Search({ placeholder }: Props) {
+
+export default function Search({
+  placeholder = 'Search',
+  withoutUrl = false,
+  onSearch,
+  debounceTime = 300,
+}: Props) {
   const [url, setUrl] = useUrlState({ name: '' })
+  const [search, setSearch] = useState(withoutUrl ? '' : url.name)
+
+  const debouncedSearch = useDebounce(search, debounceTime)
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value
+      setSearch(value)
+    },
+    [withoutUrl]
+  )
+
+  useEffect(() => {
+    if (!withoutUrl && search !== '') {
+      setUrl({ name: debouncedSearch })
+    }
+  }, [debouncedSearch, withoutUrl, setUrl, search])
+
+  useEffect(() => {
+    if (onSearch) {
+      onSearch(debouncedSearch)
+    }
+  }, [debouncedSearch, onSearch])
+
+  useEffect(() => {
+    if (!withoutUrl) {
+      setSearch(url.name)
+    }
+  }, [url.name, withoutUrl])
 
   return (
     <div className='relative'>
@@ -17,10 +57,10 @@ export default function Search({ placeholder }: Props) {
         <SearchIcon className='h-4 w-4 text-muted-foreground' />
       </label>
       <Input
-        id='SearchV1'
-        value={url.name}
-        onChange={(e) => setUrl({ name: e.target.value })}
-        placeholder={placeholder || 'Search'}
+        id='searchV1'
+        value={search}
+        onChange={handleChange}
+        placeholder={placeholder}
         className='pl-8 text-sm h-8 py-2 shadow-md shadow-gray-100 border border-[#e3e4e7] rounded-[8px] max-w-32'
       />
     </div>
