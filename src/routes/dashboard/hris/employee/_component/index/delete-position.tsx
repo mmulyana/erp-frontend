d7827f17f8ae2e413153f83d83fa4c81e3165c77
mmulyana <mulyan.t20@gmail.com>
2024-10-23 01:37:19 +0700

    feat: update

---
+++ src/routes/dashboard/hris/employee/_component/index/delete-position.tsx
@@ -0,0 +1,57 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+import { buttonVariants } from '@/components/ui/button'
+import { useDeletePosition } from '@/hooks/api/use-position'
+import { cn } from '@/utils/cn'
+import { delay } from '@/utils/delay'
+
+type ModalProps = {
+  id?: number | null
+  open: boolean
+  setOpen: (val: boolean) => void
+}
+export default function ModalDelete({ id, open, setOpen }: ModalProps) {
+  const { mutate } = useDeletePosition()
+  const onDelete = async () => {
+    if (!id) return
+    mutate(
+      { id },
+      {
+        onSuccess: () => {
+          delay(400).then(() => setOpen(false))
+        },
+      }
+    )
+  }
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Hapus posisi ini?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Posisi ini akan dihapus dari database
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => setOpen(false)}>
+            Batal
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={onDelete}
+            className={cn(buttonVariants({ variant: 'destructive' }))}
+          >
+            Lanjutkan
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
