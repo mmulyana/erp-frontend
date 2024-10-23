ad42ad71e3fdf732bd391e016ce7f95515198035
mmulyana <mulyan.t20@gmail.com>
2024-10-23 19:09:45 +0700

    feat: update

--- src/utils/types/common.ts
+++ src/utils/types/common.ts
@@ -2,3 +2,12 @@ export type Pagination = {
   page?: string
   limit?: string
 }
+
+export type StatusItem = {
+  name: string
+  value: string
+}
+
+export type StatusObject = {
+  [key: string]: string
+}
