7bf7d95916f88f14a8df6379ef7ce44b96b8ddc1
mmulyana <mulyan.t20@gmail.com>
2024-10-23 19:48:59 +0700

    feat: update

--- src/utils/data/gender.ts
+++ src/utils/data/gender.ts
@@ -1,4 +1,6 @@
-export const GENDER = [
+import { StatusItem, StatusObject } from "../types/common";
+
+export const GENDER: StatusItem[] = [
   {
     value: 'male',
     name: 'Laki-laki',
@@ -8,3 +10,11 @@ export const GENDER = [
     name: 'Perempuan',
   },
 ]
+
+export const GENDER_OBJ = GENDER.reduce<StatusObject>(
+  (acc, item) => {
+    acc[item.value] = item.name
+    return acc
+  },
+  {}
+)
