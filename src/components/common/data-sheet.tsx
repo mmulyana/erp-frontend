5bd0f36fe44945c2fdd78646e9940c0e7cbfc4ed
mmulyana <mulyan.t20@gmail.com>
2024-10-23 20:41:02 +0700

    feat: update

--- src/components/common/data-sheet.tsx
+++ src/components/common/data-sheet.tsx
@@ -1,13 +1,19 @@
+import { cn } from '@/utils/cn'
 import React from 'react'
 
 interface DataSheetProps {
   width?: number
   children: React.ReactNode
+  className?: string
 }
 
-export default function DataSheet({ children, width }: DataSheetProps) {
+export default function DataSheet({
+  children,
+  width,
+  className,
+}: DataSheetProps) {
   return (
-    <div className='flex gap-2 items-center'>
+    <div className={cn('flex gap-2 items-center', className)}>
       <div
         className='w-[140px] flex-shrink-0'
         style={{ width: width ? `${width}px` : undefined }}
