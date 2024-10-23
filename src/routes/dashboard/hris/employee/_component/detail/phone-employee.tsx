7e36bda1b3ea140ea6f9390a6e8f756f851daa32
mmulyana <mulyan.t20@gmail.com>
2024-10-23 22:23:47 +0700

    feat: update

--- src/routes/dashboard/hris/employee/_component/detail/phone-employee.tsx
+++ src/routes/dashboard/hris/employee/_component/detail/phone-employee.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@/components/ui/button'
 import { Form } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import {
+  useCreatePhone,
+  useDeletePhone,
+  useUpdatePhone,
+} from '@/hooks/api/use-employee'
+import { cn } from '@/utils/cn'
 import { PhoneNumber } from '@/utils/types/api'
-import { Phone } from 'lucide-react'
-import { KeyboardEvent, useState } from 'react'
+import { Copy, Eye, EyeOff, Pencil, Phone, Plus, Trash } from 'lucide-react'
+import { KeyboardEvent, useRef, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 
 type Props = {
@@ -11,71 +17,226 @@ type Props = {
   phones: PhoneNumber[]
 }
 export default function PhoneEmployee({ id, phones }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const { mutate: create } = useCreatePhone()
+  const { mutate: remove } = useDeletePhone()
+  const { mutate: update } = useUpdatePhone()
+
   const form = useForm<{ value: string }>({
     defaultValues: {
       value: '',
     },
   })
+
   const [isPhone, setIsPhone] = useState(false)
+  const [open, setOpen] = useState(false)
+
+  // HANDLE EDIT
+  const [selectedId, setSelectedId] = useState<number | null>(null)
+
+  const onEdit = (id: number) => {
+    setSelectedId(id)
+    setIsPhone(true)
+    const index = phones.findIndex((item) => item.id === id)
+    form.setValue('value', phones[index].value)
+    inputRef.current?.focus()
+  }
+  // HANDLE EDIT
+
+  const reset = () => {
+    setIsPhone(false)
+    form.reset()
+    containerRef.current?.focus()
+    setSelectedId(null)
+  }
 
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-        
+      if (!id) return
+      if (selectedId) {
+        update(
+          { id: selectedId, value: form.getValues('value') },
+          {
+            onSuccess: () => {
+              reset()
+            },
+          }
+        )
+        return
+      }
+      create(
+        { id, value: form.getValues('value') },
+        {
+          onSuccess: () => {
+            reset()
+          },
+        }
+      )
     }
   }
-
-  const submit = (data: { value: string }) => {}
 
   if (!id) return null
 
   return (
-    <div className='space-y-2'>
-      <div className='flex gap-2 flex-wrap'>
-        {phones?.map((phone, index) => (
-          <div
-            key={index}
-            className='px-2 py-0.5 rounded-full border border-line text-dark/50'
-          >
-            {phone.value}
-          </div>
-        ))}
-      </div>
-      {isPhone ? (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(submit)}>
-            <div className='flex gap-2 items-center'>
-              <div className='relative'>
-                <Controller
-                  control={form.control}
-                  name='value'
-                  render={({ field }) => (
-                    <Input
-                      onKeyDown={handleKeyDown}
-                      className='peer ps-9 [direction:inherit]'
-                      {...field}
-                    />
-                  )}
-                />
-                <div className='pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50'>
-                  <Phone size={16} strokeWidth={2} aria-hidden='true' />
+    <div className='space-y-2' ref={containerRef}>
+      {phones.length > 0 ? (
+        <div
+          className={cn(
+            'flex gap-2 flex-wrap items-start flex-col',
+            open && 'gap-4'
+          )}
+        >
+          {open ? (
+            <>
+              {phones?.map((phone, index) => (
+                <div key={index} className='text-dark/50 relative group'>
+                  {phone.value}
+                  <div className='absolute left-[calc(100%+14px)] top-1/2 -translate-y-1/2 flex gap-2 items-center'>
+                    <button className='w-6 h-6 rounded-full text-dark bg-muted-foreground/10 flex items-center justify-center'>
+                      <Copy size={12} />
+                    </button>
+                    <button
+                      className='w-6 h-6 rounded-full text-dark bg-muted-foreground/10 flex items-center justify-center'
+                      onClick={() => {
+                        remove({ id: phone.id })
+                      }}
+                    >
+                      <Trash size={12} />
+                    </button>
+                    <button
+                      className='w-6 h-6 rounded-full text-dark bg-muted-foreground/10 flex items-center justify-center'
+                      onClick={() => {
+                        onEdit(phone.id)
+                      }}
+                    >
+                      <Pencil size={12} />
+                    </button>
+                  </div>
+                </div>
+              ))}
+              {!isPhone && (
+                <Button
+                  type='submit'
+                  variant='secondary'
+                  onClick={() => setOpen(false)}
+                  className='h-5 font-normal text-sm rounded-md py-3 pl-6 pr-3 text-dark items-center gap-1 relative mx-auto'
+                >
+                  <EyeOff
+                    size={12}
+                    className='absolute left-2 top-1/2 -translate-y-1/2'
+                  />
+                  Tutup
+                </Button>
+              )}
+            </>
+          ) : (
+            <>
+              <div className='text-dark/50 relative group gap-2 flex items-center'>
+                {phones[0]?.value}
+                <div className='absolute left-[calc(100%+14px)] top-1/2 -translate-y-1/2 flex gap-2 items-center'>
+                  <button className='w-6 h-6 rounded-full text-dark flex items-center justify-center'>
+                    <Copy size={14} />
+                  </button>
+                  <button
+                    className='w-6 h-6 rounded-full text-dark flex items-center justify-center'
+                    onClick={() => {
+                      remove({ id: phones[0].id })
+                    }}
+                  >
+                    <Trash size={14} />
+                  </button>
+                  <button
+                    className='w-6 h-6 rounded-full text-dark flex items-center justify-center'
+                    onClick={() => {
+                      onEdit(phones[0].id)
+                    }}
+                  >
+                    <Pencil size={14} />
+                  </button>
                 </div>
               </div>
-              <Button
-                type='submit'
-                variant='secondary'
-                className='h-6 font-normal text-sm rounded-md p-0 px-2'
-              >
-                Simpan
-              </Button>
+              {phones.length > 1 && (
+                <Button
+                  type='submit'
+                  variant='secondary'
+                  onClick={() => setOpen(true)}
+                  className='h-5 font-normal text-sm rounded-md py-3 pl-6 pr-3 text-dark items-center gap-1 relative'
+                >
+                  <Eye
+                    size={12}
+                    className='absolute left-2 top-[56%] -translate-y-1/2'
+                  />
+                  Lihat semua nomor
+                  <span>{phones?.length - 1}</span>
+                </Button>
+              )}
+            </>
+          )}
+        </div>
+      ) : null}
+      {isPhone ? (
+        <Form {...form}>
+          <div className='flex gap-2 items-center'>
+            <div className='relative'>
+              <Controller
+                control={form.control}
+                name='value'
+                render={({ field }) => (
+                  <Input
+                    onKeyDown={handleKeyDown}
+                    className='pl-6 [direction:inherit] h-7 w-full rounded-md'
+                    {...field}
+                    ref={inputRef}
+                  />
+                )}
+              />
+              <div className='pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center pl-1 text-muted-foreground/80 peer-disabled:opacity-50'>
+                <Phone size={14} strokeWidth={2} aria-hidden='true' />
+              </div>
             </div>
-          </form>
+            <Button
+              type='submit'
+              variant='secondary'
+              className='h-7 font-normal text-sm rounded-md p-0 px-2'
+              onClick={() => {
+                if (!id) return
+                if (selectedId) {
+                  update(
+                    { id: selectedId, value: form.getValues('value') },
+                    {
+                      onSuccess: () => {
+                        reset()
+                      },
+                    }
+                  )
+                  return
+                }
+                create(
+                  { id, value: form.getValues('value') },
+                  {
+                    onSuccess: () => {
+                      reset()
+                    },
+                  }
+                )
+              }}
+            >
+              Simpan
+            </Button>
+          </div>
         </Form>
       ) : (
         <Button
           variant='ghost'
-          className='font-normal p-0 hover:bg-transparent gap-1 flex items-center text-blue-primary/80 hover:text-blue-primary h-fit px-2 py-0.5 relative border text-sm'
-          onClick={() => setIsPhone(true)}
+          className='font-normal  hover:bg-transparent gap-1 flex items-center text-blue-primary/80 hover:text-blue-primary h-fit p-0 relative text-sm mt-1'
+          onClick={() => {
+            setIsPhone(true)
+            inputRef.current?.focus()
+          }}
         >
+          <Plus size={14} />
           Tambah No.telp
         </Button>
       )}
