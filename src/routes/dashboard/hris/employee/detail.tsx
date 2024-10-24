c96e2c484f19a68957635cbb1fb60f609aed48ba
mmulyana <mulyan.t20@gmail.com>
2024-10-25 02:00:26 +0700

    feat: update

--- src/routes/dashboard/hris/employee/detail.tsx
+++ src/routes/dashboard/hris/employee/detail.tsx
@@ -1,6 +1,6 @@
 import { Title, useTitle } from '@/routes/dashboard/_component/header'
 import { DashboardLayout } from '@/routes/dashboard/_component/layout'
-import { Card, Cardbody, CardHead } from '@/components/common/card-v1'
+import { Card, CardBody, CardHead } from '@/components/common/card-v1'
 import DetailEmployee from './_component/detail/detail-employee'
 import DeleteEmployee from './_component/detail/delete-employee'
 import TableEmployee from './_component/detail/table-employee'
@@ -11,6 +11,7 @@ import { PATH } from '@/utils/constant/_paths'
 import { useParams } from 'react-router-dom'
 import { links } from './_component/links'
 import { useState } from 'react'
+import ExpireCertif from './_component/detail/expire-certif'
 
 export type DialogEmployee = {
   add: boolean
@@ -52,36 +53,12 @@ export default function Detail() {
           />
         </div>
         <div className='h-[calc(100vh-48px)] border-l border-line px-4 py-2 space-y-4'>
-          <Card>
-            <CardHead className='px-3'>
-              <p className='text-dark text-sm'>
-                Tenggat Waktu Sertifikasi Pegawai
-              </p>
-            </CardHead>
-            <Cardbody className='p-0'>
-              <ScrollArea className='h-48 flex flex-col gap-4'>
-                {[0, 1, 2, 3, 4].map((item) => (
-                  <div
-                    key={item}
-                    className='px-3 py-3.5 grid grid-cols-[24px_1fr] gap-4 border-b border-line'
-                  >
-                    <div className='w-6 h-6 rounded-full bg-gray-400'></div>
-                    <p className='text-sm text-dark/50'>
-                      Sertifikat <span className='text-dark'>TKBT</span>{' '}
-                      <span className='text-dark'>Muhamad Mulyana</span> akan
-                      kedaluwarsa dalam{' '}
-                      <span className='text-dark'>10 hari</span>
-                    </p>
-                  </div>
-                ))}
-              </ScrollArea>
-            </Cardbody>
-          </Card>
+          <ExpireCertif positionId={positionId} />
           <Card>
             <CardHead className='px-3'>
               <p className='text-dark text-sm'>Sertifikat Kadaluwarsa</p>
             </CardHead>
-            <Cardbody className='p-0'>
+            <CardBody className='p-0'>
               <ScrollArea className='h-48 flex flex-col gap-4'>
                 {[0, 1, 2, 3, 4].map((item) => (
                   <div
@@ -98,7 +75,7 @@ export default function Detail() {
                   </div>
                 ))}
               </ScrollArea>
-            </Cardbody>
+            </CardBody>
           </Card>
         </div>
       </div>
