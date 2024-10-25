5bf994bc1b5acf23fe798778721d47fbcddf64aa
mmulyana <mulyan.t20@gmail.com>
2024-10-25 21:16:14 +0700

    feat: update

--- src/components/common/chips.tsx
+++ src/components/common/chips.tsx
@@ -1,29 +1,36 @@
 import { cn } from '@/utils/cn'
 
 type Props = {
-  status: boolean
+  text?: string
+  status?: boolean
   className?: string
+  background?: string
 }
-export default function Chips({ status, className }: Props) {
+export default function Chips({ status, className, background, text }: Props) {
   return (
     <div
       className={cn(
-        'text-sm pl-2 pr-[9px] py-0.5 rounded-[6px] border border-[#EFF0F2] inline-flex items-center gap-1 w-fit',
+        'text-sm pl-2 pr-[9px] py-0.5 border border-[#EFF0F2] inline-flex items-center gap-1 w-fit rounded-full',
         className
       )}
     >
       <div
         className={cn(
           'w-1 h-1 rounded-full',
-          status ? 'bg-[#36B062]' : 'bg-[#D64E44]'
+          !background && status ? 'bg-green-primary' : 'bg-red-primary'
         )}
+        style={{ background: background }}
       ></div>
       <p
         className={cn(
-          status ? 'text-[#36B062]' : 'text-[#D64E44]'
+          !text
+            ? status
+              ? 'text-green-primary'
+              : 'text-red-primary'
+            : 'text-dark'
         )}
       >
-        {status ? 'Aktif' : 'Nonaktif'}
+        {text ? text : status ? 'Aktif' : 'Nonaktif'}
       </p>
     </div>
   )
