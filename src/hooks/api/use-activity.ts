01a8e9f9e726a3e8c8801659dbe4fdc5807e1988
mmulyana <mulyan.t20@gmail.com>
2024-10-28 20:24:55 +0700

    feat: update

--- src/hooks/api/use-activity.ts
+++ src/hooks/api/use-activity.ts
@@ -1,20 +1,23 @@
-import { KEYS } from '@/utils/constant/_keys'
-import { URLS } from '@/utils/constant/_urls'
-import http from '@/utils/http'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { Activity, IApi } from '@/utils/types/api'
 import { createActivity } from '@/utils/types/form'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { URLS } from '@/utils/constant/_urls'
+import { KEYS } from '@/utils/constant/_keys'
 import { AxiosResponse } from 'axios'
+import http from '@/utils/http'
 
 type Params = {
   id?: number | null
+  projectId?: number | null
   enabled: boolean
 }
 export const useActivity = (params?: Params) => {
   return useQuery({
-    queryKey: [KEYS.ACTIVITY, params?.id],
+    queryKey: [KEYS.ACTIVITY, params?.projectId, params?.id,],
     queryFn: async (): Promise<AxiosResponse<IApi<Activity[]>>> => {
-      return await http(URLS.PROJECT_ACTIVITY, { params: { id: params?.id } })
+      return await http(URLS.PROJECT_ACTIVITY, {
+        params: { id: params?.id, projectId: params?.projectId },
+      })
     },
     enabled: params?.enabled,
   })
@@ -24,12 +27,15 @@ export const useCreateActivity = () => {
   return useMutation({
     mutationFn: async (
       payload: createActivity
-    ): Promise<AxiosResponse<IApi<{ projectId: number }>>> => {
+    ): Promise<AxiosResponse<IApi<{ projectId: number, replyId?: number }>>> => {
       return await http.post(URLS.PROJECT_ACTIVITY, payload)
     },
     onSuccess: (data) => {
       queryClient.invalidateQueries({
-        queryKey: [KEYS.ACTIVITY, data.data.data?.projectId],
+        queryKey: [KEYS.ACTIVITY, data.data.data?.projectId]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [KEYS.ACTIVITY, data.data.data?.projectId, data.data.data?.replyId]
       })
     },
   })
