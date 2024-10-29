3be991d82f6a35a4dc1bbfaaec414ab3864a72ea
mmulyana <mulyan.t20@gmail.com>
2024-10-29 10:18:52 +0700

    feat: update

--- src/routes/dashboard/project/_component/activity/message-item.tsx
+++ src/routes/dashboard/project/_component/activity/message-item.tsx
@@ -9,17 +9,24 @@ import { userAtom } from '@/atom/auth'
 import { useAtomValue } from 'jotai'
 import { format } from 'date-fns'
 import { cn } from '@/utils/cn'
+import { BASE_URL } from '@/utils/constant/_urls'
 
 type MessageContentProps = Activity & {
   isOwnMessage: boolean
-  onSelectActivity?: (val: Activity & { open: boolean }) => void
+  onSelectActivity?: (val: Partial<Activity> & { open: boolean }) => void
   onDelete?: (val: number) => void
+  onToggle?: (userId: number, activityId: number) => void
+  userId?: number | null
+  showReplyButton?: boolean
 }
 
 function MessageContent({
   onSelectActivity,
   isOwnMessage,
   onDelete,
+  userId,
+  onToggle,
+  showReplyButton = true,
   ...props
 }: MessageContentProps) {
   return (
@@ -52,43 +59,59 @@ function MessageContent({
       <p
         className={`text-dark ${isOwnMessage ? 'mt-1' : 'text-lg'}`}
         onClick={() => {
-          onSelectActivity?.({ ...props, open: true })
+          onSelectActivity?.({ id: props.id, open: true })
         }}
       >
         {props.comment}
       </p>
 
-      <div className='flex gap-1.5 items-center mt-1'>
-        <div className='w-14 h-14 rounded-lg bg-gray-200'></div>
-        <div className='w-14 h-14 rounded-lg bg-gray-200'></div>
-        <div className='w-14 h-14 rounded-lg bg-gray-100 flex justify-center items-center'>
-          <p className='text-dark font-medium'>2+</p>
+      {!!props.attachments?.length && (
+        <div className='flex gap-1.5 items-center mt-1'>
+          {props?.attachments.slice(0, 2).map((item) => (
+            <img
+              className='w-14 h-14 rounded-lg object-cover object-center'
+              src={BASE_URL + '/img/' + item.attachment}
+            />
+          ))}
+          {props.attachments.length > 2 && (
+            <div className='w-14 h-14 rounded-lg bg-gray-100 flex justify-center items-center'>
+              <p className='text-dark font-medium'>
+                {props.attachments.length - 2}+
+              </p>
+            </div>
+          )}
         </div>
-      </div>
+      )}
 
       <div className='flex justify-between items-start gap-2 mt-1.5'>
         <div className='flex gap-2 items-center'>
           <Button
             className='px-2 gap-1.5 border-dark/10 rounded h-fit py-1 bg-transparent text-dark'
             variant='outline'
+            onClick={() => {
+              if (!userId) return
+              onToggle?.(userId, props.id)
+            }}
           >
             <ThumbsUp size={15} strokeWidth={2} />
             {!!props.likes?.length && (
               <p className='text-dark leading-none'>{props.likes.length}</p>
             )}
           </Button>
-          <Button
-            className='px-2 gap-1.5 border-dark/10 rounded h-fit py-1 bg-transparent text-dark'
-            variant='outline'
-            onClick={() => {
-              onSelectActivity?.({ ...props, open: true })
-            }}
-          >
-            <MessageCircle size={15} strokeWidth={2} />
-            {!!props.replies?.length && (
-              <p className='text-dark leading-none'>{props.replies.length}</p>
-            )}
-          </Button>
+          {showReplyButton && (
+            <Button
+              className='px-2 gap-1.5 border-dark/10 rounded h-fit py-1 bg-transparent text-dark'
+              variant='outline'
+              onClick={() => {
+                onSelectActivity?.({ id: props.id, open: true })
+              }}
+            >
+              <MessageCircle size={15} strokeWidth={2} />
+              {!!props.replies?.length && (
+                <p className='text-dark leading-none'>{props.replies.length}</p>
+              )}
+            </Button>
+          )}
         </div>
       </div>
     </div>
@@ -96,7 +119,8 @@ function MessageContent({
 }
 
 type MessageItemProps = Activity & {
-  onSelectActivity?: (val: Activity & { open: boolean }) => void
+  onSelectActivity?: (val: Partial<Activity> & { open: boolean }) => void
+  onToggle?: (userId: number, activityId: number) => void
   onDelete?: (val: number) => void
   withReplies?: boolean
 }
@@ -105,6 +129,7 @@ export default function MessageItem({
   onSelectActivity,
   withReplies = false,
   onDelete,
+  onToggle,
   ...props
 }: MessageItemProps) {
   const currentUser = useAtomValue(userAtom)
@@ -114,10 +139,8 @@ export default function MessageItem({
     <div>
       <div
         className={cn(
-          withReplies && 'p-2.5',
-          isOwnMessage
-            ? 'grid grid-cols-[28px_1fr] gap-2 '
-            : 'grid grid-cols-[28px_1fr] gap-2 border-b border-dark/10'
+          'grid grid-cols-[28px_1fr] gap-2',
+          withReplies && 'p-2.5'
         )}
       >
         <div className='w-7 h-7 rounded-full bg-gray-200'></div>
@@ -126,6 +149,8 @@ export default function MessageItem({
           isOwnMessage={isOwnMessage}
           onSelectActivity={onSelectActivity}
           onDelete={onDelete}
+          onToggle={onToggle}
+          showReplyButton={!props?.replyId}
         />
       </div>
 
@@ -145,6 +170,7 @@ export default function MessageItem({
                 key={`reply-${reply.id}`}
                 withReplies={false}
                 onDelete={onDelete}
+                onToggle={onToggle}
                 {...reply}
               />
             ))}
