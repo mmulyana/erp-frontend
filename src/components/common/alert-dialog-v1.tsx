6a19762364b3de602165232a8b060d23cd0d719c
mmulyana <mulyan.t20@gmail.com>
2024-10-31 02:58:43 +0700

    feat: update

---
+++ src/components/common/alert-dialog-v1.tsx
@@ -0,0 +1,62 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+
+type TProps = {
+  open?: boolean
+  setOpen?: (value: boolean) => void
+  title?: string
+  body?: string
+  onConfirm?: () => void
+  cancelText?: string
+  confirmText?: string
+}
+export default function AlertDialogV1({
+  open = false,
+  setOpen,
+  title = 'Apakah kamu yakin?',
+  body = 'Aksi ini tidak dapat dibatalkan, setelah kamu memilih melakukan konfirmasi',
+  onConfirm,
+  cancelText,
+  confirmText,
+}: TProps) {
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger />
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{body}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            onClick={() => {
+              if (setOpen) {
+                setOpen(false)
+              }
+            }}
+          >
+            {cancelText ?? 'Batal'}
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              if (onConfirm) {
+                onConfirm()
+              }
+            }}
+          >
+            {confirmText ?? 'Lanjutkan'}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
