6742bd0860d83746e8a56dd641617be06a4a9aa3
mmulyana <mulyan.t20@gmail.com>
2024-10-31 23:45:32 +0700

    feat: update

--- src/routes/dashboard/project/_component/detail/label-project.tsx
+++ src/routes/dashboard/project/_component/detail/label-project.tsx
@@ -1,17 +1,171 @@
 import Label from '@/components/common/label'
-import { cn } from '@/utils/cn'
+import { Button } from '@/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Form, FormControl, FormItem } from '@/components/ui/form'
+import { useProjectLabels } from '@/hooks/api/use-project-label'
+import {
+  useAddLabelProject,
+  useRemoveLabelProject,
+} from '@/hooks/api/use-project'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Project } from '@/utils/types/api'
+import { useMemo, useState } from 'react'
+import { Plus, X } from 'lucide-react'
+import { cn } from '@/utils/cn'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  labelId: z.number({
+    required_error: 'Silahkan pilih label',
+  }),
+})
+
+type FormValues = z.infer<typeof formSchema>
 
 type Props = {
   id: number
   data: Pick<Project, 'labels'>
 }
-export default function LabelProject({ id, data: { labels } }: Props) {
+export default function LabelProject({
+  id: projectId,
+  data: { labels },
+}: Props) {
+  // HANDLE SEARCH
+  const [search, setSearch] = useState('')
+  const handleSearch = (value: string) => {
+    setSearch(value)
+  }
+  // HANDLE SELECT
+  const [open, setOpen] = useState(false)
+
+  // GET DATA LABEL
+  const { data, isLoading } = useProjectLabels()
+  const dataLabels = useMemo(() => data?.data.data || [], [data, isLoading])
+
+  // HANDLE FORM
+  const { mutate: add } = useAddLabelProject()
+  const { mutate: remove } = useRemoveLabelProject()
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const submit = (data: FormValues) => {
+    if (!projectId) return
+    add(
+      { projectId, labelId: data.labelId },
+      {
+        onSuccess: () => {
+          setOpen(false)
+          form.reset()
+        },
+      }
+    )
+  }
+
+  // DATA FOR SELECT
+  const filteredLabels = useMemo(() => {
+    const existing = labels.map((item) => item.label.id)
+
+    const available = dataLabels.filter((item) => !existing.includes(item.id))
+
+    if (!search) {
+      return available
+    }
+
+    return available.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    )
+  }, [dataLabels, search, labels])
+
   return (
     <div className={cn('flex flex-wrap gap-2', labels.length > 2 && 'pt-2')}>
       {labels?.map((item) => (
-        <Label color={item.label.color} name={item.label.name} />
+        <Label
+          color={item.label.color}
+          name={item.label.name}
+          suffix={
+            <Button
+              variant='ghost'
+              className='border p-0 w-4 h-4 cursor-pointer z-[1] hover:bg-transparent opacity-70 pt-0.5'
+              style={{ color: item.label.color }}
+              onClick={() => {
+                remove({ id: item.id })
+              }}
+            >
+              <X size={14} strokeWidth={3} />
+            </Button>
+          }
+        />
       ))}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(submit)}>
+          <Controller
+            control={form.control}
+            name='labelId'
+            render={({ field }) => (
+              <FormItem className='flex-1'>
+                <FormControl>
+                  <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant='ghost'
+                        className='font-normal p-0 hover:bg-transparent inline-flex items-center text-gray-400 text-sm h-fit relative'
+                      >
+                        <Plus size={14} />
+                        Tambah
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className='p-0 w-[160px]' align='start'>
+                      <Command>
+                        <CommandInput
+                          placeholder='Cari...'
+                          value={search}
+                          onValueChange={handleSearch}
+                        />
+                        <CommandList>
+                          <CommandEmpty>
+                            {filteredLabels.length
+                              ? `Tidak ditemukan ${search}`
+                              : 'Kosong'}
+                          </CommandEmpty>
+                          <CommandGroup>
+                            {filteredLabels.map((item, index) => (
+                              <CommandItem
+                                key={`competency-${index}`}
+                                value={item.name}
+                                onSelect={() => {
+                                  field.onChange(item.id)
+                                  form.handleSubmit(submit)()
+                                }}
+                              >
+                                <Label name={item.name} color={item.color} />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   )
 }
