d9f79412ab70b38551518cd0f9ccdd4467ec4602
mmulyana <mulyan.t20@gmail.com>
2024-11-01 03:45:29 +0700

    refactor: client

--- src/routes/dashboard/project/client.tsx
+++ src/routes/dashboard/project/client.tsx
@@ -1,139 +1,23 @@
-import { FilterTable } from '@/components/data-table/component'
 import { DashboardLayout } from '../_component/layout'
-import { SquareUserRoundIcon, TrashIcon } from 'lucide-react'
+import { SquareUserRoundIcon } from 'lucide-react'
+import TitlePage from '../_component/title-page'
 import { Button } from '@/components/ui/button'
 import { Tab, Tabs } from '@/components/tab'
-import TitlePage from '../_component/title-page'
-import { useClient, useClientCompany } from '@/hooks/api/use-client'
-import { ColumnDef } from '@tanstack/react-table'
-import { Client as ClientType, Company } from '@/utils/types/api'
-import DropdownEdit from '@/components/common/dropdown-edit'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { DataTable } from '@/components/data-table'
 import { useState } from 'react'
-import DialogAddClient from './_component/client/dialog-add-client'
-import DialogAddCompany from './_component/client/dialog-add-company'
 import DialogDeleteCompany from './_component/client/dialog-delete-company'
 import DialogDeleteClient from './_component/client/dialog-delete-client'
-import { BASE_URL } from '@/utils/constant/_urls'
+import DialogAddCompany from './_component/client/dialog-add-company'
+import DialogAddClient from './_component/client/dialog-add-client'
+import TableCompany from './_component/client/table-company'
+import TableClient from './_component/client/table-client'
 
 export default function Client() {
-  const qClient = useClient()
-  const qClientCompany = useClientCompany()
-
-  // COLUMNS CLIENT
-  const columnsClient: ColumnDef<ClientType>[] = [
-    {
-      accessorKey: 'name',
-      header: 'Nama',
-    },
-    {
-      accessorKey: 'position',
-      header: 'Jabatan',
-    },
-    {
-      id: 'company',
-      header: 'Perusahaan',
-      cell: ({ row }) => (
-        <div className='flex gap-1 items-center'>
-          {row.original.company && (
-            <>
-              <img
-                className='w-6 h-6 rounded-full object-cover object-center shadow'
-                src={BASE_URL + '/img/' + row.original?.company.logo}
-              />
-              <p className='text-dark/70'>{row.original.company.name}</p>
-            </>
-          )}
-        </div>
-      ),
-    },
-    {
-      accessorKey: 'phone',
-      header: 'Kontak',
-    },
-    {
-      accessorKey: 'email',
-      header: 'Email',
-    },
-    {
-      id: 'action',
-      cell: ({ row }) => (
-        <div className='flex justify-end'>
-          <DropdownEdit className='-translate-x-3'>
-            <DropdownMenuGroup>
-              <DropdownMenuItem
-                className='flex items-center gap-2 cursor-pointer'
-                onClick={() => {
-                  setSelectedId(row.original.id)
-                  handleDialog('clientDelete', true)
-                }}
-              >
-                <TrashIcon className='w-3.5 h-3.5 text-dark/50' />
-                Hapus
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownEdit>
-        </div>
-      ),
-    },
-  ]
-  // COLUMNS Company
-  const columnsCompany: ColumnDef<Company>[] = [
-    {
-      accessorKey: 'name',
-      header: 'Nama',
-      cell: ({ row }) => (
-        <div className='flex gap-1 items-center'>
-          <img
-            className='w-6 h-6 rounded-full object-cover object-center shadow'
-            src={BASE_URL + '/img/' + row.original.logo}
-          />
-          <p className='text-dark/70'>{row.original.name}</p>
-        </div>
-      ),
-    },
-    {
-      accessorKey: 'address',
-      header: 'Alamat',
-    },
-    {
-      accessorKey: 'phone',
-      header: 'Kontak',
-    },
-    {
-      accessorKey: 'email',
-      header: 'Email',
-    },
-    {
-      id: 'action',
-      cell: ({ row }) => (
-        <div className='flex justify-end'>
-          <DropdownEdit className='-translate-x-3'>
-            <DropdownMenuGroup>
-              <DropdownMenuItem
-                className='flex items-center gap-2 cursor-pointer'
-                onClick={() => {
-                  setSelectedId(row.original.id)
-                  handleDialog('companyDelete', true)
-                }}
-              >
-                <TrashIcon className='w-3.5 h-3.5 text-dark/50' />
-                Hapus
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownEdit>
-        </div>
-      ),
-    },
-  ]
-
   // HANDLE DIALOG
   type Dialog = {
     clientAdd: boolean
@@ -199,45 +83,53 @@ export default function Client() {
           </DropdownMenu>
         </div>
       </TitlePage>
-      <Tabs>
-        <Tab label='Klien'>
-          <FilterTable placeholder='Cari klien' />
-          <DataTable
-            columns={columnsClient}
-            data={qClient.data?.data?.data || []}
-            isLoading={qClient.isLoading}
-            withLoading
-            withPagination
-          />
-        </Tab>
-        <Tab label='Perusahaan'>
-          <FilterTable placeholder='Cari perusahaan' />
-          <DataTable
-            columns={columnsCompany}
-            data={qClientCompany.data?.data?.data || []}
-            isLoading={qClientCompany.isLoading}
-            withLoading
-            withPagination
-          />
-        </Tab>
-      </Tabs>
+      <div className='px-4'>
+        <div className='border border-line rounded-xl overflow-hidden pt-3.5'>
+          <Tabs>
+            <Tab label='Klien'>
+              <TableClient
+                setSelectedId={setSelectedId}
+                handleDialog={handleDialog}
+              />
+            </Tab>
+            <Tab label='Perusahaan'>
+              <TableCompany
+                setSelectedId={setSelectedId}
+                handleDialog={handleDialog}
+              />
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
       <DialogAddClient
+        selectedId={selectedId}
         open={dialog.clientAdd}
-        setOpen={(val) => handleDialog('clientAdd', val)}
+        setOpen={(val) => {
+          handleDialog('clientAdd', val)
+          setSelectedId(null)
+        }}
       />
       <DialogAddCompany
+        selectedId={selectedId}
         open={dialog.companyAdd}
-        setOpen={(val) => handleDialog('companyAdd', val)}
+        setOpen={(val) => {
+          handleDialog('companyAdd', val)
+          setSelectedId(null)
+        }}
       />
       <DialogDeleteCompany
         id={selectedId}
         open={dialog.companyDelete}
-        setOpen={(val) => handleDialog('companyDelete', val)}
+        setOpen={(val) => {
+          handleDialog('companyDelete', val)
+        }}
       />
       <DialogDeleteClient
         id={selectedId}
         open={dialog.clientDelete}
-        setOpen={(val) => handleDialog('clientDelete', val)}
+        setOpen={(val) => {
+          handleDialog('clientDelete', val)
+        }}
       />
     </DashboardLayout>
   )
