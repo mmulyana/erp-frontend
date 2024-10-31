e2ca23246064b8d1f9b6284ca9e3d80fd0dd9f27
mmulyana <mulyan.t20@gmail.com>
2024-11-01 03:47:00 +0700

    fix: key label

--- src/routes/dashboard/project/_component/detail/label-project.tsx
+++ src/routes/dashboard/project/_component/detail/label-project.tsx
@@ -95,6 +95,7 @@ export default function LabelProject({
     <div className={cn('flex flex-wrap gap-2', labels.length > 2 && 'pt-2')}>
       {labels?.map((item) => (
         <Label
+          key={`label-${item.id}`}
           color={item.label.color}
           name={item.label.name}
           suffix={
