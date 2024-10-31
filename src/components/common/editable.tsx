5d1dffb993ea50c0df68dd0525965be62b360002
mmulyana <mulyan.t20@gmail.com>
2024-10-31 22:55:58 +0700

    feat: enhance performance for editable component

--- src/components/common/editable.tsx
+++ src/components/common/editable.tsx
@@ -1,5 +1,12 @@
 import { cn } from '@/utils/cn'
-import React, { KeyboardEvent, useEffect, useRef, useState } from 'react'
+import React, {
+  KeyboardEvent,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  memo,
+} from 'react'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import { Pencil, Plus } from 'lucide-react'
@@ -22,157 +29,6 @@ type Props = {
   }[]
 }
 
-export const Editable = ({
-  isEdit,
-  defaultData,
-  className,
-  classNameInput,
-  onUpdate,
-  onEdit,
-  keyData,
-  type = 'text',
-  customData,
-  customEdit,
-  options,
-  ...props
-}: Props) => {
-  const [data, setData] = useState<null | string | number>(defaultData || null)
-  const ref = useRef<HTMLInputElement>(null)
-  const containerRef = useRef<HTMLDivElement>(null)
-  const [open, setOpen] = useState(false)
-
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      if (!data || !onUpdate || !onEdit) return
-
-      onUpdate?.(data as string | number)
-      handleBlur()
-    }
-  }
-
-  useEffect(() => {
-    if (defaultData) {
-      setData(defaultData)
-    }
-
-    return () => setData(null)
-  }, [defaultData])
-
-  useEffect(() => {
-    if (isEdit === keyData) {
-      if (type == 'select') {
-        setOpen(true)
-        return
-      }
-      ref.current?.focus()
-    }
-  }, [isEdit, keyData])
-
-  const handleBlur = () => {
-    if (onEdit) {
-      onEdit(null)
-    }
-    containerRef.current?.focus()
-  }
-
-  const FormType = (type: 'text' | 'select' | 'date' | 'custom') => {
-    switch (type) {
-      case 'custom':
-        return customEdit
-      case 'date':
-        return (
-          <div className='flex gap-2 items-center'>
-            <Input
-              onChange={(e) => setData(e.target.value)}
-              value={data || ''}
-              onKeyDown={handleKeyDown}
-              className={cn(
-                'shadow-none h-fit block p-0 pl-1 rounded-md border-blue-primary w-[112px]',
-                classNameInput
-              )}
-              ref={ref}
-              type='date'
-            />
-            <Button
-              variant='secondary'
-              className='h-6 font-normal text-sm rounded-md p-0 px-2 bg-gray-200'
-              onClick={() => {
-                onUpdate?.(data as string)
-                handleBlur()
-              }}
-            >
-              Simpan
-            </Button>
-          </div>
-        )
-      case 'select':
-        return (
-          <Select
-            open={open}
-            onOpenChange={setOpen}
-            onValueChange={(val) => {
-              onUpdate?.(val)
-              onEdit?.(null)
-              containerRef.current?.focus()
-            }}
-          >
-            <SelectTrigger className='h-fit p-0 border-none bg-gray-50 py-1 pr-3'>
-              <Button
-                variant='ghost'
-                className={cn('shadow-none h-6 p-0 pl-2', classNameInput)}
-              >
-                Pilih
-              </Button>
-            </SelectTrigger>
-            <SelectContent>
-              {options?.map((option) => (
-                <SelectItem key={option.value} value={option.value}>
-                  {option.name}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        )
-      case 'text':
-      default:
-        return (
-          <Input
-            onChange={(e) => setData(e.target.value)}
-            value={data || ''}
-            onKeyDown={handleKeyDown}
-            className={cn(
-              'shadow-none h-fit py-1 px-2 border border-blue-primary rounded-md',
-              classNameInput
-            )}
-            onBlur={handleBlur}
-            ref={ref}
-          />
-        )
-    }
-  }
-
-  return (
-    <div ref={containerRef} tabIndex={-1} style={{ outline: 'none' }}>
-      {isEdit === keyData ? (
-        FormType(type)
-      ) : (
-        <div
-          className={cn('text-dark cursor-pointer', className)}
-          onClick={() => onEdit && onEdit(keyData)}
-          {...props}
-        >
-          <RenderData
-            customData={customData}
-            defaultData={defaultData}
-            keyData={keyData}
-            onEdit={onEdit}
-          />
-        </div>
-      )}
-    </div>
-  )
-}
-
 type renderProps = {
   defaultData?: string | number | null
   customData?: (val: string | number) => React.ReactNode
@@ -180,36 +36,201 @@ type renderProps = {
   onEdit?: (val: string | null) => void
 }
 
-function RenderData({ customData, defaultData, keyData, onEdit }: renderProps) {
-  if (customData && defaultData) {
+const RenderData = memo(
+  ({ customData, defaultData, keyData, onEdit }: renderProps) => {
+    if (
+      (customData && defaultData) ||
+      (typeof defaultData == 'number' && defaultData === 0)
+    ) {
+      return (
+        <span className='inline-flex gap-2 items-center group'>
+          {customData?.(defaultData)}
+          <Pencil
+            size={14}
+            className='text-gray-400 hidden group-hover:block'
+          />
+        </span>
+      )
+    }
+
+    if (defaultData || (typeof defaultData == 'number' && defaultData === 0)) {
+      return (
+        <span className='inline-flex gap-2 items-center group'>
+          {defaultData}
+          <Pencil
+            size={14}
+            className='text-gray-400 hidden group-hover:block'
+          />
+        </span>
+      )
+    }
+
     return (
-      <span className='inline-flex gap-2 items-center group'>
-        {customData?.(defaultData)}
-        <Pencil size={14} className='text-gray-400 hidden group-hover:block' />
-      </span>
+      <Button
+        variant='ghost'
+        className='font-normal p-0 hover:bg-transparent inline-flex items-center text-gray-400 text-sm h-fit pl-4 relative'
+        onClick={() => onEdit?.(keyData)}
+      >
+        <Plus
+          size={12}
+          className='absolute left-0.5 top-[50%] -translate-y-1/2'
+        />
+        Tambah
+      </Button>
     )
   }
+)
 
-  if (defaultData) {
+export const Editable = memo(
+  ({
+    isEdit,
+    defaultData,
+    className,
+    classNameInput,
+    onUpdate,
+    onEdit,
+    keyData,
+    type = 'text',
+    customData,
+    customEdit,
+    options,
+    ...props
+  }: Props) => {
+    const [data, setData] = useState<null | string | number>(
+      () => defaultData || null
+    )
+    const ref = useRef<HTMLInputElement>(null)
+    const containerRef = useRef<HTMLDivElement>(null)
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+      if (defaultData || typeof defaultData === 'number') {
+        setData(defaultData)
+      }
+      return () => setData(null)
+    }, [defaultData])
+
+    const handleKeyDown = useCallback(
+      (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+          if (!data || !onUpdate || !onEdit) return
+          onUpdate?.(data as string | number)
+          handleBlur()
+        }
+      },
+      [data, onUpdate, onEdit]
+    )
+
+    const handleBlur = useCallback(() => {
+      onEdit?.(null)
+      containerRef.current?.focus()
+    }, [onEdit])
+
+    useEffect(() => {
+      if (isEdit === keyData) {
+        if (type === 'select') {
+          setOpen(true)
+          return
+        }
+        ref.current?.focus()
+      }
+    }, [isEdit, keyData, type])
+
+    const FormType = (type: 'text' | 'select' | 'date' | 'custom') => {
+      switch (type) {
+        case 'custom':
+          return customEdit
+        case 'date':
+          return (
+            <div className='flex gap-2 items-center'>
+              <Input
+                onChange={(e) => setData(e.target.value)}
+                value={data || ''}
+                onKeyDown={handleKeyDown}
+                className={cn(
+                  'shadow-none h-fit block p-0 pl-1 rounded-md border-blue-primary w-[112px]',
+                  classNameInput
+                )}
+                ref={ref}
+                type='date'
+              />
+              <Button
+                variant='secondary'
+                className='h-6 font-normal text-sm rounded-md p-0 px-2 bg-gray-200'
+                onClick={() => {
+                  onUpdate?.(data as string)
+                  handleBlur()
+                }}
+              >
+                Simpan
+              </Button>
+            </div>
+          )
+        case 'select':
+          return (
+            <Select
+              open={open}
+              onOpenChange={setOpen}
+              onValueChange={(val) => {
+                onUpdate?.(val)
+                onEdit?.(null)
+                containerRef.current?.focus()
+              }}
+            >
+              <SelectTrigger className='h-fit p-0 border-none bg-gray-50 py-1 pr-3'>
+                <Button
+                  variant='ghost'
+                  className={cn('shadow-none h-6 p-0 pl-2', classNameInput)}
+                >
+                  Pilih
+                </Button>
+              </SelectTrigger>
+              <SelectContent>
+                {options?.map((option) => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          )
+        case 'text':
+        default:
+          return (
+            <Input
+              onChange={(e) => setData(e.target.value)}
+              value={data || ''}
+              onKeyDown={handleKeyDown}
+              className={cn(
+                'shadow-none h-fit py-1 px-2 border border-blue-primary rounded-md',
+                classNameInput
+              )}
+              onBlur={handleBlur}
+              ref={ref}
+            />
+          )
+      }
+    }
+
     return (
-      <span className='inline-flex gap-2 items-center group'>
-        {defaultData}
-        <Pencil size={14} className='text-gray-400 hidden group-hover:block' />
-      </span>
+      <div ref={containerRef} tabIndex={-1} style={{ outline: 'none' }}>
+        {isEdit === keyData ? (
+          FormType(type)
+        ) : (
+          <div
+            className={cn('text-dark cursor-pointer', className)}
+            onClick={() => onEdit && onEdit(keyData)}
+            {...props}
+          >
+            <RenderData
+              customData={customData}
+              defaultData={defaultData}
+              keyData={keyData}
+              onEdit={onEdit}
+            />
+          </div>
+        )}
+      </div>
     )
   }
-
-  return (
-    <Button
-      variant='ghost'
-      className='font-normal p-0 hover:bg-transparent inline-flex items-center text-gray-400 text-sm h-fit pl-4 relative'
-      onClick={() => onEdit?.(keyData)}
-    >
-      <Plus
-        size={12}
-        className='absolute left-0.5 top-[50%] -translate-y-1/2'
-      />
-      Tambah
-    </Button>
-  )
-}
+)
