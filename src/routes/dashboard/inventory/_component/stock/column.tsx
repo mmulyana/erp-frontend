b71790513c1046bdaea4bfd0715156e3c123d2d5
mmulyana <mulyan.t20@gmail.com>
2024-11-02 11:08:38 +0700

    feat: update

--- src/routes/dashboard/inventory/_component/stock/column.tsx
+++ src/routes/dashboard/inventory/_component/stock/column.tsx
@@ -1,19 +1,22 @@
+import DropdownEdit from '@/components/common/dropdown-edit'
+import { formatToRupiah } from '@/utils/formatCurrency'
 import { generatePath, Link } from 'react-router-dom'
 import Overlay from '@/components/common/overlay'
 import { ColumnDef } from '@tanstack/react-table'
 import { Transaction } from '@/utils/types/api'
 import { PATH } from '@/utils/constant/_paths'
-import { Button } from '@/components/ui/button'
-import { Ellipsis } from 'lucide-react'
 import { format } from 'date-fns'
-import { formatToRupiah } from '@/utils/formatCurrency'
+import {
+  DropdownMenuGroup,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu'
 
 export const column: ColumnDef<Transaction>[] = [
   {
     id: 'name',
     header: 'Nama',
     cell: ({ row }) => {
-      const detail = row.original.good.name
+      const detail = row.original.good.name || ''
       return (
         <Overlay
           className='w-fit pr-2.5'
@@ -61,24 +64,28 @@ export const column: ColumnDef<Transaction>[] = [
   {
     id: 'supplier',
     header: 'Toko',
-    cell: ({ row }) => <p>{row.original.supplier.name}</p>,
+    cell: ({ row }) => <p>{row.original?.supplier?.name}</p>,
   },
   {
     accessorKey: 'date',
     header: 'Tanggal',
-    cell: ({ row }) => <p>{format(row.original.date, 'dd MMMM yyyy')}</p>,
+    cell: ({ row }) => <p>{format(row.original.date, 'dd/MM/yyyy')}</p>,
   },
   {
     id: 'action',
-    cell: () => (
-      <div className='flex justify-end'>
-        <Button
-          variant='outline'
-          className='p-0.5 rounded-[6px] h-5 w-5 border-[#EFF0F2]'
-        >
-          <Ellipsis className='w-4 h-4 text-[#313951]' />
-        </Button>
-      </div>
-    ),
+    cell: () => {
+      return (
+        <DropdownEdit className='flex justify-end'>
+          <DropdownMenuGroup>
+            <DropdownMenuItem className='rounded-none text-sm text-dark/70 cursor-pointer'>
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem className='rounded-none text-sm text-dark/70 cursor-pointer'>
+              Hapus
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownEdit>
+      )
+    },
   },
 ]
