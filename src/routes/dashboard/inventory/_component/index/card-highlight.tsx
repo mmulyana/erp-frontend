26c4c87525f69ed398d7817a4408ff62033ee1bf
mmulyana <mulyan.t20@gmail.com>
2024-11-03 03:16:14 +0700

    feat: update

--- src/routes/dashboard/inventory/_component/index/card-highlight.tsx
+++ src/routes/dashboard/inventory/_component/index/card-highlight.tsx
@@ -1,7 +1,14 @@
-import { Package } from 'lucide-react'
+import { useGoodsLowStock, useGoodsOutOfStock } from '@/hooks/api/use-goods'
+import { useTransactionBorrowed } from '@/hooks/api/use-transaction'
+import { useApiData } from '@/hooks/use-api-data'
 import ButtonLink from '../button-link'
+import { Package } from 'lucide-react'
 
 export default function CardHighlight() {
+  const { data: borroweds } = useApiData(useTransactionBorrowed())
+  const { data: lowStocks } = useApiData(useGoodsLowStock())
+  const { data: outOfStocks } = useApiData(useGoodsOutOfStock())
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 rounded-[8px] border border-line h-fit'>
       <div className='py-4 px-6 w-full h-full'>
@@ -11,7 +18,7 @@ export default function CardHighlight() {
         <div className='mt-2.5 flex flex-col gap-3.5'>
           <p className='text-sm text-dark/50'>Stok dipinjam</p>
           <div className='w-full flex justify-between items-center'>
-            <p className='text-lg font-medium text-dark'>20</p>
+            <p className='text-lg font-medium text-dark'>{borroweds.length}</p>
             <ButtonLink />
           </div>
         </div>
@@ -24,7 +31,7 @@ export default function CardHighlight() {
         <div className='mt-2.5 flex flex-col gap-3.5'>
           <p className='text-sm text-dark/50'>Stok Hampir Habis</p>
           <div className='w-full flex justify-between items-center'>
-            <p className='text-lg font-medium text-dark'>20</p>
+            <p className='text-lg font-medium text-dark'>{lowStocks.length}</p>
             <ButtonLink />
           </div>
         </div>
@@ -36,7 +43,9 @@ export default function CardHighlight() {
         <div className='mt-2.5 flex flex-col gap-3.5'>
           <p className='text-sm text-dark/50'>Stok Habis</p>
           <div className='w-full flex justify-between items-center'>
-            <p className='text-lg font-medium text-dark'>20</p>
+            <p className='text-lg font-medium text-dark'>
+              {outOfStocks.length}
+            </p>
             <ButtonLink />
           </div>
         </div>
