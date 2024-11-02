e4addd690e360c9b77a30cd7538152beef5dc888
mmulyana <mulyan.t20@gmail.com>
2024-11-03 03:16:09 +0700

    feat: update

--- src/hooks/api/use-transaction.ts
+++ src/hooks/api/use-transaction.ts
@@ -27,6 +27,25 @@ export const useTransaction = (params?: Params) => {
     queryKey: [KEYS.TRANSACTION, params?.type, params],
   })
 }
+export const useDetailTransaction = ({
+  id,
+  enabled,
+}: {
+  id?: number | null
+  enabled: boolean
+}) => {
+  return useQuery({
+    queryFn: async (): Promise<
+      AxiosResponse<{
+        data: Transaction[]
+      }>
+    > => {
+      return await http(`${URLS.INVENTORY_TRANSACTION}/${id}`)
+    },
+    queryKey: [KEYS.TRANSACTION, id],
+    enabled,
+  })
+}
 
 export const useCreateTransaction = () => {
   const queryClient = useQueryClient()
@@ -61,5 +80,83 @@ export const useCreateTransaction = () => {
         toast.error('Terjadi kesalahan pada aplikasi')
       }
     },
+  })
+}
+export const useUpdateTransaction = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      id,
+      payload,
+    }: {
+      id: number
+      payload: Partial<CreateTransaction>
+    }): Promise<AxiosResponse<IApi<Transaction>>> => {
+      return await http.patch(`${URLS.INVENTORY_TRANSACTION}/${id}`, payload)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.TRANSACTION] })
+      queryClient.invalidateQueries({
+        queryKey: [KEYS.TRANSACTION, data.data.data?.type],
+      })
+      toast.success(data.data.message)
+    },
+    onError: (error: AxiosError<ApiError>) => {
+      if (error.response) {
+        const errorMessage =
+          error.response.data.message || 'Terjadi kesalahan pada server'
+        if (errorMessage.includes('tidak mencukupi')) {
+          toast.error(errorMessage)
+        } else {
+          toast.error(errorMessage)
+        }
+      } else if (error.request) {
+        toast.error('Tidak dapat terhubung ke server')
+      } else {
+        toast.error('Terjadi kesalahan pada aplikasi')
+      }
+    },
+  })
+}
+export const useDeleteTransaction = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async ({
+      id,
+    }: {
+      id: number
+    }): Promise<AxiosResponse<IApi<Transaction>>> => {
+      return await http.delete(`${URLS.INVENTORY_TRANSACTION}/${id}`)
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [KEYS.TRANSACTION] })
+      toast.success(data.data.message)
+    },
+    onError: (_: AxiosError<ApiError>) => {
+      //   if (error.response) {
+      //     const errorMessage =
+      //       error.response.data.message || 'Terjadi kesalahan pada server'
+      //     if (errorMessage.includes('tidak mencukupi')) {
+      //       toast.error(errorMessage)
+      //     } else {
+      //       toast.error(errorMessage)
+      //     }
+      //   } else if (error.request) {
+      //     toast.error('Tidak dapat terhubung ke server')
+      //   } else {
+      //     toast.error('Terjadi kesalahan pada aplikasi')
+      //   }
+    },
+  })
+}
+
+export const useTransactionBorrowed = () => {
+  return useQuery({
+    queryFn: async (): Promise<AxiosResponse<IApi<Transaction[]>>> => {
+      return await http(URLS.INVENTORY_TRANSACTION + '/data/borrowed')
+    },
+    queryKey: [KEYS.TRANSACTION_BORROWED],
   })
 }
