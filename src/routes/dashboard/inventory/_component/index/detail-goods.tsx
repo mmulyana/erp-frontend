2bba33623f61d7720cdefb625369f235a54595cd
mmulyana <mulyan.t20@gmail.com>
2024-11-03 02:29:37 +0700

    feat: update

--- src/routes/dashboard/inventory/_component/index/detail-goods.tsx
+++ src/routes/dashboard/inventory/_component/index/detail-goods.tsx
@@ -1,17 +1,21 @@
-import { Editable } from '@/components/common/editable'
-import PhotoProfile from '@/components/common/photo-profile'
-import { Tab, Tabs } from '@/components/tab'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetTitle,
-} from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetTitle } from '@/components/ui/sheet'
 import { useDetailGoods, useUpdateGoods } from '@/hooks/api/use-goods'
-import { BASE_URL } from '@/utils/constant/_urls'
-import { atom, useAtom } from 'jotai'
+import PhotoProfile from '@/components/common/photo-profile'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Editable } from '@/components/common/editable'
 import { useCallback, useMemo, useState } from 'react'
+import { BASE_URL } from '@/utils/constant/_urls'
+import { createGoods } from '@/utils/types/form'
+import { Button } from '@/components/ui/button'
+import { Ellipsis } from 'lucide-react'
+import { atom, useAtom } from 'jotai'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 export const selectedGoodAtom = atom<{
   open: boolean
@@ -26,8 +30,7 @@ export default function DetailGoods() {
     id: selected?.id,
     enabled: selected?.open || false,
   })
-  const good = useMemo(() => data?.data.data || {}, [data, isLoading])
-  console.log(good)
+  const good = useMemo(() => data?.data.data, [data, isLoading])
 
   const [edit, setEdit] = useState<string | null>('')
   const isEdit = useMemo(() => edit, [edit])
@@ -42,6 +45,12 @@ export default function DetailGoods() {
     setSelected(null)
   }
 
+  const handleUpdate = (payload: Partial<createGoods>) => {
+    if (!selected?.id) return
+    update({ id: selected.id, payload })
+    setEdit(null)
+  }
+
   return (
     <Sheet open={selected?.open} onOpenChange={() => onClose()} modal>
       <SheetContent className='max-w-[512px] p-0'>
@@ -51,15 +60,45 @@ export default function DetailGoods() {
           </div>
         </SheetTitle>
         <ScrollArea className='h-[calc(100vh-48px)]'>
-          {/* <PhotoProfile/> */}
-          <Tabs>
-            <Tab label='Pegawai'>
-              <div className='p-4'></div>
-            </Tab>
-            <Tab label='Transaksi'>
-              <div className='p-4'></div>
-            </Tab>
-          </Tabs>
+          <div className='mt-4 px-4 pb-8'>
+            <PhotoProfile
+              defaultPreview={
+                good?.photoUrl ? BASE_URL + '/img/' + good?.photoUrl : null
+              }
+              onUpdate={(photo) => {
+                handleUpdate({ photo })
+              }}
+              onRemove={() => {
+                handleUpdate({ photoUrl: null })
+              }}
+            />
+            <div className='relative pr-7'>
+              <Editable
+                isEdit={isEdit}
+                keyData='name'
+                onEdit={onEdit}
+                defaultData={good?.name}
+                className='text-lg font-medium text-dark mt-4 py-1 px-1'
+                classNameInput='text-lg font-medium text-dark mt-4 py-1 px-1'
+                onUpdate={(name) => handleUpdate({ name: String(name) })}
+              />
+              <DropdownMenu modal>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant='outline'
+                    className='absolute flex justify-center items-center top-1/2 -translate-y-1/2 right-0 w-6 h-6 p-0'
+                  >
+                    <Ellipsis size={14} />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className='min-w-fit -translate-x-4'>
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>Hapus</DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
         </ScrollArea>
       </SheetContent>
     </Sheet>
