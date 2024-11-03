55fc874ccad3fc8dd5e72c2f90ad70d048145f5a
mmulyana <mulyan.t20@gmail.com>
2024-11-04 05:59:43 +0700

    feat: update

--- src/routes/dashboard/inventory/stock.tsx
+++ src/routes/dashboard/inventory/stock.tsx
@@ -1,3 +1,4 @@
+import DetailTransaction from './_component/stock/detail-transaction'
 import StockBorrowed from './_component/stock/stock-borrowed'
 import StockOpname from './_component/stock/stock-opname'
 import { DashboardLayout } from '../_component/layout'
@@ -6,9 +7,20 @@ import StockIn from './_component/stock/stock-in'
 import TitlePage from '../_component/title-page'
 import { Tab, Tabs } from '@/components/tab'
 import { Package } from 'lucide-react'
-import DetailTransaction from './_component/stock/detail-transaction'
+import { atom, useAtom } from 'jotai'
+import AlertDialogV1 from '@/components/common/alert-dialog-v1'
+import { useDeleteTransaction } from '@/hooks/api/use-transaction'
+
+export const dialogDeleteTransaction = atom<{
+  id: number
+  open: boolean
+} | null>(null)
 
 export default function Stock() {
+  const { mutate } = useDeleteTransaction()
+
+  const [selected, setSelected] = useAtom(dialogDeleteTransaction)
+
   return (
     <>
       <DashboardLayout>
@@ -33,6 +45,16 @@ export default function Stock() {
           </Tab>
         </Tabs>
       </DashboardLayout>
+      <AlertDialogV1
+        open={selected?.open}
+        setOpen={() => setSelected(null)}
+        title='Hati-hati! Tindakan ini tidak bisa dibatalkan'
+        body='Data transaksi akan dihapus dalam sistem dan dikembalikan ke nilai sebelumnya'
+        onConfirm={() => {
+          if (!selected?.id) return
+          mutate({ id: selected?.id }, { onSuccess: () => setSelected(null) })
+        }}
+      />
       <DetailTransaction />
     </>
   )
