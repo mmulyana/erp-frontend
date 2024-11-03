e690f3aaf8bef71ee7d4b2a0f0b3ef0dd98f531a
mmulyana <mulyan.t20@gmail.com>
2024-11-03 23:31:00 +0700

    feat: update

--- src/routes/dashboard/inventory/stock.tsx
+++ src/routes/dashboard/inventory/stock.tsx
@@ -6,33 +6,34 @@ import StockIn from './_component/stock/stock-in'
 import TitlePage from '../_component/title-page'
 import { Tab, Tabs } from '@/components/tab'
 import { Package } from 'lucide-react'
-import { atom } from 'jotai'
-
-export const selectedAtom = atom<{ open: boolean; id: number } | null>(null)
+import DetailTransaction from './_component/stock/detail-transaction'
 
 export default function Stock() {
   return (
-    <DashboardLayout>
-      <TitlePage className='mb-2'>
-        <div className='flex gap-2 items-center'>
-          <Package className='text-[#989CA8]' />
-          <p className='text-dark font-medium'>Kelola Barang</p>
-        </div>
-      </TitlePage>
-      <Tabs>
-        <Tab label='Barang Masuk'>
-          <StockIn />
-        </Tab>
-        <Tab label='Barang Keluar'>
-          <StockOut />
-        </Tab>
-        <Tab label='Opname'>
-          <StockOpname />
-        </Tab>
-        <Tab label='Peminjaman'>
-          <StockBorrowed />
-        </Tab>
-      </Tabs>
-    </DashboardLayout>
+    <>
+      <DashboardLayout>
+        <TitlePage className='mb-2'>
+          <div className='flex gap-2 items-center'>
+            <Package className='text-[#989CA8]' />
+            <p className='text-dark font-medium'>Kelola Barang</p>
+          </div>
+        </TitlePage>
+        <Tabs>
+          <Tab label='Barang Masuk'>
+            <StockIn />
+          </Tab>
+          <Tab label='Barang Keluar'>
+            <StockOut />
+          </Tab>
+          <Tab label='Opname'>
+            <StockOpname />
+          </Tab>
+          <Tab label='Peminjaman'>
+            <StockBorrowed />
+          </Tab>
+        </Tabs>
+      </DashboardLayout>
+      <DetailTransaction />
+    </>
   )
 }
