1c9f7cbd9e820b18dce86bd11dc0d993de8f31fb
mmulyana <mulyan.t20@gmail.com>
2024-11-04 02:58:35 +0700

    fix: update

--- src/routes/dashboard/inventory/_component/stock/detail-transaction.tsx
+++ src/routes/dashboard/inventory/_component/stock/detail-transaction.tsx
@@ -1,32 +1,44 @@
 import { Form, FormField, FormItem, FormLabel } from '@/components/ui/form'
-import { useDetailTransaction } from '@/hooks/api/use-transaction'
+import {
+  useDetailTransaction,
+  useUpdateTransaction,
+} from '@/hooks/api/use-transaction'
 import { useInventoryData } from '../../_hook/use-inventory-data'
 import Modal, { ModalContainer } from '@/components/modal-v2'
 import SelectV1 from '@/components/common/select/select-v1'
 import { CommandItem } from '@/components/ui/command'
 import { useApiData } from '@/hooks/use-api-data'
 import { BASE_URL } from '@/utils/constant/_urls'
-import { Transaction } from '@/utils/types/api'
 import { Input } from '@/components/ui/input'
 import { useForm } from 'react-hook-form'
 import { atom, useAtom } from 'jotai'
 import { useEffect } from 'react'
+import { updateTransaction } from '@/utils/types/form'
 
 export const dialogDetailTransaction = atom<{
   open: boolean
   id: number
 } | null>(null)
 
-type FormValues = Partial<Transaction>
+type FormValues = {
+  supplierId: number | null
+  projectId: number | null
+  price: number | null
+  date: string
+  qty: number
+  goodsId: number
+  type: string
+}
 
 export default function DetailTransaction() {
+  // HANDLE DIALOG
   const [selected, setSelected] = useAtom(dialogDetailTransaction)
   const onClose = () => {
     setSelected(null)
   }
 
+  // HANDLE GET DATA
   const { goods, suppliers } = useInventoryData()
-
   const { data, isLoading } = useApiData(
     useDetailTransaction({
       id: selected?.id,
@@ -34,22 +46,41 @@ export default function DetailTransaction() {
     })
   )
 
+  // HANDLE FORM
+  const { mutate: update } = useUpdateTransaction()
+
   const form = useForm<FormValues>()
 
   const submit = (data: FormValues) => {
-    console.log(data)
+    if (!selected?.id) return
+    const payload: updateTransaction = {
+      supplierId: Number(data.supplierId),
+      projectId: Number(data.projectId),
+      price: Number(data.price),
+      date: new Date(data.date),
+      qty: Number(data.qty),
+    }
+
+    update(
+      { id: selected?.id, payload },
+      {
+        onSuccess: () => {
+          form.reset()
+          onClose()
+        },
+      }
+    )
   }
 
   useEffect(() => {
     if (data && !isLoading && !!selected?.open) {
       form.reset({
-        goodsId: data.goodsId,
-        date: data.date,
-        isReturned: data.isReturned,
-        price: data.price,
-        projectId: data.projectId,
-        qty: data.qty,
-        supplierId: data.supplierId,
+        goodsId: Number(data.goodsId),
+        date: new Date(data.date).toISOString().split('T')[0],
+        price: Number(data.price),
+        projectId: Number(data.projectId),
+        qty: Number(data.qty),
+        supplierId: Number(data.supplierId),
         type: data.type,
       })
     }
@@ -67,7 +98,7 @@ export default function DetailTransaction() {
         <form onSubmit={form.handleSubmit(submit)}>
           <ModalContainer setOpen={onClose}>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
-              <div className='col-span-2'>
+              <div className='col-span-1 md:col-span-2'>
                 <FormItem>
                   <FormLabel>Barang</FormLabel>
                   <SelectV1
@@ -108,19 +139,37 @@ export default function DetailTransaction() {
                 control={form.control}
                 name='qty'
                 label='Kuantitas'
-                render={({ field }) => <Input {...field} />}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    value={field.value ?? ''}
+                    onChange={(e) => {
+                      const value = e.target.value
+                      field.onChange(value === '' ? null : Number(value))
+                    }}
+                  />
+                )}
               />
               <FormField
                 control={form.control}
                 name='price'
                 label='Harga'
-                render={({ field }) => <Input {...field} />}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    value={field.value ?? ''}
+                    onChange={(e) => {
+                      const value = e.target.value
+                      field.onChange(value === '' ? null : Number(value))
+                    }}
+                  />
+                )}
               />
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-              <FormItem>
-                <FormLabel>Supplier</FormLabel>
-                {type == 'in' && (
+              {type == 'in' && (
+                <FormItem>
+                  <FormLabel>Supplier</FormLabel>
                   <SelectV1
                     name='supplierId'
                     preview={(val) => {
@@ -153,15 +202,22 @@ export default function DetailTransaction() {
                       </CommandItem>
                     ))}
                   </SelectV1>
-                )}
-              </FormItem>
+                </FormItem>
+              )}
 
               <div className='col-start-2'>
                 <FormField
+                  label='Tanggal'
                   control={form.control}
-                  name='price'
-                  label='Harga'
-                  render={({ field }) => <Input {...field} />}
+                  name='date'
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      type='date'
+                      className='block'
+                      value={field.value || ''} // Ensure value is always a string
+                    />
+                  )}
                 />
               </div>
             </div>
