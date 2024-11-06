e88b84a0027d13d922956817087f2e9c01904252
mmulyana <mulyan.t20@gmail.com>
2024-11-06 10:15:58 +0700

    feat: update

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -12,10 +12,13 @@ import CardProject from '@/components/card-project'
 import ExpireCertif from './hris/employee/_component/detail/expire-certif'
 import ExpireSafety from './hris/employee/_component/detail/expire-safety'
 
+import { useDashboardTotal } from './_hook/use-dashboard'
+
 export default function Dashboard() {
   useTitle([{ name: 'Dashboard', path: PATH.DASHBOARD_OVERVIEW }])
 
   const { data } = useApiData(useProjects())
+  const { data: dashboard } = useApiData(useDashboardTotal())
 
   return (
     <DashboardLayout>
@@ -25,27 +28,27 @@ export default function Dashboard() {
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
               <div className='border border-line rounded-2xl flex items-center gap-5 p-2'>
                 <div className='h-14 w-14 rounded-[10px] bg-green-primary/5 flex justify-center items-center'>
-                  <Users2 className='text-green-primary' size={24} />
+                  <Users2 className='text-green-primary' size={20} />
                 </div>
                 <div className='flex flex-col gap-1'>
                   <p className='text-dark/50 leading-none text-sm'>
                     Total Pegawai
                   </p>
-                  <p className='text-dark font-medium text-2xl leading-none'>
-                    21
+                  <p className='text-dark font-medium text-xl leading-none'>
+                    {dashboard?.total.employee}
                   </p>
                 </div>
               </div>
               <div className='border border-line rounded-2xl flex items-center gap-5 p-2'>
                 <div className='h-14 w-14 rounded-[10px] bg-blue-primary/5 flex justify-center items-center'>
-                  <HardHat className='text-blue-primary' size={24} />
+                  <HardHat className='text-blue-primary' size={20} />
                 </div>
                 <div className='flex flex-col gap-1'>
                   <p className='text-dark/50 leading-none text-sm'>
-                    Total Pegawai
+                    Total Proyek
                   </p>
-                  <p className='text-dark font-medium text-2xl leading-none'>
-                    21
+                  <p className='text-dark font-medium text-xl leading-none'>
+                    {dashboard?.total.project}
                   </p>
                 </div>
               </div>
