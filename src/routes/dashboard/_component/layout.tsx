410f0aa042c3eb5d7a393eacc69e34ee16f8639b
mmulyana <mulyan.t20@gmail.com>
2024-11-06 12:56:41 +0700

    feat: add sidebar provider

--- src/routes/dashboard/_component/layout.tsx
+++ src/routes/dashboard/_component/layout.tsx
@@ -1,22 +1,18 @@
-import { useMediaQuery } from '@uidotdev/usehooks'
-import Header from './header'
-import Sidebar from './sidebar'
-import { cn } from '@/utils/cn'
-import { useState } from 'react'
+import { SidebarProvider } from '@/components/ui/sidebar'
+
 import Setting from './setting/setting'
+import Sidebar from './sidebar'
+import Header from './header'
 
 export function DashboardLayout({ children }: React.PropsWithChildren) {
-  const [open, setOpen] = useState(false)
-  const isSmall = useMediaQuery('only screen and (max-width : 768px)')
-
   return (
-    <>
-      <Sidebar open={open} setOpen={setOpen} />
-      <div className={cn(isSmall ? 'pl-0' : 'pl-[264px]')}>
+    <SidebarProvider>
+      <Sidebar />
+      <main className='flex-grow'>
         <Header />
-        <div>{children}</div>
-      </div>
+        {children}
+      </main>
       <Setting />
-    </>
+    </SidebarProvider>
   )
 }
