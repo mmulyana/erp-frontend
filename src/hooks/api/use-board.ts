c57c0adb1bbf96d755b28c2a19d96804b3f1d2b0
mmulyana <mulyan.t20@gmail.com>
2024-11-07 12:52:50 +0700

    feat: update

--- src/hooks/api/use-board.ts
+++ src/hooks/api/use-board.ts
@@ -2,10 +2,12 @@ import { useQuery } from '@tanstack/react-query'
 import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
+import { AxiosResponse } from 'axios'
+import { Board, IApi } from '@/utils/types/api'
 
 export const useBoards = () => {
   return useQuery({
-    queryFn: async () => {
+    queryFn: async (): Promise<AxiosResponse<IApi<Board[]>>> => {
       return await http(URLS.KANBAN_BOARDD)
     },
     queryKey: [KEYS.KANBAN_BOARD],
