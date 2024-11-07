d56c7384bb8d676aca61b10400b9aa8d747b18f7
mmulyana <mulyan.t20@gmail.com>
2024-11-07 21:11:21 +0700

    feat: update

---
+++ src/routes/dashboard/project/_component/overview/project-by-status.tsx
@@ -0,0 +1,82 @@
+import { RadialBarChart, PolarRadiusAxis, Label, RadialBar } from 'recharts'
+
+import { useTotalProject } from '@/hooks/api/use-project'
+import { useChartProject } from '@/hooks/api/use-chart'
+import { useApiData } from '@/hooks/use-api-data'
+
+import { Card, CardBody, CardHead } from '@/components/common/card-v1'
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
+
+export default function ProjectByStatusChart() {
+  const { data } = useApiData(useChartProject())
+  const { data: totalData } = useApiData(useTotalProject())
+
+  if (!data?.chartData || !data?.chartConfig) {
+    return null
+  }
+
+  return (
+    <Card>
+      <CardHead className='border-b border-line h-fit py-2 px-2'>
+        <p className='text-sm text-dark'>Proyek per status</p>
+      </CardHead>
+      <CardBody className='px-2 h-28'>
+        <ChartContainer
+          config={data?.chartConfig}
+          className='mx-auto aspect-square w-full max-w-[250px]'
+        >
+          <RadialBarChart
+            data={data?.chartData || []}
+            endAngle={180}
+            innerRadius={80}
+            outerRadius={140}
+          >
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+                    return (
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor='middle'>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) - 16}
+                          className='fill-foreground text-2xl font-bold'
+                        >
+                          {totalData?.active}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 4}
+                          className='fill-muted-foreground'
+                        >
+                          Proyek
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </PolarRadiusAxis>
+            {Object.values(data.chartConfig).map(({ label, color }) => (
+              <RadialBar
+                dataKey={label}
+                stackId='a'
+                fill={color}
+                cornerRadius={5}
+                className='stroke-white stroke-[4px]'
+              />
+            ))}
+          </RadialBarChart>
+        </ChartContainer>
+      </CardBody>
+    </Card>
+  )
+}
