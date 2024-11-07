6dda59cb2b88f65a9c7eee8c4c12bde699071e63
mmulyana <mulyan.t20@gmail.com>
2024-11-07 08:00:15 +0700

    feat: update

--- src/hooks/api/use-cash-advance.ts
+++ src/hooks/api/use-cash-advance.ts
@@ -1,8 +1,10 @@
 import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
+import { IApi } from '@/utils/types/api'
 import { Pagination } from '@/utils/types/common'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { AxiosResponse } from 'axios'
 import { toast } from 'sonner'
 
 type ParamsLeaves = Pagination & {
@@ -31,6 +33,7 @@ export const useCreateCashAdvance = () => {
     },
     onSuccess: (data) => {
       queryClient.invalidateQueries({ queryKey: [KEYS.CASH_ADVANCES] })
+      queryClient.invalidateQueries({ queryKey: [KEYS.CASH_ADVANCES_TOTAL] })
       toast.success(data.data.message)
     },
   })
@@ -48,6 +51,15 @@ export const useDeleteCashAdvances = () => {
         queryKey: [KEYS.CASH_ADVANCES],
       })
       toast.success(data?.data?.message)
+    },
+  })
+}
+
+export const useTotalCashAdvance = () => {
+  return useQuery({
+    queryKey: [KEYS.CASH_ADVANCES_TOTAL],
+    queryFn: async (): Promise<AxiosResponse<IApi<{ total: number }>>> => {
+      return await http(URLS.CASH_ADVANCES + '/data/total')
     },
   })
 }
