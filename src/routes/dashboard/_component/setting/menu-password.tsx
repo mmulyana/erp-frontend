da219da60eeef2dda544841955b5bd0044fef7bb
mmulyana <mulyan.t20@gmail.com>
2024-11-10 03:02:02 +0700

    feat: update

--- src/routes/dashboard/_component/setting/menu-password.tsx
+++ src/routes/dashboard/_component/setting/menu-password.tsx
@@ -1,25 +1,35 @@
+import { userAtom } from '@/atom/auth'
 import InputPassword from '@/components/common/input-password'
 import { Button } from '@/components/ui/button'
 import { Form } from '@/components/ui/form'
+import { useUpdatePassword } from '@/hooks/api/use-account'
+import { UpdatePasswordDto } from '@/utils/schema/account'
+import { useAtomValue } from 'jotai'
 import { useForm } from 'react-hook-form'
 
-type Account = {
-  old_password: ''
-  new_password: ''
-  confirm_password: ''
-}
-
 export default function MenuPassword() {
-  const form = useForm<Account>({
-    defaultValues: {},
+  const user = useAtomValue(userAtom)
+
+  const { mutate } = useUpdatePassword()
+
+  const form = useForm<UpdatePasswordDto>({
+    defaultValues: {
+      oldPassword: '',
+      newPassword: '',
+    },
   })
+
+  const submit = (payload: UpdatePasswordDto) => {
+    if (!user?.id) return
+    mutate({ id: user.id, payload })
+  }
+
   return (
     <Form {...form}>
-      <form>
+      <form onSubmit={form.handleSubmit(submit)}>
         <div className='flex flex-col gap-6 max-w-sm p-6'>
-          <InputPassword label='Password lama' name='old_password' />
-          <InputPassword label='Password Baru' name='new_password' />
-          <InputPassword label='Konfirmasi Password' name='confirm_password' />
+          <InputPassword label='Password lama' name='oldPassword' />
+          <InputPassword label='Password Baru' name='newPassword' />
           <Button className='w-fit'>Perbarui</Button>
         </div>
       </form>
