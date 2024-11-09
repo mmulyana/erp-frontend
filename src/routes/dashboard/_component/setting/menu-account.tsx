6fa7d43911c194083e318a751a0cef533714a630
mmulyana <mulyan.t20@gmail.com>
2024-11-09 23:28:04 +0700

    feat: update

--- src/routes/dashboard/_component/setting/menu-account.tsx
+++ src/routes/dashboard/_component/setting/menu-account.tsx
@@ -1,23 +1,48 @@
+import { userAtom } from '@/atom/auth'
 import PhotoProfile from '@/components/common/photo-profile'
 import { Button } from '@/components/ui/button'
 import { Form, FormField } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useUpdateAccount } from '@/hooks/api/use-account'
+import { User } from '@/utils/types/api'
+import { useAtomValue } from 'jotai'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
-type Account = {
-  fullname: ''
-  email: ''
-  phone: ''
-}
-
 export default function MenuAccount() {
-  const form = useForm<Account>({
-    defaultValues: {},
+  const user = useAtomValue(userAtom)
+
+  const { mutate: update } = useUpdateAccount()
+
+  const form = useForm<User>({
+    defaultValues: {
+      email: '',
+      name: '',
+      phoneNumber: '',
+      photo: '',
+    },
   })
+
+  const submit = (data: Partial<User>) => {
+    if (!user?.id) return
+    update({ id: user?.id, payload: data })
+  }
+
+  useEffect(() => {
+    if (!!user) {
+      form.reset({
+        name: user.name,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        photo: user.photo,
+      })
+    }
+  }, [user])
+
   return (
     <Form {...form}>
-      <form>
+      <form onSubmit={form.handleSubmit(submit)}>
         <div className='flex flex-col gap-6 max-w-sm p-6'>
           <div>
             <Label className='mb-3 block font-normal text-dark/80'>Photo</Label>
@@ -27,7 +52,7 @@ export default function MenuAccount() {
             <FormField
               label='Nama'
               control={form.control}
-              name='fullname'
+              name='name'
               render={({ field }) => <Input className='w-full' {...field} />}
             />
           </div>
@@ -41,7 +66,7 @@ export default function MenuAccount() {
             <FormField
               label='No. Telp'
               control={form.control}
-              name='phone'
+              name='phoneNumber'
               render={({ field }) => <Input {...field} />}
             />
           </div>
