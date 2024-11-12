3480d76c0cde972d68483be03d8936fad02364a4
mmulyana <mulyan.t20@gmail.com>
2024-11-12 22:57:16 +0700

    feat: update

--- src/hooks/api/use-account.ts
+++ src/hooks/api/use-account.ts
@@ -3,11 +3,12 @@ import { KEYS } from '@/utils/constant/_keys'
 import { URLS } from '@/utils/constant/_urls'
 import http from '@/utils/http'
 import { AxiosError, AxiosResponse } from 'axios'
-import { ApiError, IApi, User } from '@/utils/types/api'
+import { ApiError, IApi, IApiPagination, User } from '@/utils/types/api'
 import { Pagination } from '@/utils/types/common'
 import { UpdateAccountSchema, UpdatePasswordDto } from '@/utils/schema/account'
 import { objectToFormData } from '@/utils/ObjectToFormData'
 import { toast } from 'sonner'
+import { createUser } from '@/utils/types/form'
 
 export const useAccount = ({
   id,
@@ -27,14 +28,12 @@ export const useAccount = ({
 export const useAccountPagination = (
   params?: {
     name?: string
-    email?: string
-    phoneNumber?: string
-    roleId: string
+    roleId?: string
   } & Pagination
 ) => {
   return useQuery({
-    queryKey: [KEYS.ACCOUNTS, { ...params }],
-    queryFn: async (): Promise<AxiosResponse<IApi<User>>> => {
+    queryKey: [KEYS.ACCOUNTS, params?.name, params?.page, params?.limit],
+    queryFn: async (): Promise<AxiosResponse<IApiPagination<User[]>>> => {
       return await http(URLS.ACCOUNT, { params })
     },
   })
@@ -58,7 +57,21 @@ export const useUpdateAccount = () => {
     },
     onSuccess(data) {
       toast.success(data.data.message)
-      queryClient.invalidateQueries({ queryKey: [KEYS.ACCOUNT] })
+      queryClient.invalidateQueries({ queryKey: [KEYS.ACCOUNTS] })
+    },
+  })
+}
+export const useCreateAccount = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async (payload: createUser) => {
+      return await http.post(URLS.ACCOUNT, payload)
+    },
+    onSuccess(data) {
+      toast.success(data.data.message)
+      queryClient.invalidateQueries({
+        queryKey: [KEYS.ACCOUNTS],
+      })
     },
   })
 }
@@ -72,6 +85,19 @@ export const useUpdatePassword = () => {
       payload: UpdatePasswordDto
     }) => {
       return await http.patch(`${URLS.ACCOUNT}/${id}/password/update`, payload)
+    },
+    onSuccess: (data) => {
+      toast.success(data.data.message)
+    },
+    onError: (error: AxiosError<ApiError>) => {
+      toast.error(error?.response?.data.message)
+    },
+  })
+}
+export const useResetPassword = () => {
+  return useMutation({
+    mutationFn: async ({ id }: { id: number }) => {
+      return await http.patch(`${URLS.ACCOUNT}/${id}/password/reset`)
     },
     onSuccess: (data) => {
       toast.success(data.data.message)
