42b95181dfb913f85b53203f10459c160fe472e4
mmulyana <mulyan.t20@gmail.com>
2024-11-15 00:14:59 +0700

    feat: update

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 const URL =
   import.meta.env.NODE_ENV === 'production'
-    ? undefined
+    ? import.meta.env.VITE_SOCKET_URL
     : 'http://localhost:5001'
 
 export const socket = io(URL as string)
