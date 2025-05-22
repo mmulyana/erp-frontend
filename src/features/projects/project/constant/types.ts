89c144cdb08ed650040c239f054d917cdb2b95d4
mmulyana <mulyan.t20@gmail.com>
2025-05-22 12:23:24 +0700

    feat: update

--- src/features/projects/project/constant/types.ts
+++ src/features/projects/project/constant/types.ts
@@ -1,3 +1,6 @@
+import { ProjectPriority, ProjectStatus } from '@/shared/types/api'
+import { BadgeOption, selectOption } from '@/shared/types'
+
 export const reportTypes = [
 	'Harian',
 	'Mingguan',
@@ -8,3 +11,46 @@ export const reportTypes = [
 ]
 
 export const warningTypes = [reportTypes[3], reportTypes[4]]
+
+export const statusBadges: BadgeOption[] = [
+	{
+		color: '#565659',
+		label: 'Blm dimulai',
+		value: ProjectStatus.NOT_STARTED,
+	},
+	{
+		color: '#D52B61',
+		label: 'Penawaran',
+		value: ProjectStatus.OFFERING,
+	},
+	{
+		color: '#2B5BD5',
+		label: 'Sedang dikerjakan',
+		value: ProjectStatus.DOING,
+	},
+	{
+		color: '#27B5E9',
+		label: 'Penagihan',
+		value: ProjectStatus.BILLING,
+	},
+	{
+		color: '#47AF97',
+		label: 'Selesai',
+		value: ProjectStatus.DONE,
+	},
+]
+
+export const priorityOption: selectOption[] = [
+	{
+		label: 'Rendah',
+		value: ProjectPriority.LOW,
+	},
+	{
+		label: 'Menengah',
+		value: ProjectPriority.MEDIUM,
+	},
+	{
+		label: 'Tinggi',
+		value: ProjectPriority.HIGH,
+	},
+]
