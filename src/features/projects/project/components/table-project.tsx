12518d4aaea403f8bb4a742bc38af46df0795f36
mmulyana <mulyan.t20@gmail.com>
2025-05-22 12:23:16 +0700

    feat: update

--- src/features/projects/project/components/table-project.tsx
+++ src/features/projects/project/components/table-project.tsx
@@ -6,7 +6,13 @@ import { usePagination } from '@/shared/hooks/use-pagination'
 import { paths } from '@/shared/constants/paths'
 
 import { useProjects } from '../api/use-projects'
-import { Project } from '../types'
+import { Project } from '@/shared/types/api'
+import PhotoUrl from '@/shared/components/common/photo-url'
+import Priority from './priority'
+import ProgressPercentage from '@/shared/components/common/progress-percentage'
+import CircularProgress from '@/shared/components/common/circular-progress'
+import StatusBadge from '@/shared/components/common/status-badge'
+import { statusBadges } from '../constant/types'
 
 export default function TableProject() {
 	const { limit, page, q } = usePagination()
@@ -28,12 +34,47 @@ export default function TableProject() {
 		{
 			id: 'client',
 			header: 'Klien',
-			cell: ({ row }) => row.original.client?.name,
+			cell: ({ row }) => (
+				<p className='text-ink-primary'>
+					{row.original.client?.name}{' '}
+					{row.original.client?.company && (
+						<span className='opacity-60'>({row.original.client.name})</span>
+					)}
+				</p>
+			),
 		},
 		{
 			id: 'lead',
-			header: 'PJ Lapangan',
-			cell: ({ row }) => row.original.lead?.username,
+			header: 'Penanggung Jwb',
+			cell: ({ row }) => {
+				if (!row.original.lead) return null
+				return (
+					<div className='flex gap-2 items-center'>
+						<PhotoUrl
+							style={{ img: 'h-7 w-7', icon: 'h-4 w-4' }}
+							url={row.original.lead?.photoUrl || ''}
+						/>
+						<p className='text-ink-primary'>{row.original.lead?.username}</p>
+					</div>
+				)
+			},
+		},
+		{
+			id: 'priority',
+			header: 'Prioritas',
+			cell: ({ row }) => <Priority value={row.original.priority as string} />,
+		},
+		{
+			id: 'progress',
+			header: 'Progress',
+			cell: ({ row }) => <CircularProgress progress={row.original.progressPercentage} />,
+		},
+		{
+			id: 'status',
+			header: 'Status',
+			cell: ({ row }) => (
+				<StatusBadge options={statusBadges} value={row.original.status} />
+			),
 		},
 	]
 	// COLUMNS
