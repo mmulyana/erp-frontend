8caf160cec7b734fdc5d4767f61651b74fc03b40
mmulyana <mulyan.t20@gmail.com>
2025-05-24 00:14:30 +0700

    feat: update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,6 @@
 import useProtected from '@/features/auth/hooks/use-protected'
 import LoginForm from '@/features/auth/component/login-form'
 
-import bg from '/public/images/login-illustration.png'
-import logo from '/public/images/logo.png'
-
 export default function LoginPage() {
 	useProtected()
 
@@ -15,10 +12,13 @@ export default function LoginPage() {
 				</div>
 				<div className='h-full w-full justify-center items-center bg-[#475DEF] hidden md:flex'>
 					<div className='flex flex-col gap-[120px] items-center justify-center'>
-						<img src={bg} className='w-[401px] h-auto' />
+						<img
+							src='/images/login-illustration.png'
+							className='w-[401px] h-auto'
+						/>
 						<div className='flex flex-col gap-6 items-center justify-center'>
 							<div className='flex items-center gap-2'>
-								<img src={logo} className='w-10 h-10' />
+								<img src='/images/logo.png' className='w-10 h-10' />
 								<p className='text-white text-lg font-medium'>Barokah ERP</p>
 							</div>
 							<p className='text-white font-light max-w-[280px] mx-auto text-center'>
