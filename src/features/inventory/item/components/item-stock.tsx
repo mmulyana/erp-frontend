2ed754c92eaa0ee247ec8125980276b6a5b057c6
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:13:57 +0700

    feat: update

--- src/features/inventory/item/components/item-stock.tsx
+++ src/features/inventory/item/components/item-stock.tsx
@@ -1,19 +1,31 @@
 import { Card } from '@/shared/components/ui/card'
+import { useItem } from '../api/use-item'
+import CardV1 from '@/shared/components/common/card-v1'
+import { Database } from 'lucide-react'
 
 type props = {
 	id?: string
 }
 export default function ItemStock({ id }: props) {
+	const { data } = useItem({ id })
 	return (
-		<Card className='p-6 flex gap-2 flex-col'>
+		<CardV1
+			title='Kuantitas'
+			icon={<Database size={20} className='text-ink-primary' />}
+			style={{ content: 'pt-4 space-y-4' }}
+		>
 			<div>
 				<p className='text-ink-light'>Total</p>
-				<p className='text-ink-secondary text-2xl font-medium'>120</p>
+				<p className='text-ink-secondary text-2xl font-medium'>
+					{data?.data?.totalStock}
+				</p>
 			</div>
 			<div>
 				<p className='text-ink-light'>Tersedia</p>
-				<p className='text-ink-secondary text-2xl font-medium'>20</p>
+				<p className='text-ink-secondary text-2xl font-medium'>
+					{data?.data?.availableStock}
+				</p>
 			</div>
-		</Card>
+		</CardV1>
 	)
 }
