342eb4f50bcf87ab19f5918fbfc9c906cf0c350f
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:10:25 +0700

    feat: update

--- src/features/inventory/dashboard/components/item-due-soon.tsx
+++ src/features/inventory/dashboard/components/item-due-soon.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom'
 import { Bell } from 'lucide-react'
 
 import { ScrollArea } from '@/shared/components/ui/scroll-area'
+import { paths } from '@/shared/constants/paths'
 import PhotoUrl from '@/shared/components/common/photo-url'
 import CardV1 from '@/shared/components/common/card-v1'
 
+import { useLowStock } from '../../item/api/use-low-stock'
+
 export default function ItemDueSoon() {
+	const { data } = useLowStock()
+
+	const inventories = (data?.data && data.data) || []
+
 	return (
 		<CardV1
 			title='Barang hampir habis'
@@ -12,14 +20,27 @@ export default function ItemDueSoon() {
 			style={{ card: 'h-[290px] pr-0', content: 'pt-4' }}
 		>
 			<ScrollArea className='pr-6'>
-				<div className='flex justify-between items-center'>
-					<div className='flex gap-4 items-center'>
-						<PhotoUrl url='' style={{ img: 'h-12 w-12 rounded-xl' }} />
-						<p className='text-ink-primary'>Semen</p>
-					</div>
-					<p className='text-ink-primary'>
-						<span className='opacity-50'>Sisa</span> 8 Pcs
-					</p>
+				<div className='flex flex-col gap-6'>
+					{inventories?.map((i) => (
+						<div key={i.id} className='flex justify-between items-center'>
+							<div className='flex gap-4 items-center'>
+								<PhotoUrl
+									url={i.photoUrl || ''}
+									style={{ img: 'h-12 w-12 rounded-xl' }}
+								/>
+								<Link
+									to={`${paths.inventoryMasterdataItem}/${i.id}`}
+									className='text-ink-primary'
+								>
+									{i.name}
+								</Link>
+							</div>
+							<p className='text-ink-primary'>
+								<span className='opacity-50'>Sisa</span> {i.totalStock}{' '}
+								{i.unitOfMeasurement}
+							</p>
+						</div>
+					))}
 				</div>
 			</ScrollArea>
 		</CardV1>
