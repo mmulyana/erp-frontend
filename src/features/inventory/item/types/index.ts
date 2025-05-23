1bfc101bdc5fea42cc5e7fef9137acb95582fc2c
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:16:23 +0700

    chore: update

--- src/features/inventory/item/types/index.ts
+++ src/features/inventory/item/types/index.ts
@@ -1,30 +1,10 @@
 export type ItemForm = {
 	name: string
-	locationId?: string | null
+	warehouseId?: string | null
 	brandId?: string | null
 	minimum?: number
 	description?: string
 	unitOfMeasurement?: string
 	photoUrl?: string | File | null
-}
-
-export type Item = ItemForm & {
-	id: string
-	brand: {
-		id: string
-		name: string
-	}
-	location: {
-		id: string
-		name: string
-	}
-	quantity?: number
-	availableStock?: number
-	totalStock?: number
 	category?: string
-	user?: {
-		id: string
-		username: string
-	}
-	createdAt: string
 }
