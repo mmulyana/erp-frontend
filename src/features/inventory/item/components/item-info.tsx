fe5546047b35d0051c815b4a651aeef3e10ecb15
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:13:49 +0700

    feat: update

--- src/features/inventory/item/components/item-info.tsx
+++ src/features/inventory/item/components/item-info.tsx
@@ -8,6 +8,8 @@ import { Button } from '@/shared/components/ui/button'
 import { Card } from '@/shared/components/ui/card'
 
 import { useItem } from '../api/use-item'
+import CardV1 from '@/shared/components/common/card-v1'
+import ModalEditItem from './modal-edit-item'
 
 type props = {
 	id?: string
@@ -16,36 +18,47 @@ export default function ItemInfo({ id }: props) {
 	const { data, isPending } = useItem({ id })
 
 	return (
-		<Card className='p-6 grid grid-cols-1 md:grid-cols-[120px_1fr] gap-4 relative'>
-			<div className='w-full h-auto'>
-				<PhotoUrl url={data?.data?.photoUrl as string} />
-			</div>
-			<div className='flex flex-col items-start gap-2 justify-center'>
-				<p className='text-xl font-medium text-ink-primary'>
-					{data?.data?.name}
-				</p>
-				<div className='flex gap-2 items-center'>
-					<CalendarDays size={16} className='text-ink-light' />
-					<LoaderWrapper isLoading={isPending}>
-						{data?.data?.createdAt && (
-							<p>
-								{format(new Date(data?.data?.createdAt as string), 'PPP', {
-									locale: ind,
-								})}
-							</p>
-						)}
-					</LoaderWrapper>
+		<CardV1
+			action={<ModalEditItem variant='info' />}
+			style={{
+				card: 'relative',
+			}}
+		>
+			<div className='grid grid-cols-1 md:grid-cols-[120px_1fr] gap-2'>
+				<PhotoUrl
+					url={data?.data?.photoUrl as string}
+					style={{
+						img: 'w-[104px] h-[104px] border border-dashed border-line bg-gray-50',
+						icon: 'w-8 h-8',
+					}}
+				/>
+				<div className='flex flex-col items-start gap-2 justify-center'>
+					<p className='text-2xl font-medium text-ink-primary'>
+						{data?.data?.name}
+					</p>
+					<div className='flex gap-2 items-center'>
+						<CalendarDays size={16} className='text-ink-light' />
+						<LoaderWrapper isLoading={isPending}>
+							{data?.data?.createdAt && (
+								<p>
+									{format(new Date(data?.data?.createdAt as string), 'PPP', {
+										locale: ind,
+									})}
+								</p>
+							)}
+						</LoaderWrapper>
+					</div>
+					{data?.data?.description && data?.data?.description !== '<p></p>' && (
+						<div className='mt-4'>
+							<p className='text-ink-light text-sm'>Deskripsi</p>
+							<div
+								className='w-full py-2 prose prose-li:leading-none'
+								dangerouslySetInnerHTML={{ __html: data?.data?.description }}
+							/>
+						</div>
+					)}
 				</div>
-				<div>
-					<p className='text-ink-light'>Deskripsi</p>
-					<p>{data?.data?.description}</p>
-				</div>
 			</div>
-
-			<Button variant='outline' className='gap-2 absolute top-6 right-6'>
-				<Pencil size={16} />
-				<span className='px-0.5'>Edit</span>
-			</Button>
-		</Card>
+		</CardV1>
 	)
 }
