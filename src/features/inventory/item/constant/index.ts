6cfa03401f8fd3837e35f4d9b118c6b729819341
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:16:15 +0700

    feat: update

---
+++ src/features/inventory/item/constant/index.ts
@@ -0,0 +1,19 @@
+import { BadgeOption } from '@/shared/types'
+
+export const statusItem: BadgeOption[] = [
+	{
+		color: '#D52B42',
+		label: 'Habis',
+		value: 'Habis',
+	},
+	{
+		color: '#EE682F',
+		label: 'Hampir habis',
+		value: 'Hampir habis',
+	},
+	{
+		color: '#47AF97',
+		label: 'Tersedia',
+		value: 'Tersedia',
+	},
+]
