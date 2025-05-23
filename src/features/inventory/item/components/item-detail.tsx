b066b2829dd312c2936ee2069b0add498aa6cc1c
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:13:37 +0700

    feat: update

--- src/features/inventory/item/components/item-detail.tsx
+++ src/features/inventory/item/components/item-detail.tsx
@@ -1,43 +1,68 @@
-import { Card } from '@/shared/components/ui/card'
-import { useItem } from '../api/use-item'
+import { ExternalLink, List } from 'lucide-react'
 import { Link } from 'react-router-dom'
+
+import StatusBadge from '@/shared/components/common/status-badge'
+import CardV1 from '@/shared/components/common/card-v1'
 import { paths } from '@/shared/constants/paths'
+
+import ModalEditItem from './modal-edit-item'
+import { useItem } from '../api/use-item'
+import { statusItem } from '../constant'
 
 type props = {
 	id?: string
 }
 export default function ItemDetail({ id }: props) {
 	const { data } = useItem({ id })
-
 	return (
-		<Card className='p-6 flex flex-col gap-4'>
-			<p className='font-medium text-ink-secondary'>Detail</p>
+		<CardV1
+			title='Detail'
+			icon={<List size={20} className='text-ink-primary' />}
+			style={{ content: 'space-y-4 pt-4', card: 'relative' }}
+			action={<ModalEditItem variant='detail' />}
+		>
 			<div className='flex justify-between items-center'>
-				<p className='text-ink-light'>Kategori</p>
-				<p className='text-ink-secondary'>{data?.data?.category}</p>
+				<p className='text-ink-primary/50'>Status</p>
+				<StatusBadge value={data?.data?.status} options={statusItem} />
 			</div>
 			<div className='flex justify-between items-center'>
-				<p className='text-ink-light'>Satuan pengukuran</p>
-				<p className='text-ink-secondary'>{data?.data?.unitOfMeasurement}</p>
+				<p className='text-ink-primary/50'>Kategori</p>
+				<p className='text-ink-primary'>{data?.data?.category}</p>
 			</div>
 			<div className='flex justify-between items-center'>
-				<p className='text-ink-light'>Lokasi</p>
+				<p className='text-ink-primary/50'>Minimum</p>
+				<p className='text-ink-primary'>{data?.data?.minimum}</p>
+			</div>
+			<div className='flex justify-between items-center'>
+				<p className='text-ink-primary/50'>Satuan pengukuran</p>
+				<p className='text-ink-primary'>{data?.data?.unitOfMeasurement}</p>
+			</div>
+			<div className='flex justify-between items-center'>
+				<p className='text-ink-primary/50'>Lokasi</p>
 				<div className='flex gap-1 items-center'>
-					<p className='text-ink-secondary'>{data?.data?.location.name}</p>
-					<Link to={`${paths.inventoryMasterdataLocation}/${id}`}>Lihat</Link>
+					<p className='text-ink-primary'>{data?.data?.warehouse?.name}</p>
+					<Link
+						to={`${paths.inventoryMasterdataLocation}/${data?.data?.warehouseId}`}
+						className='flex gap-1 items-center'
+					>
+						<span className='px-0.5'>Lihat</span>
+						<ExternalLink size={16} />
+					</Link>
 				</div>
 			</div>
 			<div className='flex justify-between items-center'>
-				<p className='text-ink-light'>Merek</p>
-				<div className='flex gap-1 items-center'>
-					<p className='text-ink-secondary'>{data?.data?.brand.name}</p>
-					<Link to={`${paths.inventoryMasterdataBrand}/${id}`}>Lihat</Link>
+				<p className='text-ink-primary/50'>Merek</p>
+				<div className='flex gap-2 items-center'>
+					<p className='text-ink-primary'>{data?.data?.brand?.name}</p>
+					<Link
+						to={`${paths.inventoryMasterdataBrand}/${data?.data?.brandId}`}
+						className='flex gap-1 items-center'
+					>
+						<span className='px-0.5'>Lihat</span>
+						<ExternalLink size={16} />
+					</Link>
 				</div>
 			</div>
-			<div className='flex justify-between items-center'>
-				<p className='text-ink-light'>Dibuat oleh</p>
-				<p className='text-ink-secondary'>{data?.data?.user?.username}</p>
-			</div>
-		</Card>
+		</CardV1>
 	)
 }
