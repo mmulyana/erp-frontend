6ebc1b799436d30b9813791c18db042867956a71
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:16:50 +0700

    feat: update

--- src/pages/inventory/dashboard.tsx
+++ src/pages/inventory/dashboard.tsx
@@ -1,17 +1,15 @@
-import { Hammer, Store, Warehouse } from 'lucide-react'
 import { parseAsIsoDate, useQueryStates } from 'nuqs'
 
 import { LineTransaction } from '@/features/inventory/ledger/components/line-transaction'
 import TableTransaction from '@/features/inventory/ledger/components/table-transaction'
-
 import ItemAvailability from '@/features/inventory/item/components/item-availability'
 import ItemDueSoon from '@/features/inventory/dashboard/components/item-due-soon'
 import DateRangePicker from '@/shared/components/common/date-range-picker'
 import PieLoan from '@/features/inventory/loan/components/pie-loan'
-import CardV1 from '@/shared/components/common/card-v1'
 
 import { DefaultLayout } from '@/shared/layout/default-layout'
 import { useWeek } from '@/shared/hooks/use-week'
+import TotalData from '@/features/inventory/dashboard/components/total-data'
 
 export default function DashboardInventory() {
 	const { startOfWeek, endOfWeek } = useWeek()
@@ -27,36 +25,8 @@ export default function DashboardInventory() {
 			module='inventory'
 		>
 			<div className='space-y-6'>
-				<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-					<CardV1
-						title='Total Barang'
-						icon={<Hammer size={20} className='text-ink-primary' />}
-						style={{
-							content: 'text-right pb-4',
-						}}
-					>
-						<p className='text-2xl font-medium text-ink-primary'>140</p>
-					</CardV1>
-					<CardV1
-						title='Gudang'
-						icon={<Warehouse size={20} className='text-ink-primary' />}
-						style={{
-							content: 'text-right pb-4',
-						}}
-					>
-						<p className='text-2xl font-medium text-ink-primary'>4</p>
-					</CardV1>
-					<CardV1
-						title='Supplier'
-						icon={<Store size={20} className='text-ink-primary' />}
-						style={{
-							content: 'text-right pb-4',
-						}}
-					>
-						<p className='text-2xl font-medium text-ink-primary'>14</p>
-					</CardV1>
-				</div>
 				<div className='h-fit space-y-6'>
+					<TotalData />
 					<div className='flex justify-end'>
 						<DateRangePicker />
 					</div>
