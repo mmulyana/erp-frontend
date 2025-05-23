402409adf3a219ab5162b52f820d3f347733d705
mmulyana <mulyan.t20@gmail.com>
2025-05-23 07:16:30 +0700

    chore: update

--- src/features/inventory/supplier/types/index.ts
+++ src/features/inventory/supplier/types/index.ts
@@ -6,24 +6,3 @@ export type SupplierForm = {
 	googleMapUrl?: string
 	photoUrl?: File | string | null
 }
-
-export type Supplier = {
-	id: string
-	name: string
-	address?: string
-	googleMapUrl?: string
-	photoUrl?: string
-	phone?: string
-	email?: string
-
-	stockIn: {
-		id: string
-		price: number
-	}[]
-
-	_count: {
-		stockIn: number
-	}
-
-	createdAt: string
-}
