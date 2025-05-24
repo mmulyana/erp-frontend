ff5b1a7f52d2641d52f5e5c7b894cf30e52d5089
mmulyana <mulyan.t20@gmail.com>
2025-05-24 07:17:33 +0700

    feat: update

--- src/features/projects/project/api/use-projects.ts
+++ src/features/projects/project/api/use-projects.ts
@@ -6,7 +6,15 @@ import { urls } from '@/shared/constants/urls'
 import { Project } from '@/shared/types/api'
 import http from '@/shared/utils/http'
 
-export const useProjects = (params?: Pagination) => {
+export const useProjects = (
+	params?: Pagination & {
+		leadId?: string
+		clientId?: string
+		status?: string
+		sortBy?: string
+		sortOrder?: string
+	}
+) => {
 	return useQuery({
 		queryKey: [keys.project, params],
 		queryFn: async (): Promise<IApiPagination<Project[]>> => {
