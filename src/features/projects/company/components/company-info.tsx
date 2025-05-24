6e30c6861156f5b306b514843c701e2bd3590a16
mmulyana <mulyan.t20@gmail.com>
2025-05-24 07:49:56 +0700

    feat: update

--- src/features/projects/company/components/company-info.tsx
+++ src/features/projects/company/components/company-info.tsx
@@ -1,9 +1,14 @@
 import CardV1 from '@/shared/components/common/card-v1'
-import { useCompany } from '../../company/api/use-company'
+import { useCompany } from '../api/use-company'
 import { Building } from 'lucide-react'
-import ModalDetailClient from './modal-detail-client'
 
-export default function ClientCompany({ id }: { id?: string }) {
+export default function CompanyInfo({
+	id,
+	action,
+}: {
+	id?: string
+	action?: React.ReactNode
+}) {
 	const { data } = useCompany({ id })
 
 	return (
@@ -11,7 +16,7 @@ export default function ClientCompany({ id }: { id?: string }) {
 			title='Perusahaan'
 			icon={<Building size={20} className='text-ink-primary' />}
 			style={{ content: 'pt-4 space-y-6' }}
-			action={<ModalDetailClient variant='company' />}
+			action={action}
 		>
 			<div className='flex justify-between items-center'>
 				<p className='text-ink-primary/50'>Nama</p>
