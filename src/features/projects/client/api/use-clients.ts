b944ac2213a693eab7ea9fe9a13689d2879443e8
mmulyana <mulyan.t20@gmail.com>
2025-05-24 07:50:01 +0700

    feat: update

--- src/features/projects/client/api/use-clients.ts
+++ src/features/projects/client/api/use-clients.ts
@@ -6,7 +6,12 @@ import { urls } from '@/shared/constants/urls'
 import { Client } from '@/shared/types/api'
 import http from '@/shared/utils/http'
 
-export const useClients = (params?: Pagination) => {
+export const useClients = (
+	params?: Pagination & {
+		companyId?: string
+		sortOrder?: string
+	}
+) => {
 	return useQuery({
 		queryKey: [keys.client, params],
 		queryFn: async (): Promise<IApiPagination<Client[]>> => {
