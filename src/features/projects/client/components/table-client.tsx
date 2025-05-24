81ebb0cfe57d994bbaeaecf2fb67b64e26ae37d6
mmulyana <mulyan.t20@gmail.com>
2025-05-24 07:17:26 +0700

    feat: update

--- src/features/projects/client/components/table-client.tsx
+++ src/features/projects/client/components/table-client.tsx
@@ -1,16 +1,17 @@
 import { ColumnDef } from '@tanstack/react-table'
-import { useSetAtom } from 'jotai'
+import { useNavigate } from 'react-router-dom'
+
+import PhotoUrl from '@/shared/components/common/photo-url'
 
 import { DataTable } from '@/shared/components/common/data-table'
 import { usePagination } from '@/shared/hooks/use-pagination'
-
-import { atomModalClient } from './modal-detail-client'
-import { useClients } from '../api/use-clients'
-import PhotoUrl from '@/shared/components/common/photo-url'
+import { paths } from '@/shared/constants/paths'
 import { Client } from '@/shared/types/api'
 
+import { useClients } from '../api/use-clients'
+
 export default function TableClient() {
-	const setModal = useSetAtom(atomModalClient)
+	const navigate = useNavigate()
 	const { limit, page, q } = usePagination()
 
 	const { isLoading, data } = useClients({
@@ -73,7 +74,9 @@ export default function TableClient() {
 				totalPages={data?.data.total_pages}
 				withPagination
 				nonClickableColumns={['company']}
-				onCellClick={({ id }) => setModal({ id, open: true })}
+				onCellClick={({ id }) =>
+					navigate(`${paths.projectMasterdataClient}/${id}`)
+				}
 			/>
 		</>
 	)
