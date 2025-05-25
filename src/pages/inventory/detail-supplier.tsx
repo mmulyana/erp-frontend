e217027b5152619674f7af199e5f12217138e5e2
mmulyana <mulyan.t20@gmail.com>
2025-05-26 06:46:05 +0700

    feat: update

--- src/pages/inventory/detail-supplier.tsx
+++ src/pages/inventory/detail-supplier.tsx
@@ -1,18 +1,15 @@
-import { useItem } from '@/features/inventory/item/api/use-item'
-import { useSupplier } from '@/features/inventory/supplier/api/use-supplier'
-import ModalSupplier, {
-	atomModalSupplier,
-} from '@/features/inventory/supplier/components/modal-supplier'
+import { useParams } from 'react-router-dom'
+import { House } from 'lucide-react'
+
+import SupplierTransaction from '@/features/inventory/supplier/components/supplier-transaction'
 import SupplierDetail from '@/features/inventory/supplier/components/supplier-detail'
 import SupplierInfo from '@/features/inventory/supplier/components/supplier-info'
-import SupplierTotal from '@/features/inventory/supplier/components/supplier-total'
-import { Button } from '@/shared/components/ui/button'
+import { useSupplier } from '@/features/inventory/supplier/api/use-supplier'
+
+import DetailLayout from '@/shared/layout/detail-layout'
+import { useDynamicLinks } from '@/shared/utils/link'
 import { paths } from '@/shared/constants/paths'
-import DetailLayout, { Link } from '@/shared/layout/detail-layout'
-import { useSetAtom } from 'jotai'
-import { House, Pencil } from 'lucide-react'
-import { useMemo } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link } from '@/shared/types'
 
 const links: Link[] = [
 	{
@@ -22,62 +19,44 @@ const links: Link[] = [
 		hideName: true,
 	},
 	{
-		name: 'Item',
-		path: paths.inventoryMasterdataItem,
+		name: 'Supplier',
+		path: paths.inventoryMasterdataSupplier,
 	},
 	{
 		name: 'Detail',
-		path: paths.inventoryMasterdataItem,
+		path: paths.inventoryMasterdataSupplier,
 	},
 ]
 
 export default function DetailSupplier() {
 	const { id } = useParams()
-	const setModal = useSetAtom(atomModalSupplier)
 
 	const { data } = useSupplier({ id })
 
-	const linkMemo: Link[] = useMemo(() => {
-		if (!id || !data?.data) return links
-
-		return [
-			...links.filter((i) => i.name !== 'Detail'),
-			{
-				name: data?.data?.name || id,
-				path: paths.projectMasterdataProjectsDetail,
-			},
-		]
-	}, [id, data])
+	const dynamicLink = useDynamicLinks({
+		baseLinks: links,
+		replaceName: 'Detail',
+		newLink: data?.data
+			? {
+					name: data.data.name ?? '',
+					path: `${paths.inventoryMasterdataSupplier}/${data.data.id}`,
+			  }
+			: undefined,
+		condition: !!(id && data?.data),
+	})
 
 	return (
 		<DetailLayout
-			links={linkMemo}
-			titleAction='SRE'
-			buttonAction={
-				<Button
-					variant='outline'
-					className='gap-1'
-					onClick={() => {
-						if (id) {
-							setModal({ id: id as string, open: true })
-						}
-					}}
-				>
-					<Pencil size={16} />
-					<span className='px-0.5'>Edit</span>
-				</Button>
-			}
+			links={dynamicLink}
+			style={{
+				header: 'w-[600px]',
+			}}
 		>
-			<div className='grid grid-cols-1 md:grid-cols-[580px_1fr] gap-6 w-[800px] max-w-full px-4 md:px-0 mx-auto pt-6'>
-				<div className='space-y-6'>
-					<SupplierInfo id={id} />
-					<SupplierDetail id={id} />
-				</div>
-				<div className='space-y-6'>
-					<SupplierTotal id={id} />
-				</div>
+			<div className='space-y-6 w-[600px] max-w-full px-4 md:px-0 mx-auto py-6'>
+				<SupplierInfo id={id} />
+				<SupplierDetail id={id} />
+				<SupplierTransaction id={id} />
 			</div>
-			<ModalSupplier />
 		</DetailLayout>
 	)
 }
