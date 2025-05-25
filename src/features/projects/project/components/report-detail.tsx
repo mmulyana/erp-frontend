3000fbccd50a4603c857f6736411978664c3ab03
mmulyana <mulyan.t20@gmail.com>
2025-05-26 06:45:49 +0700

    feat: update

--- src/features/projects/project/components/report-detail.tsx
+++ src/features/projects/project/components/report-detail.tsx
@@ -1,4 +1,5 @@
 import { Dialog, DialogContent } from '@/shared/components/ui/dialog'
+import { useIsMobile } from '@/shared/hooks/use-mobile'
 import { cn } from '@/shared/utils/cn'
 
 import { useReport } from '../api/report/use-report'
@@ -17,15 +18,18 @@ export default function ReportDetail({
 	const { data } = useReport({ id })
 	const imagesLength = data?.data?.attachments.length || 0
 
+	const isMobile = useIsMobile()
+
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogContent
 				className={cn(
-					'h-screen md:h-[80vh] w-full p-0 overflow-hidden',
+					'min-h-screen md:min-h-min md:h-[80vh] w-full p-0 overflow-hidden',
 					!!imagesLength && 'max-w-6xl '
 				)}
+				showClose={isMobile}
 			>
-				<div className='flex flex-col md:flex-row h-full pt-10 md:pt-0'>
+				<div className='flex flex-col md:flex-row h-full pt-12 md:pt-0'>
 					<ReportAttachment id={id} />
 					<ReportComment id={id} />
 				</div>
