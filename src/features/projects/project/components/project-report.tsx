a7e3efc782e3f36699a092f2767daeecad5f6081
mmulyana <mulyan.t20@gmail.com>
2025-05-25 20:24:15 +0700

    feat: update

--- src/features/projects/project/components/project-report.tsx
+++ src/features/projects/project/components/project-report.tsx
@@ -1,23 +1,14 @@
-import { Link } from 'react-router-dom'
-import { id } from 'date-fns/locale'
-import { format } from 'date-fns'
-import {
-	Image,
-	History,
-	Calendar,
-	AlertTriangle,
-	MessageSquare,
-	MessageSquareMoreIcon,
-} from 'lucide-react'
+import { History } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 import { ScrollArea } from '@/shared/components/ui/scroll-area'
-import PhotoUrl from '@/shared/components/common/photo-url'
-import { paths } from '@/shared/constants/paths'
 import { cn } from '@/shared/utils/cn'
 
 import { useReports } from '../api/report/use-reports'
 import { ModalAddReport } from './modal-add-report'
 import { warningTypes } from '../constant/types'
+import ReportDetail from './report-detail'
+import ReportItem from './report-item'
 
 type props = {
 	projectId?: string
@@ -25,119 +16,68 @@ type props = {
 	limit?: number
 }
 export default function ProjectReport({ projectId, variant, limit }: props) {
-	const { data } = useReports({ projectId, limit: String(limit) })
+	const { data, refetch } = useReports({ projectId, limit: String(limit) })
+	const [open, setOpen] = useState(false)
+	const [id, setId] = useState('')
+
+	useEffect(() => {
+		if (!open) {
+			refetch()
+		}
+	}, [open])
 
 	return (
-		<div className={cn(variant === 'detail' && 'pt-6')}>
-			<div className='flex justify-between items-start md:items-center'>
-				<div className='flex gap-2 items-center'>
-					{variant === 'detail' ? (
-						<>
-							<p className='text-ink-secondary font-medium'>Laporan</p>
-							<div className='bg-[#e3e3e3] rounded-md px-2 py-0.5'>
-								<p className='text-xs text-ink-primary font-medium'>
-									{data?.data.data.length}
+		<>
+			<div className={cn(variant === 'detail' && 'pt-6')}>
+				<div className='flex justify-between items-start md:items-center'>
+					<div className='flex gap-2 items-center'>
+						{variant === 'detail' ? (
+							<>
+								<p className='text-ink-secondary font-medium'>Laporan</p>
+								<div className='bg-[#e3e3e3] rounded-md px-2 py-0.5'>
+									<p className='text-xs text-ink-primary font-medium'>
+										{data?.data.data.length}
+									</p>
+								</div>
+							</>
+						) : (
+							<>
+								<History size={20} className='text-ink-primary' />
+								<p className='text-ink-secondary font-medium mb-2'>
+									Laporan Terbaru
 								</p>
-							</div>
-						</>
-					) : (
-						<>
-							<History size={20} className='text-ink-primary' />
-							<p className='text-ink-secondary font-medium mb-2'>
-								Laporan Terbaru
-							</p>
-						</>
+							</>
+						)}
+					</div>
+
+					{variant === 'detail' && (
+						<div className='flex gap-2 items-center flex-wrap justify-end md:justify-between'>
+							<ModalAddReport projectId={projectId} />
+						</div>
 					)}
 				</div>
-
-				{variant === 'detail' && (
-					<div className='flex gap-2 items-center flex-wrap justify-end md:justify-between'>
-						<ModalAddReport projectId={projectId} />
+				<ScrollArea className='mb-4 h-[320px]'>
+					<div className='space-y-4 pt-4'>
+						{data?.data.data.map((i, index) => {
+							const isWarning = warningTypes.includes(i.type)
+							return (
+								<ReportItem
+									key={i.id}
+									item={i}
+									variant={variant}
+									isWarning={isWarning}
+									isLast={index === data.data.data.length - 1}
+									onClick={() => {
+										setId(i.id)
+										setOpen(true)
+									}}
+								/>
+							)
+						})}
 					</div>
-				)}
+				</ScrollArea>
 			</div>
-			<ScrollArea className='mb-4 h-[320px]'>
-				<div className='space-y-4 pt-4'>
-					{data?.data.data.map((i, index) => {
-						const isWarning = warningTypes.includes(i.type)
-
-						return (
-							<div className='flex' key={i.id}>
-								<div className='w-14 min-h-full relative'>
-									<div
-										className={cn(
-											'z-10 h-8 w-8 rounded-full bg-brand text-white flex justify-center items-center absolute left-1/2 -translate-x-1/2',
-											isWarning && 'bg-error'
-										)}
-									>
-										{isWarning ? (
-											<AlertTriangle size={18} strokeWidth={2.5} />
-										) : (
-											<MessageSquare fill='#FFF' size={16} />
-										)}
-									</div>
-									<div
-										className={cn(
-											'bg-ink-primary/10 absolute h-[calc(100%+28px)] w-0.5 left-1/2 -translate-x-1/2',
-											index === data.data.data.length - 1 && 'h-0'
-										)}
-									></div>
-								</div>
-								<div className='py-4 px-6 w-full rounded-xl border border-border bg-white'>
-									<div className='grid grid-cols-[40px_1fr] gap-4'>
-										<PhotoUrl
-											url={i.user.photoUrl || ''}
-											style={{ img: 'w-10 h-10' }}
-										/>
-										<div>
-											<p className='text-ink-primary font-medium lowercase mb-2'>
-												{i.user.username}{' '}
-												<span className='opacity-50 font-normal'>
-													{warningTypes.includes(i.type)
-														? 'Melaporkan'
-														: 'Menambahkan laporan'}{' '}
-												</span>
-												{i.type}{' '}
-												{variant === 'dashboard' && (
-													<>
-														<span className='opacity-50 font-normal'>di </span>
-														<Link
-															to={`${paths.projectMasterdataProjects}/${i.project.id}`}
-														>
-															{i.project.name}
-														</Link>
-													</>
-												)}
-											</p>
-
-											<p>"{i.message}"</p>
-										</div>
-									</div>
-									<div className='flex gap-4 items-center pl-14 pt-4'>
-										<div className='flex gap-2 items-center'>
-											<MessageSquareMoreIcon
-												size={20}
-												className='text-[#959597]'
-											/>
-											<p className='text-[#959597]'>{i._count.comments}</p>
-										</div>
-										<div className='flex gap-2 items-center'>
-											<Image size={20} className='text-[#959597]' />
-											<p className='text-[#959597]'>{i._count.attachments}</p>
-										</div>
-										<div className='flex gap-2 items-center ml-0 md:ml-auto'>
-											<Calendar size={20} className='text-[#959597]' />
-											<p className='text-[#959597]'>
-												{format(new Date(i.createdAt), 'PPP', { locale: id })}
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						)
-					})}
-				</div>
-			</ScrollArea>
-		</div>
+			<ReportDetail open={open} setOpen={setOpen} id={id} />
+		</>
 	)
 }
