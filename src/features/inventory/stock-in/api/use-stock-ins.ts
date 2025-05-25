5dfaf49b401fbf5e699d730edb856fe23b454b4d
mmulyana <mulyan.t20@gmail.com>
2025-05-26 06:40:52 +0700

    feat: update params

--- src/features/inventory/stock-in/api/use-stock-ins.ts
+++ src/features/inventory/stock-in/api/use-stock-ins.ts
@@ -3,11 +3,17 @@ import { useQuery } from '@tanstack/react-query'
 import { IApiPagination, Pagination } from '@/shared/types'
 import { keys } from '@/shared/constants/keys'
 import { urls } from '@/shared/constants/urls'
+import { StockIn } from '@/shared/types/api'
 import http from '@/shared/utils/http'
 
-import { StockIn } from '../type'
-
-export const useStockIns = (params?: Pagination) => {
+export const useStockIns = (
+	params?: Pagination & {
+		supplierId?: string
+		createdBy?: string
+		sortBy?: string
+		sortOrder?: string
+	}
+) => {
 	return useQuery({
 		queryKey: [keys.stockIn, params],
 		queryFn: async (): Promise<IApiPagination<StockIn[]>> => {
