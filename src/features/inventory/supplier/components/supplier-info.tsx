6a368de03efb52c36e55b23959d7214eafbe5642
mmulyana <mulyan.t20@gmail.com>
2025-05-26 06:41:21 +0700

    feat: update

--- src/features/inventory/supplier/components/supplier-info.tsx
+++ src/features/inventory/supplier/components/supplier-info.tsx
@@ -1,26 +1,35 @@
-import { CalendarDays, ExternalLink } from 'lucide-react'
+import { CalendarDays, Store } from 'lucide-react'
 import { id as ind } from 'date-fns/locale'
 import { format } from 'date-fns'
 
 import { LoaderWrapper } from '@/shared/components/common/loader-wrapper'
 import PhotoUrl from '@/shared/components/common/photo-url'
-import { Card } from '@/shared/components/ui/card'
+import CardV1 from '@/shared/components/common/card-v1'
 
+import ModalDetailSupplier from './modal-detail-supplier'
 import { useSupplier } from '../api/use-supplier'
-import { Link } from 'react-router-dom'
 
 export default function SupplierInfo({ id }: { id?: string }) {
 	const { data, isPending } = useSupplier({ id })
 
 	return (
-		<Card className='p-6 grid grid-cols-1 md:grid-cols-[120px_1fr] gap-4 relative'>
-			<div className='w-full h-auto'>
-				<PhotoUrl url={data?.data?.photoUrl as string} />
-			</div>
+		<CardV1
+			icon={<Store size={20} className='text-ink-primary' />}
+			title='Supplier'
+			style={{
+				content: 'flex gap-4 items-center pt-4',
+			}}
+			action={<ModalDetailSupplier variant='info' />}
+		>
+			<PhotoUrl
+				url={data?.data?.photoUrl as string}
+				style={{ img: 'h-20 w-20' }}
+			/>
 			<div className='flex flex-col items-start gap-2 justify-center'>
 				<p className='text-xl font-medium text-ink-primary'>
 					{data?.data?.name}
 				</p>
+				<p className='text-sm text-ink-primary/50'>Dibuat sejak</p>
 				<div className='flex gap-2 items-center'>
 					<CalendarDays size={16} className='text-ink-light' />
 					<LoaderWrapper isLoading={isPending}>
@@ -33,21 +42,7 @@ export default function SupplierInfo({ id }: { id?: string }) {
 						)}
 					</LoaderWrapper>
 				</div>
-				<div>
-					<p className='text-ink-light'>Alamat</p>
-					<p className='text-ink-secondary'>{data?.data?.address ?? '-'}</p>
-					{data?.data?.googleMapUrl && (
-						<Link
-							to={data.data.googleMapUrl}
-							target='_blank'
-							className='text-sm mt-2 flex items-center gap-2'
-						>
-							Lihat google map
-							<ExternalLink size={16} />
-						</Link>
-					)}
-				</div>
 			</div>
-		</Card>
+		</CardV1>
 	)
 }
