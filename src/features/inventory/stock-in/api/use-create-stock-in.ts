72884a3d1a4f7bf2cd86c149e8f6e56323a20e85
mmulyana <mulyan.t20@gmail.com>
2025-05-27 02:12:10 +0700

    feat: update

--- src/features/inventory/stock-in/api/use-create-stock-in.ts
+++ src/features/inventory/stock-in/api/use-create-stock-in.ts
@@ -1,10 +1,18 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { toast } from 'sonner'
+
 import { urls } from '@/shared/constants/urls'
+import { keys } from '@/shared/constants/keys'
+import { StockIn } from '@/shared/types/api'
+import { IApi } from '@/shared/types'
 import http from '@/shared/utils/http'
 
 import { CreateStockInPayload } from '../type'
 
 export function useCreateStockIn() {
+	const queryClient = useQueryClient()
+
 	return useMutation({
 		mutationFn: async (payload: CreateStockInPayload) => {
 			const formData = new FormData()
@@ -20,11 +28,18 @@ export function useCreateStockIn() {
 				formData.append('photoUrl', payload.photoUrl)
 			}
 
-			return await http.post(urls.stockIn, formData, {
+			return await http.post<IApi<StockIn>>(urls.stockIn, formData, {
 				headers: {
 					'Content-Type': 'multipart/form-data',
 				},
 			})
+		},
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({ queryKey: [keys.stockIn] })
+			toast.success(data.data.message)
+		},
+		onError: (error: AxiosError<any>) => {
+			toast.error(error.response?.data.message)
 		},
 	})
 }
