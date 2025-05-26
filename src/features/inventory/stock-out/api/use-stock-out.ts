7db9f475d7704fdca31a454e09c853650a065ee8
mmulyana <mulyan.t20@gmail.com>
2025-05-27 02:12:41 +0700

    feat: update

--- src/features/inventory/stock-out/api/use-stock-out.ts
+++ src/features/inventory/stock-out/api/use-stock-out.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 
-import { IApi, IApiPagination } from '@/shared/types'
 import { keys } from '@/shared/constants/keys'
 import { urls } from '@/shared/constants/urls'
+import { StockOut } from '@/shared/types/api'
+import { IApi } from '@/shared/types'
 import http from '@/shared/utils/http'
-import { StockOut } from '../types'
 
 type Params = {
 	id?: string
@@ -12,7 +12,7 @@ type Params = {
 
 export const useStockOut = (params?: Params) => {
 	return useQuery({
-		queryKey: [keys.stockInDetail, params?.id],
+		queryKey: [keys.stockOutDetail, params?.id],
 		queryFn: async (): Promise<IApi<StockOut>> => {
 			const { data } = await http(`${urls.stockOut}/${params?.id}`)
 			return data.data
