e08e763128a664aba760b3af47bfcebfc8c975ff
mmulyana <mulyan.t20@gmail.com>
2025-06-01 12:00:18 +0700

    feat: update

--- src/features/inventory/location/api/use-locations.ts
+++ src/features/inventory/location/api/use-locations.ts
@@ -1,18 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 
-import { IApiPagination } from '@/shared/types'
+import { IApiPagination, Pagination } from '@/shared/types'
 
 import { keys } from '@/shared/constants/keys'
 import { urls } from '@/shared/constants/urls'
 import http from '@/shared/utils/http'
 
-type Params = {
-	search?: string
-	page?: string
-	limit?: string
-}
-
-export const useLocations = (params?: Params) => {
+export const useLocations = (params?: Pagination) => {
 	return useQuery({
 		queryKey: [keys.location, params],
 		queryFn: async (): Promise<IApiPagination<any[]>> => {
