d8c15fbc9e96302213d2761ed16b8019fa035136
mmulyana <mulyan.t20@gmail.com>
2025-06-01 12:00:57 +0700

    feat: update

--- src/pages/inventory/brand.tsx
+++ src/pages/inventory/brand.tsx
@@ -6,8 +6,17 @@ import CreatedSelect from '@/shared/components/common/select/created-select'
 import TableBrand from '@/features/inventory/brand/components/table-brand'
 import SortButton from '@/shared/components/common/sort-button'
 import HeadPage from '@/shared/components/common/head-page'
+import { parseAsString, useQueryStates } from 'nuqs'
+import { useHasQueryValue } from '@/shared/hooks/use-has-query'
+import FilterReset from '@/shared/components/common/filter-reset'
 
 export default function Brand() {
+	const [query, setQuery] = useQueryStates({
+		sort: parseAsString.withDefault(''),
+	})
+
+	const hasQuery = useHasQueryValue(query)
+
 	return (
 		<DefaultLayout className='space-y-6' module='inventory'>
 			<HeadPage
@@ -19,7 +28,15 @@ export default function Brand() {
 			<div className='p-6 rounded-xl border borde-border bg-white space-y-6'>
 				<div className='flex gap-4 items-center'>
 					<SearchV3 />
-					<SortButton style={{ trigger: 'ml-auto' }}>
+					<FilterReset
+						show={hasQuery}
+						onClick={() =>
+							setQuery({
+								sort: null,
+							})
+						}
+					/>
+					<SortButton style={{ trigger: 'ml-0 md:ml-auto' }}>
 						<CreatedSelect />
 					</SortButton>
 				</div>
