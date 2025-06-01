117eaa9c021644184723dc4932066fe44a8cabbb
mmulyana <mulyan.t20@gmail.com>
2025-06-01 22:35:04 +0700

    feat: update

---
+++ src/pages/user.tsx
@@ -0,0 +1,30 @@
+import ModalAddUser from '@/features/user/components/modal-add-user'
+import TableUser from '@/features/user/components/table-user'
+import HeadPage from '@/shared/components/common/head-page'
+import SearchV3 from '@/shared/components/common/search-v3'
+import CreatedSelect from '@/shared/components/common/select/created-select'
+import SortButton from '@/shared/components/common/sort-button'
+import { DefaultLayout } from '@/shared/layout/default-layout'
+
+export default function User() {
+	return (
+		<DefaultLayout module='admin' className='space-y-6'>
+			<HeadPage
+				title='User'
+				subtitle='Kelola data user'
+				action={<ModalAddUser />}
+			/>
+
+			<div className='p-6 rounded-xl border borde-border bg-white space-y-6'>
+				<div className='flex justify-between items-center gap-4'>
+					<SearchV3 />
+
+					<SortButton style={{ trigger: 'ml-0 md:ml-auto' }}>
+						<CreatedSelect />
+					</SortButton>
+				</div>
+				<TableUser />
+			</div>
+		</DefaultLayout>
+	)
+}
