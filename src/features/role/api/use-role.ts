71ae04f7ae5f4582dfed024462394712e293c75e
mmulyana <mulyan.t20@gmail.com>
2025-06-01 22:36:58 +0700

    feat: update

---
+++ src/features/role/api/use-role.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { keys } from '@/shared/constants/keys'
+import { urls } from '@/shared/constants/urls'
+import { Role } from '@/shared/types/api'
+import { IApi } from '@/shared/types'
+import http from '@/shared/utils/http'
+
+type Params = {
+	id?: string
+}
+
+export const useRole = (params?: Params) => {
+	return useQuery({
+		queryKey: [keys.rolesDetail, params?.id],
+		queryFn: async (): Promise<IApi<Role>> => {
+    const { data } = await http(`${urls.role}/${params?.id}`, {
+				params,
+			})
+			return data.data
+		},
+		enabled:
+			params?.id !== null && params?.id !== undefined && params?.id !== '',
+	})
+}
