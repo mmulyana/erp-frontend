d1917f5d0e1c876c3620e0aa556b2392fb1e364f
mmulyana <mulyan.t20@gmail.com>
2025-06-01 12:00:04 +0700

    feat: update

--- src/features/inventory/item/constant/index.ts
+++ src/features/inventory/item/constant/index.ts
@@ -1,19 +1,35 @@
-import { BadgeOption } from '@/shared/types'
+import { BadgeOption, selectOption } from '@/shared/types'
 
 export const statusItem: BadgeOption[] = [
 	{
 		color: '#D52B42',
 		label: 'Habis',
-		value: 'Habis',
+		value: 'OutOfStock',
 	},
 	{
 		color: '#EE682F',
 		label: 'Hampir habis',
-		value: 'Hampir habis',
+		value: 'LowStock',
 	},
 	{
 		color: '#47AF97',
 		label: 'Tersedia',
-		value: 'Tersedia',
+		value: 'Available',
+	},
+]
+
+
+export const statusOption: selectOption[] = [
+	{
+		label: 'Habis',
+		value: 'OutOfStock',
+	},
+	{
+		label: 'Hampir habis',
+		value: 'LowStock',
+	},
+	{
+		label: 'Tersedia',
+		value: 'Available',
 	},
 ]
