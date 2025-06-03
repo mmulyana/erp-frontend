52d888c65c586a536bfc659e0d597e69699a848d
mmulyana <mulyan.t20@gmail.com>
2025-06-03 13:49:49 +0700

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,20 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { loadEnv } from 'vite'
 
-const env = loadEnv(process.env.MODE || '', process.cwd(), '')
-
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './test/vitest.setup.ts',
-		include: ['src/**/*.{test,spec}.{js,ts,tsx}'],
-		exclude: ['node_modules', 'e2e'],
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	define: {
-		'import.meta.env': env,
-	},
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/vitest.setup.ts',
+    include: ['src/**/*.{test,spec}.{js,ts,tsx}'],
+    exclude: ['node_modules', 'e2e']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
